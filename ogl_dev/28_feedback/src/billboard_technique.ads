6f47e61894a3d6f887871510ec5ab769010fde40
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-08-05 15:10:04 +1000

    Added examples 22 and 28

---
+++ ogl_dev/28_feedback/src/billboard_technique.ads
@@ -0,0 +1,32 @@
+
+with GL.Objects.Programs;
+with GL.Types;
+with GL.Uniforms;
+
+Package Billboard_Technique is
+
+   type Technique is private;
+
+   function Billboard_Program (theTechnique : Technique)
+                               return GL.Objects.Programs.Program;
+   procedure Init (theTechnique : out Technique);
+   procedure Set_Billboard_Size (theTechnique : Technique;
+                                 Size : GL.Types.Single);
+   procedure Set_Camera_Position (theTechnique : Technique;
+                                  Position : GL.Types.Singles.Vector3);
+   procedure Set_Colour_Texture_Unit (theTechnique : Technique;
+                                      Texture_Unit : GL.Types.Int);
+   procedure Set_View_Point (theTechnique : Technique;
+                             View_Point : GL.Types.Singles.Matrix4);
+
+private
+
+   type Technique is record
+      Program                  : GL.Objects.Programs.Program;
+      View_Point_Location      : GL.Uniforms.Uniform;
+      Camera_Position_Location : GL.Uniforms.Uniform;
+      Colour_Map_Location      : GL.Uniforms.Uniform;
+      Billboard_Size_Location  : GL.Uniforms.Uniform;
+   end record;
+
+end Billboard_Technique;
