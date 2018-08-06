0d647ac9cbed2d4342e6a9aacab92f06c34b52e8
Roger <rogermc@grapevine.com.au>
2018-08-06 22:21:33 +1000

    Coding

--- ogl_dev/common/assimp/assimp_mesh.adb
+++ ogl_dev/common/assimp/assimp_mesh.adb
@@ -9,21 +9,31 @@ package body Assimp_Mesh is
 
     function Init_From_Scene (theMesh : in out Mesh; theScene : Scene.AI_Scene;
                               File_Name : String) return Boolean;
-    function Init_Materials (theMesh : in out AI_Mesh; theScene : Scene.AI_Scene;
+    function Init_Materials (theMesh : in out Mesh; theScene : Scene.AI_Scene;
                              File_Name : String) return Boolean;
-    procedure Init_Mesh (theMesh : in out AI_Mesh; Mesh_Index : UInt);
+    procedure Init_Mesh (theMesh : in out Mesh; Mesh_Index : UInt; anAI_Mesh : AI_Mesh);
+    function To_API_Mesh (anAI_Mesh : AI_Mesh) return API_Mesh;
 
     --  ------------------------------------------------------------------------
 
     function Init_From_Scene (theMesh : in out Mesh; theScene  : Scene.AI_Scene;
-                              File_Name : String)return Boolean is
+                              File_Name : String) return Boolean is
+        use AI_Mesh_Package;
+        anAI_Mesh : AI_Mesh;
+        Index     : UInt := 0;
     begin
-        return False;
+        for iterator  in theScene.Meshes.Iterate loop
+            anAI_Mesh := Element (iterator);
+            Index := Index + 1;
+            Init_Mesh (theMesh, Index, anAI_Mesh);
+        end loop;
+
+        return Init_Materials (theMesh, theScene, File_Name);
     end Init_From_Scene;
 
     --  ------------------------------------------------------------------------
 
-    function Init_Materials (theMesh : in out AI_Mesh; theScene : Scene.AI_Scene;
+    function Init_Materials (theMesh : in out Mesh; theScene : Scene.AI_Scene;
                              File_Name : String) return Boolean is
     begin
         return False;
@@ -31,8 +41,10 @@ package body Assimp_Mesh is
 
     --  ------------------------------------------------------------------------
 
-    procedure Init_Mesh (theMesh : in out AI_Mesh; Mesh_Index : UInt) is
+    procedure Init_Mesh (theMesh : in out Mesh; Mesh_Index : UInt; anAI_Mesh : AI_Mesh) is
+        pai_Mesh : API_Mesh := To_API_Mesh (anAI_Mesh);
     begin
+
         null;
 --          Assimp.API.Init_Mesh (theMesh : in out API_Mesh; Mesh_Index : UInt)
 --            (Material, Tex_Type, unsigned (Tex_Index), C_Path'Access);
@@ -54,6 +66,14 @@ package body Assimp_Mesh is
     begin
         null;
     end Render_Mesh;
+
+    --  ------------------------------------------------------------------------
+
+    function To_API_Mesh (anAI_Mesh : AI_Mesh) return API_Mesh is
+        newMesh : API_Mesh;
+    begin
+        return newMesh;
+    end To_API_Mesh;
 
     --  ------------------------------------------------------------------------
 
