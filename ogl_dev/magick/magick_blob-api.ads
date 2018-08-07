fe52d26b0d2a6a427924a1e5f5e1492fa7e6afea
Roger <rogermc@iinet.net.au.com>
2018-08-07 21:49:10 +1000

    Ongoing Blob C interface development

--- ogl_dev/magick/magick_blob-api.ads
+++ ogl_dev/magick/magick_blob-api.ads
@@ -3,32 +3,36 @@ with System;
 
 with Interfaces.C;
 
-with Blob_Reference;
+--  with Blob_Reference;
 
 package Magick_Blob.API is
 
-   -- File_To_Image and Image_To_Blob moved to Magick_Image.API to avoid circularities
+    -- File_To_Image and Image_To_Blob moved to Magick_Image.API to avoid circularities
+    --
+    --     package Class_Blob is
+    --
+    --        type Blob is tagged limited record
+    --              null;
+    --               Blob_Ref : access Blob_Reference.Class_Blob_Ref.Blob_Ref;
+    --           Blob_Ref : System.Address;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:75
+    --        end record;
+    --        pragma Import (CPP, Blob);
 
-   package Class_Blob is
+    function New_Blob return Blob;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:31
+    pragma Import (C, New_Blob, "newBlob");
+    --        pragma Cpp_Constructor (New_Blob, "");
 
-      type Blob is tagged limited record
-             Blob_Ref : access Blob_Reference.Class_Blob_Ref.Blob_Ref;
---           Blob_Ref : System.Address;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:75
-      end record;
-      pragma Import (CPP, Blob);
+    procedure Delete_Blob (this : access Blob);  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:40
+    pragma Import (C, Delete_Blob, "deleteBlob");
+    --        pragma Import (CPP, Delete_Blob, "_ZN6Magick4BlobD1Ev");
 
-      function New_Blob return Blob;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:31
-      pragma Cpp_Constructor (New_Blob, "_ZN6Magick4BlobC1Ev");
+    function Data (this : access constant Blob'Class) return System.Address;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:55
+    pragma Import (C, Data, "data");
+      --      pragma Import (CPP, Data, "_ZNK6Magick4Blob4dataEv");
 
-      procedure Delete_Blob (this : access Blob);  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:40
-      pragma Import (CPP, Delete_Blob, "_ZN6Magick4BlobD1Ev");
-
-      function Data (this : access constant Blob'Class) return System.Address;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:55
-      pragma Import (CPP, Data, "_ZNK6Magick4Blob4dataEv");
-
-      function Length (this : access constant Blob'Class) return Interfaces.C.size_t;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:58
-      pragma Import (CPP, Length, "_ZNK6Magick4Blob6lengthEv");
-
-   end Class_Blob;
+    function Length (this : access constant Blob'Class) return Interfaces.C.size_t;  -- /usr/local/Cellar/imagemagick/7.0.7-31/include/ImageMagick-7/Magick++/Blob.h:58
+    pragma Import (C, Length, "length");
+      --  pragma Import (CPP, Length, "_ZNK6Magick4Blob6lengthEv");
+      --     end Class_Blob;
 
 end Magick_Blob.API;
--- ogl_dev/magick/magick_image.ads
+++ ogl_dev/magick/magick_image.ads
@@ -2,6 +2,7 @@
 with Magick_Blob.API;
 limited with Magick_Image.API;
 with Core_Image;
+with Image_Reference;
 
 package Magick_Image is
 
@@ -9,10 +10,12 @@ package Magick_Image is
 
    procedure Read_File (theImage : in out Core_Image.AI_Image;
                         File_Name : String);
-   procedure Write_File (theImage : in out Magick_Image.API.Class_Image.MPP_Image;
+--     procedure Write_File (theImage : in out Magick_Image.API.Class_Image.MPP_Image;
+   procedure Write_File (theImage : Core_Image.AI_Image;
                           File_Name : String);
-   procedure Write_Blob (theImage : in out Magick_Image.API.Class_Image.MPP_Image;
-                         theBlob  : in out Magick_Blob.API.Class_Blob.Blob;
+--     procedure Write_Blob (Image_Ptr : in out Image_Reference.Class_Image_Ref.Image_Ref;
+   procedure Write_Blob (theImage : Core_Image.AI_Image;
+                         theBlob  : Magick_Blob.Blob_Data;
                          Data_Type : String);
 
 end Magick_Image;
