3ec3267b9bcc268e289fb0d7dcf4032037684973
Roger <rogermc@iinet.net.au.com>
2018-08-08 21:42:09 +1000

    Ongoing write blob

--- ogl_dev/magick/magick_image-api.ads
+++ ogl_dev/magick/magick_image-api.ads
@@ -1,29 +1,34 @@
 
+with System;
+
 with Interfaces.C; use Interfaces.C;
 with Interfaces.C.Strings;
 
-with Image_Reference;
+--  with Image_Reference;
 
 package Magick_Image.API is
 
 --     package Class_Image is
 
-      type MPP_Image is tagged limited record
-         Ref : access Image_Reference.Class_Image_Ref.Image_Ref;
+--        type MPP_Image is tagged limited record
+      type MPP_Image is record
+         Ref : System.Address;
+--           Ref : access Image_Reference.Class_Image_Ref.Image_Ref;
       end record;
-      pragma Import (Cpp, MPP_Image);
+    pragma Convention (C_Pass_By_Copy, MPP_Image);
+--        pragma Import (Cpp, MPP_Image);
 
 --        procedure Delete_Image (this : access Image);  -- ../Magick++/lib/Magick++/imagePP.h:95
 --        pragma Import (CPP, Delete_Image, "_ZN6Magick5ImageD1Ev");
 
-      function New_Image return MPP_Image;
-      pragma Cpp_Constructor (New_Image, "_ZN6Magick5ImageC1Ev");
+--        function New_Image return MPP_Image;
+--        pragma Cpp_Constructor (New_Image, "_ZN6Magick5ImageC1Ev");
 
       --  According to the GNAT Reference Manual, T
       --  there is no need to indicate the C++ mangled names associated
       --  with each subprogram because it is assumed that all the calls to
       --  these primitives will be dispatching calls.
-      procedure Read (this : in out MPP_Image;
+      procedure Read (Image : in out MPP_Image;
                       File_Name : Interfaces.C.Strings.chars_ptr);  -- ../Magick++/lib/Magick++/ImagePP.h:1236
       pragma Import (Cpp, Read, "readFile");
 --        pragma Import (Cpp, Read, "_ZN6Magick5Image4readERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE");
