ab0bd4c7f7b822eaaea76bc2ae7d261e7cf8141a
Roger <rogermc@iinet.net.au.com>
2018-08-10 17:13:01 +1000

    Update to use api_vectors_matrices

--- ogl_dev/common/assimp/assimp_util.ads
+++ ogl_dev/common/assimp/assimp_util.ads
@@ -5,20 +5,20 @@ with GL.Types; use GL.Types;
 
 with Assimp_Types;
 
-with API_Vectors;
+with API_Vectors_Matrices;
 
 package Assimp_Util is
 
    function To_Assimp_AI_String
      (UB_String :  Ada.Strings.Unbounded.Unbounded_String)
       return Assimp_Types.AI_String;
-   function To_OGL_Vector2 (C_Vec : API_Vectors.API_Vector_2D)
+   function To_OGL_Vector2 (C_Vec : API_Vectors_Matrices.API_Vector_2D)
                             return Singles.Vector2;
-   function To_OGL_Vector3 (C_Vec : API_Vectors.API_Vector_3D)
+   function To_OGL_Vector3 (C_Vec : API_Vectors_Matrices.API_Vector_3D)
                             return Singles.Vector3;
-   function To_Colour3D (C_Colours : API_Vectors.API_Colour_3D)
+   function To_Colour3D (C_Colours : API_Vectors_Matrices.API_Colour_3D)
                           return Singles.Vector3;
-   function To_Colour4D (C_Colours : API_Vectors.API_Colour_4D)
+   function To_Colour4D (C_Colours : API_Vectors_Matrices.API_Colour_4D)
                           return Singles.Vector4;
 
 end Assimp_Util;
