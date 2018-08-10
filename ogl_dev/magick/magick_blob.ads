0e51ea5c9747103243c82d4e07a03f931a2ff65a
Roger <rogermc@grapevine.com.au>
2018-08-10 12:18:40 +1000

    Delete uneeded code

--- ogl_dev/magick/magick_blob.ads
+++ ogl_dev/magick/magick_blob.ads
@@ -3,16 +3,8 @@ with Ada.Containers.Doubly_Linked_Lists;
 
 with GL.Types;
 
-limited with Magick_Blob.API;
-
---  Magic_Blob corresponds to Magic++ Blob
 package Magick_Blob is
    use GL.Types;
-
-   type Data_Array is  array (UInt range <>) of UByte;
-
-   type Allocator is (Malloc_Allocator, New_Allocator);
-   pragma Convention (C, Allocator);
 
    package Blob_Package is new Ada.Containers.Doubly_Linked_Lists (UByte);
    subtype Blob_Data is Blob_Package.List;
