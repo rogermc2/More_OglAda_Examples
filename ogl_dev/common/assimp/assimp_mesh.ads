7cec637a4a3f9db81da8be431faa37cf2b78f5c4
Roger <rogermc@iinet.net.au.com>
2018-08-13 11:40:13 +1000

    Recover

--- ogl_dev/common/assimp/assimp_mesh.ads
+++ ogl_dev/common/assimp/assimp_mesh.ads
@@ -159,7 +159,7 @@ package Assimp_Mesh is
 --     pragma Convention (C_Pass_By_Copy, API_Mesh);
 
    procedure Load_Mesh (File_Name : String; theMesh : in out Mesh);
-   procedure Render_Mesh (theMesh : Mesh);
+--     procedure Render_Mesh (theMesh : Mesh);
 
     private
          for AI_Primitive_Type use
