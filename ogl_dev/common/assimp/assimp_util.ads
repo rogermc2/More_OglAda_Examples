e7b68bf409d4c1d9f0aa006e3519e9d0623a7070
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-08-13 14:43:05 +1000

    To_Unbounded_String coded

--- ogl_dev/common/assimp/assimp_util.ads
+++ ogl_dev/common/assimp/assimp_util.ads
@@ -19,6 +19,8 @@ package Assimp_Util is
    function To_Colour3D (C_Colours : API_Vectors_Matrices.API_Colour_3D)
                           return Singles.Vector3;
    function To_Colour4D (C_Colours : API_Vectors_Matrices.API_Colour_4D)
-                          return Singles.Vector4;
+                         return Singles.Vector4;
+   function To_Unbounded_String (AI_String : Assimp_Types.AI_String)
+                                 return Ada.Strings.Unbounded.Unbounded_String;
 
 end Assimp_Util;
