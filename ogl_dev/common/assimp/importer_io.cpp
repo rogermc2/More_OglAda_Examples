c4a900ca6b1728e577c38d0b02cfaa43dd4cece8
Roger <rogermc@grapevine.com.au>
2018-08-17 16:23:16 +1000

    Read_File seems to be working

--- ogl_dev/common/assimp/importer_io.cpp
+++ ogl_dev/common/assimp/importer_io.cpp
@@ -10,13 +10,15 @@
 //  use the latter's constructor or assignment operator.
 
 //  Read_File returns a pointer to the imported data; NULL if the import failed.
+static Assimp::Importer aiScene_Object;
+
 const aiScene* aiReadFile(const char* fileName, unsigned int Flags)
   {
   const aiScene*   theScene_Ptr;
-  Assimp::Importer aiScene_Object;
   std::string cppString (fileName);
 
   aiScene_Object.ReadFile(cppString, Flags);
   theScene_Ptr = aiScene_Object.GetScene();
+  printf ("importer_IO.aiReadFile theScene_Ptr: %p\n", theScene_Ptr);
   return theScene_Ptr;
   }
