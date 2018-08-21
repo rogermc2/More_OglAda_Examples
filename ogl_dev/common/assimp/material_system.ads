a9040ab89642e5b2e76c6da8172b89ffc059c6ac
Roger <rogermc@iinet.net.au.com>
2018-08-21 21:41:00 +1000

    Coding

--- ogl_dev/common/assimp/material_system.ads
+++ ogl_dev/common/assimp/material_system.ads
@@ -8,8 +8,8 @@ with Assimp_Types;
 
 package Material_System is
 
-    function Get_Material_String (aMaterial : Material.API_Material; Key : out Interfaces.C.Strings.chars_ptr;
-                                  Property_Type, Property_Index : out Interfaces.C.unsigned;
+    function Get_Material_String (aMaterial : Material.API_Material; Key : Assimp_Types.API_String;
+                                  Property_Type, Property_Index : Interfaces.C.unsigned;
                                   Data_String : out Assimp_Types.API_String) return Assimp_Types.API_Return ;
 
 end Material_System;
