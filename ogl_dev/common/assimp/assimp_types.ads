afc85240dcae21d471381b9f4059e53fe28a903d
Roger <rogermc@iinet.net.au.com>
2018-09-05 17:28:25 +1000

    Change API string

--- ogl_dev/common/assimp/assimp_types.ads
+++ ogl_dev/common/assimp/assimp_types.ads
@@ -1,5 +1,6 @@
 
 with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Pointers;
 with Interfaces.C.Strings;
 
 with GL.Types; use GL.Types;
@@ -10,6 +11,7 @@ package Assimp_Types is
 
    type API_Return is (API_Return_Out_Of_Memory, API_Return_Failure,
                        API_Return_Success, API_Enforce_Enum_Size);
+
    Max_Length : constant Interfaces.C.size_t := 1024;
    subtype API_String_Data_Array is char_array (0 .. Max_Length - 1);
 
@@ -28,9 +30,14 @@ package Assimp_Types is
 
    type API_String is record
       Length  : Interfaces.C.size_t := 0;
-      Data    : API_String_Data_Array := (others => Interfaces.C.char'Val (0));
+      Data    : access API_String_Data_Array := null;
+--        Data    : API_String_Data_Array := (others => Interfaces.C.char'Val (0));
    end record;
    pragma Convention (C_Pass_By_Copy, API_String);
+
+   type Raw_Byte_Data is array (UInt range <>) of aliased UByte;
+   package Raw_Data_Pointers is new
+     Interfaces.C.Pointers (UInt, UByte, Raw_Byte_Data, UByte'Last);
 
 private
 
