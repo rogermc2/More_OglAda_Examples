00d7fadf6883af360384540be61a2e30983c9dad
Roger <rogermc@iinet.net.au.com>
2018-09-07 10:16:50 +1000

    Removed  some in outs

--- ogl_dev/common/assimp/scene.ads
+++ ogl_dev/common/assimp/scene.ads
@@ -102,9 +102,9 @@ package Scene is
          end record;
          pragma Convention (C_Pass_By_Copy, API_Scene);
 
-    procedure To_AI_Scene (C_Scene : in out API_Scene;
+    procedure To_AI_Scene (C_Scene : API_Scene;
                            theScene : in out Scene.AI_Scene);
-    procedure To_Node_List (Root_Node : in out API_Node;
+    procedure To_Node_List (Root_Node : API_Node;
                             Nodes : in out Scene.AI_Nodes_List);
 
 private
