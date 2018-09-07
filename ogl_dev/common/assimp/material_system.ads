4cdc46496c52bba08605f4ab3408b15ed489dd81
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-09-07 16:35:58 +1000

    Debugging

--- ogl_dev/common/assimp/material_system.ads
+++ ogl_dev/common/assimp/material_system.ads
@@ -9,7 +9,8 @@ with Assimp_Types;
 package Material_System is
 
     function Get_Material_String (aMaterial : Material.API_Material; Key : Assimp_Types.API_String;
-                                  Property_Type, Property_Index : Interfaces.C.unsigned;
+                                  Property_Type  : Material.AI_Property_Type_Info;
+                                  Property_Index : Interfaces.C.unsigned;
                                   Data_String : out Assimp_Types.API_String) return Assimp_Types.API_Return ;
 
 end Material_System;
