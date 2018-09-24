df4523990e3da71250372e71eede0e3d0ee7b897
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-09-24 15:28:34 +1000

    New files

---
+++ ogl_dev/common/assimp/ai_conversion.ads
@@ -0,0 +1,11 @@
+
+with Interfaces.C;
+
+with Material;
+
+package AI_Conversion is
+
+   function To_AI_Materials_Map (Num_Materials    : Interfaces.C.unsigned := 0;
+                                 C_Material_Array : in out Material.API_Material_Array)
+                                 return Material.AI_Material_Map;
+end AI_Conversion;
