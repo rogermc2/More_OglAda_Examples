89762a42b1434b94eb48748c7e3c6e071944810d
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-09-24 16:08:36 +1000

    To_AI_Property coded

--- ogl_dev/common/assimp/ai_conversion.ads
+++ ogl_dev/common/assimp/ai_conversion.ads
@@ -5,6 +5,8 @@ with Material;
 
 package AI_Conversion is
 
+   Conversion_Exception : Exception;
+
    function To_AI_Materials_Map (Num_Materials    : Interfaces.C.unsigned := 0;
                                  C_Material_Array : in out Material.API_Material_Array)
                                  return Material.AI_Material_Map;
