e6d17d5d3fa864a958b91270ca447d91b06201d6
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-09-24 11:58:22 +1000

    Print_AI_Property_Data and Print_API_Property_Data added

--- ogl_dev/common/assimp/assimp_util.adb
+++ ogl_dev/common/assimp/assimp_util.adb
@@ -2,10 +2,67 @@
 with Interfaces.C;
 with Interfaces.C.Strings;
 
+with Ada.Containers;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package body Assimp_Util is
 
+   procedure Print_AI_Property_Data (Title     : String;
+                                     aProperty : Material.AI_Material_Property) is
+
+   begin
+      New_Line;
+      Put_Line (Title & " Property_Data:");
+      Put_Line (" Key: " & Ada.Strings.Unbounded.To_String (aProperty.Key));
+      Put_Line (" Semantic, Texture_Index: " & UInt'Image (aProperty.Semantic)
+                & UInt'Image (aProperty.Texture_Index));
+      Put_Line (" Semantic, Data_Type, Buffer size: " &
+                  Material.AI_Property_Type_Info'Image (aProperty.Data_Type) &
+                  Ada.Containers.Count_Type'Image (aProperty.Data_Buffer.Length));
+   end Print_AI_Property_Data;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Print_API_Property_Data (Title     : String;
+                                      aProperty : Material.API_Material_Property) is
+      use Interfaces.C;
+   begin
+      New_Line;
+      Put_Line (Title & " Property_Data:");
+      Put_Line (" Key length, Key: " & size_t'Image (aProperty.Key.Length) &
+                  ", " & To_String (aProperty.Key));
+      if aProperty.Key.Length = 0 then
+         Put_Line ("Invalid key!");
+      end if;
+      Put_Line (" Semantic, Texture_Index: " & unsigned'Image (aProperty.Semantic)
+                & unsigned'Image (aProperty.Texture_Index));
+      Put_Line (" Semantic, Data_Type, Buffer size: " &
+                  Material.AI_Property_Type_Info'Image (aProperty.Data_Type) &
+                  unsigned'Image (aProperty.Data_Length));
+   end Print_API_Property_Data;
+
+   --  -------------------------------------------------------------------------
+
+--     procedure Print_API_Property_Array (Title  : String;
+--                                        anArray : Material.API_Property_Array) is
+--        use Interfaces.C;
+--     begin
+--        for index in anArray'First .. anArray'Last loop
+--              Print_API_Property_Data (Title & unsigned'Image (index), anArray (index));
+--        end loop;
+--
+--      exception
+--          when others =>
+--              Put_Line ("An exception occurred in Print_API_Property_Array.");
+--              raise;
+--     end Print_API_Property_Array;
+
+   --  -------------------------------------------------------------------------
+
+
    function To_Assimp_API_String
-     (UB_String :  Ada.Strings.Unbounded.Unbounded_String) return Assimp_Types.API_String is
+     (UB_String :  Ada.Strings.Unbounded.Unbounded_String)
+      return Assimp_Types.API_String is
       use Interfaces.C;
       theString     : constant String := Ada.Strings.Unbounded.To_String (UB_String);
       Assimp_String : Assimp_Types.API_String;
@@ -21,7 +78,7 @@ package body Assimp_Util is
    --  ------------------------------------------------------------------------
 
    function To_OGL_Vector2 (C_Vec : API_Vectors_Matrices.API_Vector_2D)
-                             return Singles.Vector2 is
+                            return Singles.Vector2 is
       Vec : Singles.Vector2;
    begin
       Vec (GL.X) := Single (C_Vec.X);
@@ -32,7 +89,7 @@ package body Assimp_Util is
    --  ------------------------------------------------------------------------
 
    function To_OGL_Vector3 (C_Vec : API_Vectors_Matrices.API_Vector_3D)
-                             return Singles.Vector3 is
+                            return Singles.Vector3 is
       Vec : Singles.Vector3;
    begin
       Vec (GL.X) := Single (C_Vec.X);
@@ -44,7 +101,7 @@ package body Assimp_Util is
    --  ------------------------------------------------------------------------
 
    function To_Colour3D (C_Colours : API_Vectors_Matrices.API_Colour_3D)
-                          return Singles.Vector3 is
+                         return Singles.Vector3 is
       theColours : Singles.Vector3;
    begin
       theColours :=
@@ -55,7 +112,7 @@ package body Assimp_Util is
    --  ------------------------------------------------------------------------
 
    function To_Colour4D (C_Colours : API_Vectors_Matrices.API_Colour_4D)
-                          return Singles.Vector4 is
+                         return Singles.Vector4 is
       theColours : Singles.Vector4;
    begin
       theColours :=
@@ -67,7 +124,7 @@ package body Assimp_Util is
    --  ------------------------------------------------------------------------
 
    function To_Unbounded_String (API_String : Assimp_Types.API_String)
-                                  return Ada.Strings.Unbounded.Unbounded_String is
+                                 return Ada.Strings.Unbounded.Unbounded_String is
       use Interfaces.C.Strings;
       use Ada.Strings.Unbounded;
       API_String_Ptr : constant chars_ptr := New_Char_Array (API_String.Data);
--- ogl_dev/common/assimp/assimp_util.ads
+++ ogl_dev/common/assimp/assimp_util.ads
@@ -4,11 +4,18 @@ with Ada.Strings.Unbounded;
 with GL.Types; use GL.Types;
 
 with Assimp_Types;
+with Material;
 
 with API_Vectors_Matrices;
 
 package Assimp_Util is
 
+   procedure Print_AI_Property_Data (Title     : String;
+                                     aProperty : Material.AI_Material_Property);
+   procedure Print_API_Property_Data (Title     : String;
+                                      aProperty : Material.API_Material_Property);
+--     procedure Print_API_Property_Array (Title  : String;
+--                                        anArray : Material.API_Property_Array);
    function To_Assimp_API_String
      (UB_String : Ada.Strings.Unbounded.Unbounded_String)
       return Assimp_Types.API_String;
@@ -17,7 +24,7 @@ package Assimp_Util is
    function To_OGL_Vector3 (C_Vec : API_Vectors_Matrices.API_Vector_3D)
                             return Singles.Vector3;
    function To_Colour3D (C_Colours : API_Vectors_Matrices.API_Colour_3D)
-                          return Singles.Vector3;
+                         return Singles.Vector3;
    function To_Colour4D (C_Colours : API_Vectors_Matrices.API_Colour_4D)
                          return Singles.Vector4;
    function To_Unbounded_String (API_String : Assimp_Types.API_String)
