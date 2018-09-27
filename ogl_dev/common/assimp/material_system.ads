19e44bda56e4ac4243533d4d64e48cd93d5c9303
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-09-27 16:28:09 +1000

    Changed Property_Type from uint to AI_Property_Type_Info type

--- ogl_dev/common/assimp/material_system.ads
+++ ogl_dev/common/assimp/material_system.ads
@@ -14,7 +14,7 @@ package Material_System is
 
    function Get_Material_String (aMaterial      : Material.AI_Material;
                                  Key            : String;
-                                 Property_Type  : GL.Types.UInt;
+                                 Property_Type  : Material.AI_Property_Type_Info;
                                  Property_Index : GL.Types.UInt;
                                  Data_String    : out Ada.Strings.Unbounded.Unbounded_String)
                                  return Assimp_Types.API_Return;
