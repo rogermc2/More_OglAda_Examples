53c9ca39306a2acc22e257275fe9fd757c5436d1
Roger <rogermc@iinet.net.au.com>
2018-09-27 22:40:14 +1000

    Data_To_UB_String Debug

--- ogl_dev/common/assimp/ai_conversion.adb
+++ ogl_dev/common/assimp/ai_conversion.adb
@@ -2,6 +2,7 @@
 with Interfaces.C.Strings;
 
 with Ada.Containers;
+with Ada.Unchecked_Conversion;
 
 with GL.Types;
 
@@ -27,6 +28,57 @@ package body AI_Conversion is
     function To_Integer (Bytes_In : Byte_Array4) return GL.Types.Int;
 
     --  ----------------------------------------------------------------------
+
+    function Data_To_UB_String (String_Data : Assimp_Types.Raw_Byte_Data) return
+     Ada.Strings.Unbounded.Unbounded_String is
+     use Interfaces;
+      use GL.Types;
+
+     pragma Warnings (Off);
+      function To_Unsigned8 is new Ada.Unchecked_Conversion (Unsigned_16, Unsigned_8);
+     pragma Warnings (On);
+
+      Data4       : constant Byte_Array4 := Byte_Array4 (String_Data (1 .. 4));
+      Data_Length : UInt := String_Data'Length;
+      Data_String : Ada.Strings.Unbounded.Unbounded_String :=
+                        Ada.Strings.Unbounded.To_Unbounded_String ("");
+   begin
+      Put_Line ("Material_System.Get_Texture Data_Length: " & UInt'Image (Data_Length));
+      if Data_Length >= 5 then
+--           Data_Length := UInt (To_Integer (Data4)); -- number of Bytes
+--           Put_Line ("Material_System.Get_Texture Data_Length: " & UInt'Image (Data_Length));
+         declare
+            -- One byte contains two characters
+            Str_Data : String (1 .. 2 * Integer (Data_Length - 4));
+            aByte    : UByte;
+            U16      : Unsigned_16;
+            U8_First : Unsigned_8;
+            U8_Scnd : Unsigned_8;
+            Str_Index : Integer := 1;
+         begin
+            for index in 5 .. Data_Length loop
+               Put_Line ("Material_System.Get_Texture index: " & UInt'Image (index));
+               aByte := String_Data (index);
+               U16 := Unsigned_16 (aByte);
+               U8_First := To_Unsigned8 (Shift_Right (U16, 8));
+               U8_Scnd := To_Unsigned8 (U16);
+               Str_Data (Str_Index) := Character'Val (U8_First);
+               Str_Data (Str_Index + 1) := Character'Val (U8_Scnd);
+               Str_Index := Str_Index + 2;
+            end loop;
+            Data_String := Ada.Strings.Unbounded.To_Unbounded_String (Str_Data);
+         end;
+      end if;
+      Put_Line ("Material_System.Get_Texture Data_String: " & Ada.Strings.Unbounded.To_String (Data_String));
+      return Data_String;
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Material_System.Get_Texture.");
+         raise;
+   end Data_To_UB_String;
+
+   --  -------------------------------------------------------------------------
 
     function To_AI_Material (C_Material : Material.API_Material) return Material.AI_Material is
         use Interfaces.C;
@@ -93,14 +145,14 @@ package body AI_Conversion is
         use Material.API_Property_Array_Package;
         use Raw_Data_Pointers;
 
-        Key_Length        : size_t := API_Property.Key.Length;
+        Key_Length    : size_t := API_Property.Key.Length;
         --  Data_Length number of bytes
-        Data_Length       : constant size_t := size_t (API_Property.Data_Length);
-        Data_Array        : Raw_Byte_Data (1 .. UInt (Data_Length));
-        Data4             : Byte_Array4;
-        Key_String        : constant String (1 .. Integer (Key_Length)) :=
-                              To_Ada (API_Property.Key.Data);
-        AI_Property       : Material.AI_Material_Property (API_Property.Data_Type);
+        Data_Length  : constant size_t := size_t (API_Property.Data_Length);
+        Data_Array   : Raw_Byte_Data (1 .. UInt (Data_Length));
+        Data4        : Byte_Array4;
+        Key_String   : constant String (1 .. Integer (Key_Length)) :=
+                          To_Ada (API_Property.Key.Data);
+        AI_Property  : Material.AI_Material_Property (API_Property.Data_Type);
     begin
         if Key_Length > 0 then
             declare
@@ -120,8 +172,10 @@ package body AI_Conversion is
                 case API_Property.Data_Type is
                 when Material.PTI_Float => AI_Property.Data_Buffer.Float_Data := 0.0;
                 when Material.PTI_Double => AI_Property.Data_Buffer.Double_Data := 0.0;
+--                  when Material.PTI_String => AI_Property.Data_Buffer.String_Data
+--                        := Ada.Strings.Unbounded.To_Unbounded_String ("");
                 when Material.PTI_String => AI_Property.Data_Buffer.String_Data
-                      := Ada.Strings.Unbounded.To_Unbounded_String ("");
+                      := Data_To_UB_String (Data_Array);
                 when Material.PTI_Integer =>
                     for index in 1 .. 4 loop
                         Data4 (index) := Data_Array (UInt (index));
