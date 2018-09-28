58a94f156eebcd5fae1c4eea069dd040a818e4a5
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-09-28 11:16:05 +1000

    Omgoing development

--- ogl_dev/common/assimp/material_system.adb
+++ ogl_dev/common/assimp/material_system.adb
@@ -25,39 +25,8 @@ package body Material_System is
                                    Index          : GL.Types.UInt;
                                    theProperty    : out AI_Material_Property)
                                    return API_Return;
-   function To_Integer (Bytes_In : Byte_Array4) return GL.Types.Int;
 
    --  -------------------------------------------------------------------------------------
-
-   function Data_To_UB_String (String_Data : Ada.Strings.Unbounded.Unbounded_String) return
-     Ada.Strings.Unbounded.Unbounded_String is
-      use GL.Types;
-      Size_String : String_4;
-      Data_Length : UInt;
-      theString   : constant String :=
-                        Ada.Strings.Unbounded.To_String (String_Data);
-      Data_String :  Ada.Strings.Unbounded.Unbounded_String :=
-                        Ada.Strings.Unbounded.To_Unbounded_String ("");
-   begin
-      Data_Length := theString'Length;
-      if Data_Length >= 5 then
-         Size_String := String_4 (theString (1 .. 4));
-         declare
-            Str_Data : constant String (1 .. Integer (Data_Length - 4)) :=
-                         theString (5 .. Integer (Data_Length));
-         begin
-            Data_String := Ada.Strings.Unbounded.To_Unbounded_String (Str_Data);
-         end;
-      end if;
-      return Data_String;
-
-   exception
-      when others =>
-         Put_Line ("An exception occurred in Material_System.Get_Texture.");
-         raise;
-   end Data_To_UB_String;
-
-   --  -------------------------------------------------------------------------
 
    function Get_Material_Integer (aMaterial     : AI_Material;
                                   Key           : String;
@@ -68,27 +37,20 @@ package body Material_System is
       use Assimp_Types.Byte_Data_Package;
       Result      : API_Return :=  Assimp_Types.API_Return_Failure;
       theProperty : AI_Material_Property;
-      --        curs        : Cursor := theProperty.Data_Buffer.First;
-      --        theData     : Byte_Array4;
-      --        aChar       : Character;
    begin
       theInteger := 0;
       Result := Get_Material_Property (aMaterial, Key, Property_Type,
                                        Index, theProperty);
-      --        if Result = API_Return_Success then
-      --           if theProperty.Data_Type = PTI_Integer or
-      --             theProperty.Data_Type = PTI_Buffer then
-      --              for index in 1 ..4 loop
-      --                 aChar := Character (Element (curs));
-      --                 theData (index) := GL.Types.UByte (Character'Pos (aChar));
-      --                 Next (curs);
-      --              end loop;
-      --              theInteger := To_Integer (theData);
-      --           else
-      --              Put ("Material_System.Get_Material_Integer, ");
-      --              Put_Line ("property type is neither integer nor buffer.");
-      --           end if;
-      --        end if;
+      if Result = API_Return_Success then
+         if theProperty.Data_Type = PTI_Integer then
+            theInteger := theProperty.Data_Buffer.Integer_Data;
+         elsif theProperty.Data_Type = PTI_Buffer then
+            theInteger := theProperty.Data_Buffer.Buffer_Data;
+         else
+            Put ("Material_System.Get_Material_Integer, ");
+            Put_Line ("property type is neither integer nor buffer.");
+         end if;
+      end if;
       return Result;
 
    exception
@@ -177,8 +139,7 @@ package body Material_System is
          --           Data_Length := GL.Types.UInt (Buffer.Length - 4);
          if aProperty.Data_Type = Material.PTI_String then
             Put_Line ("Material_System.Get_Material_String PTI_String.");
-            Data_String := Data_To_UB_String
-              (aProperty.Data_Buffer.String_Data);
+            Data_String := aProperty.Data_Buffer.String_Data;
          end if;
       end if;
       Put_Line ("Material_System.Get_Material_String Data_String: " &
@@ -197,7 +158,7 @@ package body Material_System is
                          Tex_Type  : AI_Texture_Type;
                          Tex_Index : GL.Types.UInt := 0;
                          Path      : out Ada.Strings.Unbounded.Unbounded_String)
-                            return API_Return is
+                         return API_Return is
       use Ada.Strings.Unbounded;
       use GL.Types;
       use Assimp_Types;
@@ -227,25 +188,6 @@ package body Material_System is
          Put_Line ("An exception occurred in Material_System.Get_Texture.");
          raise;
    end Get_Texture;
-
-   --  -------------------------------------------------------------------------
-
-   function To_Integer (Bytes_In : Byte_Array4) return GL.Types.Int is
-      use Interfaces;
-      Int32  : Unsigned_32 := 0;
-      Int8   : Unsigned_8;
-   begin
-      for index in 0 .. 3  loop
-         Int8 := Unsigned_8 (Bytes_In (4 - index));
-         Int8 := Rotate_Left (Int8, 4);
-         Int32 := Int32 + Unsigned_32 (Int8);
-         if index /= 3 then
-            Int32 := Shift_Left (Int32, 8);
-         end if;
-      end loop;
-      return GL.Types.Int (Int32);
-
-   end To_Integer;
 
    --  -------------------------------------------------------------------------
 
