0a6e2241ed1e445fe0ae7cdcdc3134e61d4c57ee
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-10-08 15:40:58 +1100

    Reimplemented Read_Image

--- ogl_dev/magick/magick_image.ads
+++ ogl_dev/magick/magick_image.ads
@@ -1,14 +1,21 @@
 
+with System;
 
 with Magick_Blob;
 with Core_Image;
 
 package Magick_Image is
 
+    type MPP_Image is record
+         Ref : System.Address;
+   end record;
+
    Image_Exception : Exception;
 
    function Get_Blob_Data return Magick_Blob.Blob_Data;
    function Get_Image return Core_Image.Image;
    procedure Load_Blob (File_Name, Data_Type : String);
+   procedure Read_Image (theImage  : out MPP_Image;
+                         File_Name : String);
 
 end Magick_Image;
