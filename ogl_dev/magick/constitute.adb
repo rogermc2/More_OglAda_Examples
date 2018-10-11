40941455a3010e48d7cab1cdae2d303ba197b66c
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-10-11 22:07:59 +1100

    Changed Read_Image to use imageinfo input parameter

--- ogl_dev/magick/constitute.adb
+++ ogl_dev/magick/constitute.adb
@@ -15,7 +15,6 @@ package body Constitute is
       Image_Ptr : access Core_Image.API_Image := Null;
       Except    : aliased Magick_Exception.AI_Exception_Info;
    begin
-      Put_Line ("Constitute.Read_Image reading image. " & Interfaces.C.To_Ada (Info.File_Name));
       Image_Ptr := ImageMagick.API.Read_Image (Info, Except'Access);
       Put_Line ("Constitute.Read_Image image read.");
       if Image_Ptr /= Null then
@@ -37,15 +36,26 @@ package body Constitute is
 
    procedure Read_Image (theImage  : in out Core_Image.API_Image;
                          File_Name : String) is
-      theFile   : aliased constant Interfaces.C.char_array :=
+      use Interfaces.C;
+      theFile   : constant Interfaces.C.char_array :=
                     Interfaces.C.To_C (File_Name);
+      Info      : aliased Core_Image.API_Image_Info;
+      Except    : aliased Magick_Exception.AI_Exception_Info;
       Image_Ptr : access Core_Image.API_Image := Null;
    begin
-      Image_Ptr := ImageMagick.API.Read_Image (theFile);
+      for index in 1 .. File_Name'Length loop
+         Info.File_Name (size_t (index - 1)) := char (File_Name (index));
+      end loop;
+      Put_Line ("Constitute.Read_Image 2 reading file. " & File_Name);
+      Put_Line ("Constitute.Read_Image 2 Info.File_Name: " &
+                  To_Ada (Info.File_Name));
+      Put_Line ("Constitute.Read_Image 2 Info.Signature: " &
+                  size_t'Image (Info.Signature));
+      Image_Ptr := ImageMagick.API.Read_Image (Info'Access, Except'Access);
       if Image_Ptr /= Null then
          theImage := Image_Ptr.all;
       else
-         raise Constitute_Exception with "Constitute.Read_Image error. ";
+         raise Constitute_Exception with "Constitute.Read_Image 2 error. ";
       end if;
     exception
         when others =>
--- ogl_dev/magick/imagemagick-api.ads
+++ ogl_dev/magick/imagemagick-api.ads
@@ -10,10 +10,11 @@ package ImageMagick.API is
    function Read_Image (Info : access Core_Image.API_Image_Info;
                         Except : access Magick_Exception.AI_Exception_Info)
                         return Core_Image.API_Image_Ptr;
-
-   function Read_Image (File_Name : Interfaces.C.char_array)
-                        return Core_Image.API_Image_Ptr;
    pragma Import (C, Read_Image, "ReadImage");
+
+--     function Read_Image (File_Name : Interfaces.C.char_array)
+--                          return Core_Image.API_Image_Ptr;
+--     pragma Import (C, Read_Image, "ReadImage");
 
    function Write_Image (Info : access Core_Image.API_Image_Info;
                          theImage : access Core_Image.API_Image;
