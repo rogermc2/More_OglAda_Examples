9bd84eece6bce264b34ff3fc787b44d2fa05d1a4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-10-12 15:21:55 +1100

    Some "tested" notes added

--- ogl_dev/magick/magick_image-api.ads
+++ ogl_dev/magick/magick_image-api.ads
@@ -31,6 +31,7 @@ package Magick_Image.API is
    function Get_Image_Data return System.Address;
    pragma Import (Cpp, Get_Image_Data, "imageData");  --  image_io.data
 
+    --  Load_Blob tested with project 19_specular_lighting
    function Load_Blob (File_Name : Interfaces.C.Strings.chars_ptr;
                        Data_Type : Interfaces.C.Strings.chars_ptr) return int;
    pragma Import (Cpp, Load_Blob, "loadBlob");  --  image_io.writeBlob
@@ -38,11 +39,11 @@ package Magick_Image.API is
 --        function New_Image return MPP_Image;
 --        pragma Cpp_Constructor (New_Image, "_ZN6Magick5ImageC1Ev");
 
-      --  According to the GNAT Reference Manual, T
+      --  According to the GNAT Reference Manual,
       --  there is no need to indicate the C++ mangled names associated
       --  with each subprogram because it is assumed that all the calls to
       --  these primitives will be dispatching calls.
-      --  The CPP wrappers are in imag-io.h
+      --  ******  The CPP wrappers are in imag-io.h  *****
       procedure Read (theImage : in out MPP_Image;
                       File_Name : Interfaces.C.Strings.chars_ptr);  -- ../Magick++/lib/Magick++/ImagePP.h:1236
       pragma Import (Cpp, Read, "readFile");
