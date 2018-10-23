48b75f66d5b3f821383d7318b8ce1ab8792a50fd
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-10-23 13:06:56 +1100

    Implemented To_AI_Texture_Coords_Map

--- ogl_dev/common/assimp/assimp_mesh.ads
+++ ogl_dev/common/assimp/assimp_mesh.ads
@@ -170,7 +170,7 @@ package Assimp_Mesh is
         Tangents          : Vector_3D_Array_Pointers.Pointer;
         Bit_Tangents      : Vector_3D_Array_Pointers.Pointer;
         Colours           : Colours_4D_Array_Pointer;
-        Texture_Coords    : API_Vector_3D_Ptr_Array;
+        Texture_Coords    : Texture_Coords_3D_Array_Pointer;
         Num_UV_Components : API_Unsigned_Array (1 .. API_Max_Texture_Coords);
         Faces             : Faces_Array_Pointer;
         Num_Bones         : Interfaces.C.unsigned := 0;
