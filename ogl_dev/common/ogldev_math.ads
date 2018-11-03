031eabbf0a5834a77ca09aeca3e70247f97b548a
Roger <rogermc@iinet.net.au.com>
2018-11-03 14:32:09 +1100

    Unneeded AI_2D_Package and Vector3_Package declarations commented out

--- ogl_dev/common/ogldev_math.ads
+++ ogl_dev/common/ogldev_math.ads
@@ -9,9 +9,9 @@ with Assimp_Types;
 package Ogldev_Math is
    use GL.Types.Singles;
 
-   package AI_2D_Package is new
-     Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector2);
-   subtype AI_2D_Map is AI_2D_Package.Map;
+--     package AI_2D_Package is new
+--       Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector2);
+--     subtype AI_2D_Map is AI_2D_Package.Map;
 
    package AI_3D_Package is new
      Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector3);
@@ -20,9 +20,9 @@ package Ogldev_Math is
    type Perspective_Projection_Info is private;
    type Orthographic_Projection_Info is private;
 
-   package Vector3_Package is new
-     Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector3);
-   type Vector3D is new Vector3_Package.Map with null Record;
+--     package Vector3_Package is new
+--       Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector3);
+--     type Vector3D is new Vector3_Package.Map with null Record;
 
    function Get_Orthograpic_Bottom (Info : Orthographic_Projection_Info)
                                     return Single;
