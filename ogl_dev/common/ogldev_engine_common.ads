b4547ad72972ff8cd815aa4908ba7888b0d0503a
Roger <rogermc@grapevine.com.au>
2018-11-06 15:52:12 +1100

    Changed vaue types to INT

--- ogl_dev/common/ogldev_engine_common.ads
+++ ogl_dev/common/ogldev_engine_common.ads
@@ -2,14 +2,14 @@
 with GL.Types; use GL.Types;
 package Ogldev_Engine_Common is
 
-    Colour_Texture_Unit_Index          : constant UInt := 0;
-    Shadow_Texture_Unit_Index          : constant UInt := 1;
-    Cascade_Shadow_Texture_Unit0_Index : constant UInt := Shadow_Texture_Unit_Index;
-    Normal_Texture_Unit_Index          : constant UInt := 2;
-    Random_Texture_Unit_Index          : constant UInt := 3;
-    Displacement_Texture_Unit_Index    : constant UInt := 4;
-    Motion_Texture_Unit_Index          : constant UInt := 5;
-    Cascade_Shadow_Texture_Unit1_Index : constant UInt := 6;
-    Cascade_Shadow_Texture_Unit2_Index : constant UInt := 7;
+    Colour_Texture_Unit_Index          : constant Int := 0;
+    Shadow_Texture_Unit_Index          : constant Int := 1;
+    Cascade_Shadow_Texture_Unit0_Index : constant Int := Shadow_Texture_Unit_Index;
+    Normal_Texture_Unit_Index          : constant Int := 2;
+    Random_Texture_Unit_Index          : constant Int := 3;
+    Displacement_Texture_Unit_Index    : constant Int := 4;
+    Motion_Texture_Unit_Index          : constant Int := 5;
+    Cascade_Shadow_Texture_Unit1_Index : constant Int := 6;
+    Cascade_Shadow_Texture_Unit2_Index : constant Int := 7;
 
 end Ogldev_Engine_Common;
