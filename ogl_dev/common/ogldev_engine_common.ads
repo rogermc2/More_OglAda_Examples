8f589b5ea22a303a15c07a59ff871f0e51320ac3
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-11-07 11:14:25 +1100

    Removed _Index from tecture unit names

--- ogl_dev/common/ogldev_engine_common.ads
+++ ogl_dev/common/ogldev_engine_common.ads
@@ -2,14 +2,14 @@
 with GL.Types; use GL.Types;
 package Ogldev_Engine_Common is
 
-    Colour_Texture_Unit_Index          : constant Int := 0;
-    Shadow_Texture_Unit_Index          : constant Int := 1;
-    Cascade_Shadow_Texture_Unit0_Index : constant Int := Shadow_Texture_Unit_Index;
-    Normal_Texture_Unit_Index          : constant Int := 2;
-    Random_Texture_Unit_Index          : constant Int := 3;
-    Displacement_Texture_Unit_Index    : constant Int := 4;
-    Motion_Texture_Unit_Index          : constant Int := 5;
-    Cascade_Shadow_Texture_Unit1_Index : constant Int := 6;
-    Cascade_Shadow_Texture_Unit2_Index : constant Int := 7;
+    Colour_Texture_Unit          : constant Int := 0;
+    Shadow_Texture_Unit          : constant Int := 1;
+    Cascade_Shadow_Texture_Unit0 : constant Int := Shadow_Texture_Unit;
+    Normal_Texture_Unit          : constant Int := 2;
+    Random_Texture_Unit          : constant Int := 3;
+    Displacement_Texture_Unit    : constant Int := 4;
+    Motion_Texture_Unit          : constant Int := 5;
+    Cascade_Shadow_Texture_Unit1 : constant Int := 6;
+    Cascade_Shadow_Texture_Unit2 : constant Int := 7;
 
 end Ogldev_Engine_Common;
