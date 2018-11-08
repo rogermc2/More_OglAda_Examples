7be52ae3145d3a9c10982f3ce15875bed7561bae
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-11-08 11:38:59 +1100

    Debugging gl-api-ext.Program_Parameter

--- ogl_dev/gl/gl-ext.adb
+++ ogl_dev/gl/gl-ext.adb
@@ -12,10 +12,13 @@ package body GL.Ext is
     begin
       Put_Line ("GL.Ext.Set_Geometry_Input_Type theProgram.Raw_Id, GL_GEOMETRY_INPUT_TYPE_EXT: " &
                   GL.Types.UInt'Image (theProgram.Raw_Id) & "  " &
-                  Unsigned'Image (GL_GEOMETRY_INPUT_TYPE_EXT));
-      GL.API.Ext.Program_Parameter (theProgram.Raw_Id, GL.Types.Int (GL_GEOMETRY_INPUT_TYPE_EXT),
-                                    GL.Types.Int (Input_Type'Enum_Rep));
+                  GL.API.Ext.Geometry_Type'Image (GL.API.Ext.GL_GEOMETRY_INPUT_TYPE_EXT));
+      GL.API.Ext.Program_Parameter
+        (theProgram.Raw_Id,
+         GL.API.Ext.GL_GEOMETRY_INPUT_TYPE_EXT,
+                       GL.Types.Int (Input_Type'Enum_Rep));
       Raise_Exception_On_OpenGL_Error;
+
     end Set_Geometry_Input_Type;
 
     --  ----------------------------------------------------------------------------
@@ -23,19 +26,24 @@ package body GL.Ext is
     procedure Set_Geometry_Output_Type (theProgram : GL.Objects.Programs.Program;
                                        Output_Type : Connection_Mode) is
     begin
-      GL.API.Ext.Program_Parameter (theProgram.Raw_Id, GL.Types.Int (GL_GEOMETRY_OUTPUT_TYPE_EXT),
-                                    GL.Types.Int (Output_Type'Enum_Rep));
+      GL.API.Ext.Program_Parameter
+        (theProgram.Raw_Id,
+         GL.API.Ext.GL_GEOMETRY_OUTPUT_TYPE_EXT,
+                       GL.Types.Int (Output_Type'Enum_Rep));
       Raise_Exception_On_OpenGL_Error;
+
     end Set_Geometry_Output_Type;
 
     --  ----------------------------------------------------------------------------
 
    procedure Set_Geometry_Vertices_Out_Type (theProgram : GL.Objects.Programs.Program;
-                                             Output_Type : Unsigned) is
+                                             Output_Type : GL.Types.Int) is
     begin
-      GL.API.Ext.Program_Parameter (theProgram.Raw_Id, GL.Types.Int (GL_GEOMETRY_VERTICES_OUT_EXT),
-                                    GL.Types.Int (Output_Type));
+      GL.API.Ext.Program_Parameter
+        (theProgram.Raw_Id,
+         GL.API.Ext.GL_GEOMETRY_VERTICES_OUT_EXT, Output_Type);
       Raise_Exception_On_OpenGL_Error;
+
     end Set_Geometry_Vertices_Out_Type;
 
     --  ----------------------------------------------------------------------------
