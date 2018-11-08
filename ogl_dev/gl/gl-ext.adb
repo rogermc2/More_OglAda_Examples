9b28c6b9286fee9f6e3a60feb0463ef881c5b3ce
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-11-08 12:03:24 +1100

    Debugging Program_Parameter

--- ogl_dev/gl/gl-ext.adb
+++ ogl_dev/gl/gl-ext.adb
@@ -16,7 +16,7 @@ package body GL.Ext is
       GL.API.Ext.Program_Parameter
         (theProgram.Raw_Id,
          GL.API.Ext.GL_GEOMETRY_INPUT_TYPE_EXT,
-                       GL.Types.Int (Input_Type'Enum_Rep));
+         Input_Type);
       Raise_Exception_On_OpenGL_Error;
 
     end Set_Geometry_Input_Type;
@@ -28,8 +28,7 @@ package body GL.Ext is
     begin
       GL.API.Ext.Program_Parameter
         (theProgram.Raw_Id,
-         GL.API.Ext.GL_GEOMETRY_OUTPUT_TYPE_EXT,
-                       GL.Types.Int (Output_Type'Enum_Rep));
+         GL.API.Ext.GL_GEOMETRY_OUTPUT_TYPE_EXT, Output_Type);
       Raise_Exception_On_OpenGL_Error;
 
     end Set_Geometry_Output_Type;
@@ -41,7 +40,7 @@ package body GL.Ext is
     begin
       GL.API.Ext.Program_Parameter
         (theProgram.Raw_Id,
-         GL.API.Ext.GL_GEOMETRY_VERTICES_OUT_EXT, Output_Type);
+         GL.API.Ext.GL_GEOMETRY_VERTICES_OUT_EXT, Connection_Mode'Enum_Val (Output_Type));
       Raise_Exception_On_OpenGL_Error;
 
     end Set_Geometry_Vertices_Out_Type;
