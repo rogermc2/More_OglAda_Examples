997d3d4af897ff595c12dcd9588d19412418b8b2
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-11-12 17:54:19 +1100

    Fixed Set_Vertex_Attrib_Pointer stride and offset

--- ogl_dev/common/ogldev_math.ads
+++ ogl_dev/common/ogldev_math.ads
@@ -9,20 +9,12 @@ with Assimp_Types;
 package Ogldev_Math is
    use GL.Types.Singles;
 
---     package AI_2D_Package is new
---       Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector2);
---     subtype AI_2D_Map is AI_2D_Package.Map;
-
    package AI_3D_Package is new
      Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector3);
    subtype AI_3D_Map is AI_3D_Package.Map;
 
    type Perspective_Projection_Info is private;
    type Orthographic_Projection_Info is private;
-
---     package Vector3_Package is new
---       Ada.Containers.Indefinite_Ordered_Maps (UInt, Singles.Vector3);
---     type Vector3D is new Vector3_Package.Map with null Record;
 
    function Get_Orthograpic_Bottom (Info : Orthographic_Projection_Info)
                                     return Single;
