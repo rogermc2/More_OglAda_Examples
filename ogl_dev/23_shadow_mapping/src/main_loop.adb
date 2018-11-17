a805816d9d74a14ffdd1a5b89f46fccb53b55db1
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-11-17 12:21:14 +1100

    New example 23

---
+++ ogl_dev/23_shadow_mapping/src/main_loop.adb
@@ -0,0 +1,189 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GL.Low_Level.Enums;
+with GL.Objects;
+with GL.Objects.Programs;
+with GL.Objects.Shaders;
+with GL.Objects.Vertex_Arrays;
+--  with GL.Toggles;
+with GL.Types.Colors;
+with GL.Uniforms;
+with GL.Window;
+
+with Glfw;
+with Glfw.Input;
+with Glfw.Input.Keys;
+with Glfw.Windows.Context;
+
+with Program_Loader;
+with Utilities;
+
+with Ogldev_Camera;
+with Ogldev_Engine_Common;
+with Ogldev_Math;
+with Ogldev_Pipeline;
+with Ogldev_Texture;
+
+with Shadow_Map_Technique;
+with Meshes_23;
+
+procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
+   use GL.Types;
+
+   Background                  : constant GL.Types.Colors.Color := (0.7, 0.7, 0.7, 0.0);
+
+   VAO                         : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   theLighting_Technique       : Shadow_Map_Technique.Technique;
+   Game_Camera                 : Ogldev_Camera.Camera;
+   Dir_Light                   : Shadow_Map_Technique.Direct_Light;
+   Sphere_Mesh                 : Meshes_23.Mesh_23;
+   theTexture                  : Ogldev_Texture.Ogl_Texture;
+   Normal_Map                  : Ogldev_Texture.Ogl_Texture;
+   Trivial_Normal_Map          : Ogldev_Texture.Ogl_Texture;
+   Perspective_Proj_Info       : Ogldev_Math.Perspective_Projection_Info;
+   Scale                       : Single := 0.0;
+   Bump_Map_Enabled            : Boolean := True;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Init (Window : in out Glfw.Windows.Window; Result : out Boolean) is
+
+      Window_Width        : Glfw.Size;
+      Window_Height       : Glfw.Size;
+--        Position            : constant Singles.Vector3 := (0.0, 0.0, 1.0); --  Normalized by Camera.Init
+--        Target              : constant Singles.Vector3 := (0.0, 0.0, 1.0);  --  Normalized by Camera.Init
+
+      Camera_Position     : constant Singles.Vector3 := (0.5, 1.025, 0.25);
+      Target              : constant Singles.Vector3 := (0.0, -0.5, 1.0);
+      Up                  : constant Singles.Vector3 := (0.0, 1.0, 0.0);
+      Particle_System_Pos : constant GL.Types.Singles.Vector3 := (0.0, 0.0, -1.0);
+   begin
+      VAO.Initialize_Id;
+      VAO.Bind;
+      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
+      Ogldev_Math.Set_Perspective_Info
+        (Perspective_Proj_Info, 60.0, UInt (Window_Width), UInt (Window_Height),
+         1.0, 100.0);
+      Ogldev_Camera.Init_Camera (Game_Camera,
+                                 Int (Window_Width), Int (Window_Height),
+                                 Camera_Position, Target, Up);
+      Result := Shadow_Map_Technique.Init (theLighting_Technique);
+      if Result then
+         Shadow_Map_Technique.Init_Directional_Light (Dir_Light);
+         GL.Objects.Programs.Use_Program
+           (Shadow_Map_Technique.Light_Program (theLighting_Technique));
+         Shadow_Map_Technique.Set_Directional_Light
+           (theLighting_Technique, Dir_Light);
+         Shadow_Map_Technique.Set_Colour_Texture_Unit
+           (theLighting_Technique, Ogldev_Engine_Common.Colour_Texture_Unit);
+         Shadow_Map_Technique.Set_Normal_Map_Texture_Unit
+           (theLighting_Technique, Ogldev_Engine_Common.Normal_Texture_Unit);
+
+         Meshes_23.Load_Mesh (Sphere_Mesh, "../Content/box.obj");
+         if Ogldev_Texture.Init_Texture (theTexture, GL.Low_Level.Enums.Texture_2D,
+                                         "../Content/bricks.jpg") then
+            Ogldev_Texture.Load (theTexture);
+            Ogldev_Texture.Bind
+              (theTexture, Ogldev_Engine_Common.Colour_Texture_Unit);
+
+            if Ogldev_Texture.Init_Texture (Normal_Map, GL.Low_Level.Enums.Texture_2D,
+                                            "../Content/normal_map.jpg") then
+               Ogldev_Texture.Load (Normal_Map);
+               if Ogldev_Texture.Init_Texture (Trivial_Normal_Map, GL.Low_Level.Enums.Texture_2D,
+                                            "../Content/normal_up.jpg") then
+                   Ogldev_Texture.Load (Trivial_Normal_Map);
+
+                else
+                   Put_Line ("Main_Loop.Init, normal_up.jpg failed to load.");
+                end if;
+            else
+               Put_Line ("Main_Loop.Init, normal_map.jpg failed to load.");
+            end if;
+         else
+            Put_Line ("Main_Loop.Init, bricks.jpg failed to load.");
+         end if;
+      else
+         Put_Line ("Main_Loop.Init, Ogldev_Basic_Lighting failed to initialize.");
+      end if;
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Main_Loop.Init.");
+         raise;
+   end Init;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render_Scene (Window : in out Glfw.Windows.Window) is
+      use GL.Types.Singles;
+      use Ogldev_Camera;
+      Window_Width         : Glfw.Size;
+      Window_Height        : Glfw.Size;
+      Pipe                 : Ogldev_Pipeline.Pipeline;
+   begin
+      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
+      GL.Window.Set_Viewport (0, 0, GL.Types.Int (Window_Width),
+                              GL.Types.Int (Window_Height));
+
+      Ogldev_Camera.Update_Camera (Game_Camera, Window);
+
+      Utilities.Clear_Background_Colour_And_Depth (Background);
+--        GL.Toggles.Enable (GL.Toggles.Vertex_Program_Point_Size);
+
+      Shadow_Map_Technique.Use_Program (theLighting_Technique);
+
+
+      Ogldev_Pipeline.Set_Rotation (Pipe, 0.0, Scale, 0.0);
+      Ogldev_Pipeline.Set_World_Position (Pipe, 0.0, 0.0, -3.0);
+      Ogldev_Pipeline.Set_Camera (Pipe, Get_Position (Game_Camera),
+                                  Get_Target (Game_Camera), Get_Up (Game_Camera));
+      Ogldev_Pipeline.Set_Perspective_Info (Pipe, Perspective_Proj_Info);
+      Ogldev_Pipeline.Init_Transforms (Pipe);
+
+--        Utilities.Print_Matrix ("Main_Loop.Render_Scene World_Transform",
+--                                Ogldev_Pipeline.Get_World_Transform (Pipe));
+      Ogldev_Texture.Bind (theTexture, Ogldev_Engine_Common.Colour_Texture_Unit);
+      if Bump_Map_Enabled then
+         Ogldev_Texture.Bind (Normal_Map, Ogldev_Engine_Common.Normal_Texture_Unit);
+      else
+         Ogldev_Texture.Bind (Trivial_Normal_Map, Ogldev_Engine_Common.Normal_Texture_Unit);
+      end if;
+
+      Shadow_Map_Technique.Set_WVP (theLighting_Technique,
+                                     Ogldev_Pipeline.Get_WVP_Transform (Pipe));
+      Shadow_Map_Technique.Set_World_Matrix
+        (theLighting_Technique, Ogldev_Pipeline.Get_World_Transform (Pipe));
+
+--        Utilities.Print_Matrix ("Main_Loop.Render_Scene WVP_Transform",
+--                                Ogldev_Pipeline.Get_WVP_Transform (Pipe));;
+
+      Meshes_23.Render (Sphere_Mesh);
+
+   exception
+      when  others =>
+         Put_Line ("An exception occurred in Main_Loop.Render_Scene.");
+         raise;
+   end Render_Scene;
+
+   --  ------------------------------------------------------------------------
+
+   use Glfw.Input;
+   Running : Boolean;
+begin
+   Init (Main_Window, Running);
+   while Running loop
+--        delay (0.2);
+      Render_Scene (Main_Window);
+      Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
+      Glfw.Input.Poll_Events;
+      Running := Running and not
+        (Main_Window.Key_State (Glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
+      Running := Running and not Main_Window.Should_Close;
+   end loop;
+
+exception
+   when others =>
+      Put_Line ("An exception occurred in Main_Loop.");
+      raise;
+end Main_Loop;
---
+++ ogl_dev/23_shadow_mapping/src/shadow_map.adb
@@ -0,0 +1,32 @@
+--  OGLDev Tutorial 23, Shadow Mapping
+--  Ada implemented by Roger Mc Murtrie
+--  17 November 1918
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Glfw;
+with Glfw.Windows;
+
+with Initialize;
+with Main_Loop;
+
+procedure Shadow_Map is
+    Main_Window  : Glfw.Windows.Window;
+    Window_Title : constant String := "OGLDev Tutorial 23 - Sadow Mapping";
+begin
+    Glfw.Init;
+    Initialize (Main_Window, Window_Title);
+    Main_Loop (Main_Window);
+    Glfw.Shutdown;
+
+exception
+    when anError : Constraint_Error =>
+        Put ("Shadow_Map returned a constraint error: ");
+        Put_Line (Exception_Information (anError));
+
+    when anError :  others =>
+        Put_Line ("An exception occurred in Shadow_Map.");
+        Put_Line (Exception_Information (anError));
+
+end Shadow_Map;
