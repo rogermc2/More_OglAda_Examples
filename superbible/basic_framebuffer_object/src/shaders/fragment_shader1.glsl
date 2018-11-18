2dd7eaded3ea694435beea85f078c0b04b59a25b
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-11-18 14:53:02 +1100

    Example working Please enter the commit message for your changes. Lines starting

--- superbible/basic_framebuffer_object/src/shaders/fragment_shader1.glsl
+++ superbible/basic_framebuffer_object/src/shaders/fragment_shader1.glsl
@@ -10,6 +10,5 @@
 
 void main()
 {
-    //    fragment_colour = 0.5*sin(fs_in.colour * vec4(40.0, 20.0, 30.0, 1.0)) + vec4(0.5);
-    fragment_colour = vec4(1.0, 1.0, 1.0, 1.0);
+    fragment_colour = 0.5*sin(fs_in.colour * vec4(40.0, 20.0, 30.0, 1.0)) + vec4(0.5);
 }
