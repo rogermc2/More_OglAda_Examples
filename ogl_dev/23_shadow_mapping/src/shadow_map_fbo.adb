b97093b34b5cc125261b653a0a3fb046199a45a0
Roger <rogermc@grapevine.com.au>
2018-11-18 17:14:49 +1100

    Bind_For_Reading and Writing routines added

--- ogl_dev/23_shadow_mapping/src/shadow_map_fbo.adb
+++ ogl_dev/23_shadow_mapping/src/shadow_map_fbo.adb
@@ -8,6 +8,24 @@ with GL.Pixels;
 
 package body Shadow_Map_FBO is
 
+
+   procedure Bind_For_Reading (aShadow_Map : in out Shadow_Map;
+                               Tex_Unit : GL.Objects.Textures.Texture_Unit) is
+       use GL.Objects.Textures.Targets;
+   begin
+        GL.Objects.Textures.Set_Active_Unit (Tex_Unit);
+        Texture_2D.Bind (aShadow_Map.Map);
+   end Bind_For_Reading;
+
+   --  ------------------------------------------------------------------------------
+
+   procedure Bind_For_Writing (Frame_Buffer : in out GL.Objects.Framebuffers.Framebuffer) is
+   begin
+        GL.Objects.Framebuffers.Draw_Target.Bind (Frame_Buffer);
+   end Bind_For_Writing;
+
+   --  ------------------------------------------------------------------------------
+
    function Init (aShadow_Map : in out Shadow_Map;
                   Window_Width, Window_Height : GL.Types.Int) return Boolean is
       use GL.Objects.Framebuffers;
