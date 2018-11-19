71166bb84107debefcbf0ed6e1ae55541d3cb07d
Roger <rogermc@iinet.net.au.com>
2018-11-19 12:24:39 +1100

    Debugging shadow_map_fbo

--- ogl_dev/23_shadow_mapping/src/shadow_map_fbo.ads
+++ ogl_dev/23_shadow_mapping/src/shadow_map_fbo.ads
@@ -12,9 +12,8 @@ package Shadow_Map_FBO is
    procedure Bind_For_Reading (aShadow_Map : in out Shadow_Map;
                                Tex_Unit : GL.Objects.Textures.Texture_Unit);
    procedure Bind_For_Writing (aShadow_Map : in out Shadow_Map);
-   function Init (aShadow_Map : in out Shadow_Map;
-                  Window_Width, Window_Height : GL.Types.Int)
-                  return Boolean;
+   procedure Init (aShadow_Map : in out Shadow_Map;
+                  Window_Width, Window_Height : GL.Types.Int);
 
 private
 
