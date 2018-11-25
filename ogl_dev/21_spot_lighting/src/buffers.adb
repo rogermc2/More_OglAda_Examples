5da33f2ad070fd45395cc6f3d28fd48fa52d07b5
Roger <rogermc@grapevine.com.au>
2018-11-25 12:35:12 +1100

    Debugging

--- ogl_dev/21_spot_lighting/src/buffers.adb
+++ ogl_dev/21_spot_lighting/src/buffers.adb
@@ -14,7 +14,7 @@ package body Buffers is
            (0.0, 0.1, Field_Depth,          0.0, 1.0,  0.0, 1.0, 0.0),
            (Field_Width, 0.0, 0.0,          1.0, 0.0,  0.0, 1.0, 0.0),
 
-           (Field_Width, 0.0, 0.0,          1.0, 0.0,  0.0, 1.0, 0.0),
+           (Field_Width, -0.1, 0.0,          1.0, 0.0,  0.0, 1.0, 0.0),
            (0.0,  0.0, Field_Depth,         0.0, 1.0,  0.0, 1.0, 0.0),
            (Field_Width,  0.0, Field_Depth, 1.0, 1.0,  0.0, 1.0, 0.0));
 
