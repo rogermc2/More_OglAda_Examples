321642bd584953e70011c4f09f232cb0e7342838
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-11-25 18:55:34 +1100

    New example 17

---
+++ ogl_dev/17_ambient_lighting/src/buffers.ads
@@ -0,0 +1,12 @@
+
+with GL.Objects.Buffers;
+with GL.Types;
+
+package Buffers is
+
+   procedure Create_Buffers
+     (Vertex_Buffer, Index_Buffer : in out GL.Objects.Buffers.Buffer);
+
+    --  ------------------------------------------------------------------------
+
+end Buffers;
