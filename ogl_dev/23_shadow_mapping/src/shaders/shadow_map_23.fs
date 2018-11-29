9739e908004e9914ba14d29e3cf0fd5cf3c79bd7
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2018-11-30 00:18:14 +1100

    Debugging

--- ogl_dev/23_shadow_mapping/src/shaders/shadow_map_23.fs
+++ ogl_dev/23_shadow_mapping/src/shaders/shadow_map_23.fs
@@ -1,15 +1,17 @@
 #version 410 core
                                                                                     
-in vec2 TexCoordOut;                                                                
-uniform sampler2D gShadowMap;                                                       
+in vec2 TexCoordOut;
+uniform sampler2D gShadowMap;
                                                                                     
 out vec4 fragment_colour;
                                                                                     
 void main()                                                                         
 {                                                                                   
-    float Depth = texture(gShadowMap, TexCoordOut).x;                               
+    float Depth = texture(gShadowMap, TexCoordOut).x;
     Depth = 1.0 - (1.0 - Depth) * 25.0;
     //  One floating-point number in the constructor sets all components to the same value.
     // This should result in grey
-    fragment_colour = 0.8 * vec4(Depth);
+  fragment_colour = vec4(Depth);
+ //  fragment_colour = texture(gShadowMap, TexCoordOut.xy);
+    //fragment_colour = vec4(1.0,0.0,0.0,1.0);
 }
