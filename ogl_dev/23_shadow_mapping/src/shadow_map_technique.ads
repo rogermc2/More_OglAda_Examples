dc81f19587001ff9a9109fbe78866aa38dda58a6
Roger <rogermc@iinet.net.au.com>
2018-12-05 15:46:29 +1100

    Revert

--- ogl_dev/23_shadow_mapping/src/shadow_map_technique.ads
+++ ogl_dev/23_shadow_mapping/src/shadow_map_technique.ads
@@ -8,29 +8,21 @@ Package Shadow_Map_Technique is
 
    type Technique is private;
 
-   function Display_Program (theTechnique : Technique)
-                            return GL.Objects.Programs.Program;
-   function Shadow_Program (theTechnique : Technique)
-                            return GL.Objects.Programs.Program;
+   function Shadow_Map_Program (theTechnique : Technique)
+                                return GL.Objects.Programs.Program;
    procedure Init (theTechnique : out Technique);
-   procedure Set_Shadow_Map_Texture_Units (theTechnique : Technique;
-                                           Texture_Unit : GL.Types.Int);
-   procedure Set_Display_WVP (theTechnique : Technique;
-                              WVP          : GL.Types.Singles.Matrix4);
-   procedure Set_Shadow_WVP (theTechnique : Technique;
-                              WVP          : GL.Types.Singles.Matrix4);
-   procedure Use_Display_Program (theTechnique : Technique);
-   procedure Use_Shadow_Program (theTechnique : Technique);
+   procedure Set_Texture_Unit (theTechnique : Technique;
+                               Texture_Unit : GL.Types.Int);
+   procedure Set_WVP (theTechnique : Technique;
+                      WVP          : GL.Types.Singles.Matrix4);
+   procedure Use_Program (theTechnique : Technique);
 
 private
 
   type Technique is record
-      Display_Program       : GL.Objects.Programs.Program;
-      Shadow_Program        : GL.Objects.Programs.Program;
-      Display_WVP_Location  : GL.Uniforms.Uniform := 0;
-      Shadow_WVP_Location   : GL.Uniforms.Uniform := 0;
-      Display_Texture_Unit_Location : GL.Uniforms.Uniform := 0;
-      Shadow_Texture_Unit_Location  : GL.Uniforms.Uniform := 0;
+      Shadow_Map_Program    : GL.Objects.Programs.Program;
+      WVP_Location          : GL.Uniforms.Uniform := 0;
+      Texture_Unit_Location : GL.Uniforms.Uniform := 0;
    end record;
 
 end Shadow_Map_Technique;
