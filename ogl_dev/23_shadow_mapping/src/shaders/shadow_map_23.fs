425b41e1297877ba8d1bbf082ab125373a4558d5
Roger <rogermc@grapevine.com.au>
2018-12-05 15:46:29 +1100

    Revert

--- ogl_dev/23_shadow_mapping/src/shaders/shadow_map_23.fs
+++ ogl_dev/23_shadow_mapping/src/shaders/shadow_map_23.fs
@@ -1,18 +1,13 @@
-#version 410 core
+#version 330                                                                        
                                                                                     
-in vec2 TexCoordOut;
-uniform sampler2D gShadowMap;
+in vec2 TexCoordOut;                                                                
+uniform sampler2D gShadowMap;                                                       
                                                                                     
-out vec4 fragment_colour;
+out vec4 FragColor;                                                                 
                                                                                     
 void main()                                                                         
 {                                                                                   
-//    float Depth = texture(gShadowMap, TexCoordOut).x;
-//    Depth = 1.0 - (1.0 - Depth) * 25.0;
-    //  One floating-point number in the constructor sets all components to the same value.
-    // This should result in grey
-//  fragment_colour = 0.8 * vec4(Depth);
- // fragment_colour = texture(gShadowMap, TexCoordOut);
-  //  fragment_colour = texture(gShadowMap, vec2(0.0,1.0));
-   fragment_colour = vec4(1.0,1.0,0.0,1.0);
+    float Depth = texture(gShadowMap, TexCoordOut).x;                               
+    Depth = 1.0 - (1.0 - Depth) * 25.0;                                             
+    FragColor = vec4(Depth);                                                        
 }
