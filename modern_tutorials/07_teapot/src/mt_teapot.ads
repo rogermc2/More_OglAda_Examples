c32dddf5222a3bf6d9f5d9e7bdb6fc36758ca491
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-14 23:41:58 +1100

    Build_Elements qorking

--- modern_tutorials/07_teapot/src/mt_teapot.ads
+++ modern_tutorials/07_teapot/src/mt_teapot.ads
@@ -8,6 +8,9 @@ package MT_Teapot is
 
     Res_U  : constant GL.Types.Int := 10;
     Res_V  : constant GL.Types.Int := 10;
+    Element_Array_Size : GL.Types.Int
+     := 2 * 3 * Teapot_Data.Num_Patchs * (MT_Teapot.Res_U) * (MT_Teapot.Res_V);
+    subtype Element_Array is GL.Types.Int_Array (1 .. Element_Array_Size);
 
    type Vertices_Array is new
      GL.Types.Singles.Vector3_Array (1 .. Teapot_Data.Num_Patchs * Res_U * Res_V);
@@ -19,5 +22,5 @@ package MT_Teapot is
     procedure Build_CP_Elements (CP_Elements : out Teapot_CP_Elements);  --  For debugging
     procedure Build_Teapot (Vertices : out Vertices_Array;
                             Colours : out Teapot_Colours;
-                            Elements : out GL.Types.Int_Array);
+                            Elements : out Element_Array);
 end MT_Teapot;
