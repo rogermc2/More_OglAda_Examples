7441af0384d2574547b3f1675773155578473a33
Roger <rogermc@grapevine.com.au>
2018-12-15 11:26:12 +1100

    Debugging Colours buffer loading problem

--- modern_tutorials/07_teapot/src/teacup_maths.ads
+++ modern_tutorials/07_teapot/src/teacup_maths.ads
@@ -3,13 +3,15 @@ with Interfaces.C.Pointers;
 
 with GL.Types; use GL.Types;
 
+with Utilities;
+
 package Teacup_Maths is
 
-   type Vector1 is array (Size range <>) of aliased Single;
-   pragma Convention (C, Vector1);
-
-   package Vector1_Pointers is new Interfaces.C.Pointers
-     (Size, GL.Types.Single, Vector1, GL.Types.Single'Last);
+--     type Vector1 is array (Size range <>) of aliased Single;
+--     pragma Convention (C, Vector1);
+--
+--     package Vector1_Pointers is new Interfaces.C.Pointers
+--       (GL.Types.Int, GL.Types.Single, Utilities.Singles_Array, GL.Types.Single'Last);
 
     function Bernstein_Polynomial (I, N : Int; U : Single) return Single;
     function Binomial_Coefficient (I, N : Int) return Single;
