cf48147064c215a036ddc8a6b5178e078f946575
Roger <rogermc@grapevine.com.au>
2018-12-15 11:57:06 +1100

    Deleted Loader declarations

--- modern_tutorials/07_teapot/src/teacup_maths.ads
+++ modern_tutorials/07_teapot/src/teacup_maths.ads
@@ -7,12 +7,6 @@ with Utilities;
 
 package Teacup_Maths is
 
---     type Vector1 is array (Size range <>) of aliased Single;
---     pragma Convention (C, Vector1);
---
---     package Vector1_Pointers is new Interfaces.C.Pointers
---       (GL.Types.Int, GL.Types.Single, Utilities.Singles_Array, GL.Types.Single'Last);
-
     function Bernstein_Polynomial (I, N : Int; U : Single) return Single;
     function Binomial_Coefficient (I, N : Int) return Single;
     function Factorial (N : Int) return Int;
