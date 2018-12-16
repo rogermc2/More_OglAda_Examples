46fd0ffc450b014f30731da7d47884b00423674e
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-16 18:58:34 +1100

    Added exception handling to maths routines

--- modern_tutorials/07_teapot/src/teacup_maths.ads
+++ modern_tutorials/07_teapot/src/teacup_maths.ads
@@ -3,6 +3,8 @@ with GL.Types; use GL.Types;
 
 package Teacup_Maths is
 
+   Maths_Error : Exception;
+
     function Bernstein_Polynomial (I, N : Int; U : Single) return Single;
     function Binomial_Coefficient (I, N : Int) return Single;
     function Factorial (N : Int) return Int;
