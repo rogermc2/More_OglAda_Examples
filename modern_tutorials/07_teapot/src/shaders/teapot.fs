28be16a9ef3c532cdf5ef1c9267c2933f7dbcf15
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-17 17:31:01 +1100

    Fixed colour in

--- modern_tutorials/07_teapot/src/shaders/teapot.fs
+++ modern_tutorials/07_teapot/src/shaders/teapot.fs
@@ -1,10 +1,10 @@
 #version 410 core
 
-uniform vec3 f_color;
+in vec3 f_color;
 
 out vec4 FragColor;
 
 void main(void)
   {
-  FragColor = vec4(f_color.xyz, 1.0);
+  FragColor = vec4(f_color, 1.0);
   }
