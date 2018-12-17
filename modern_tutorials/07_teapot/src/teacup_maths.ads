3f894e39412689e8bb05b132be6587ac8d0a7e57
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-17 17:04:10 +1100

    Deleted Factorial function

--- modern_tutorials/07_teapot/src/teacup_maths.ads
+++ modern_tutorials/07_teapot/src/teacup_maths.ads
@@ -7,6 +7,5 @@ package Teacup_Maths is
 
     function Bernstein_Polynomial (I, N : Int; U : Single) return Single;
     function Binomial_Coefficient (I, N : Int) return Single;
-    function Factorial (N : Int) return Int;
 
 end Teacup_Maths;
