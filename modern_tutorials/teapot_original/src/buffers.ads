f950dbc438b5ff4f1117d9c2b024ea5c93d45f74
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-18 19:21:05 +1100

    Remove unwanted routines

--- modern_tutorials/teapot_original/src/buffers.ads
+++ modern_tutorials/teapot_original/src/buffers.ads
@@ -7,14 +7,8 @@ with Teapot_Data;
 
 package Buffers is
 
-    procedure Create_Colour_Buffer (Colour_Buffer : in out GL.Objects.Buffers.Buffer;
-                                    Colours : Pascal_Teapot.Colours_Array);
     procedure Create_CP_Colour_Buffer (CP_Colour_Buffer : in out GL.Objects.Buffers.Buffer;
                                        CP_Colours : Pascal_Teapot.CP_Colours_Array);
-    procedure Create_CP_Elements_Buffer (CP_IBO : in out GL.Objects.Buffers.Buffer;
-                                         CP_Indices : Pascal_Teapot.Patch_Element_Array);
-    procedure Create_Elements_Buffer (IBO : in out GL.Objects.Buffers.Buffer;
-                                      Indices : GL.Types.Int_Array);
     procedure Create_Vertex_Buffer (VBO : in out GL.Objects.Buffers.Buffer;
                                     Vertices : GL.Types.Singles.Vector3_Array);
     procedure Create_CP_Vertex_Buffer (CP_VBO : in out GL.Objects.Buffers.Buffer;
