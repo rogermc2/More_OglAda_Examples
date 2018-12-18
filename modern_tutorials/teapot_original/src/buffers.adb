b8778c00aa9ada3c282e5888e93691dc80f9575f
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-19 00:11:17 +1100

    emoved unneeded routine

--- modern_tutorials/teapot_original/src/buffers.adb
+++ modern_tutorials/teapot_original/src/buffers.adb
@@ -9,9 +9,8 @@ with Utilities;
 package body Buffers is
    use GL.Types;
 
-
    procedure Create_CP_Colour_Buffer (CP_Colour_Buffer : in out GL.Objects.Buffers.Buffer;
-                                   CP_Colours : Pascal_Teapot.CP_Colours_Array) is
+                                      CP_Colours : Pascal_Teapot.CP_Colours_Array) is
       use GL.Objects.Buffers;
    begin
       CP_Colour_Buffer.Initialize_Id;
@@ -27,23 +26,6 @@ package body Buffers is
 
    --  ------------------------------------------------------------------------
 
-   procedure Create_CP_Vertex_Buffer (CP_VBO : in out GL.Objects.Buffers.Buffer;
-                                      CP_Vertices : Teapot_Data.CP_Data) is
-      use GL.Objects.Buffers;
-   begin
-      CP_VBO.Initialize_Id;
-      Array_Buffer.Bind (CP_VBO);
-      Utilities.Load_Vertex_Buffer
-        (Array_Buffer, GL.Types.Singles.Vector3_Array (CP_Vertices), Static_Draw);
-
-   exception
-      when others =>
-         Put_Line ("An exception occurred in CP Buffers.Create_Vertex_Buffer.");
-         raise;
-   end Create_CP_Vertex_Buffer;
-
-   --  ------------------------------------------------------------------------
-
    procedure Create_Vertex_Buffer (VBO      : in out GL.Objects.Buffers.Buffer;
                                    Vertices : GL.Types.Singles.Vector3_Array) is
       use GL.Objects.Buffers;
@@ -51,7 +33,7 @@ package body Buffers is
       VBO.Initialize_Id;
       Array_Buffer.Bind (VBO);
       Utilities.Load_Vertex_Buffer
-          (Array_Buffer,  Singles.Vector3_Array (Vertices), Static_Draw);
+          (Array_Buffer, Singles.Vector3_Array (Vertices), Static_Draw);
 
    exception
       when others =>
