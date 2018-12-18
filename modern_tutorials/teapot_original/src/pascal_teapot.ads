a3275e1f2041e2d8865c9dcaf187610d64c5e66e
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-18 23:01:47 +1100

    Removed more unneeded code

--- modern_tutorials/teapot_original/src/pascal_teapot.ads
+++ modern_tutorials/teapot_original/src/pascal_teapot.ads
@@ -6,31 +6,16 @@ with Teapot_Data;
 package Pascal_Teapot is
     use GL.Types;
 
-    Res_U  : constant Int := 10;
-    Res_V  : constant Int := 10;
-
-    Patch_Size : constant Int := Teapot_Data.Patchs'Length;
-    Element_Array_Size : Int
-     := 2 * 3 * Teapot_Data.Patchs'Length * (Pascal_Teapot.Res_U) * (Pascal_Teapot.Res_V);
-   subtype Element_Array is GL.Types.Int_Array (1 .. Element_Array_Size);
-
-   type CP_Element_Array_Type is array
-    (Int range <>, Int range <>) of Int;
-   type CP_Element_Array is new CP_Element_Array_Type
-     (1 .. Teapot_Data.Bezier_Patch'Length,
-      1 .. Teapot_Data.Bezier_Patch'Length (2));
-
-   type Patch_Element_Array is array (Int range 1 .. Teapot_Data.Num_Patchs) of CP_Element_Array;
+   Patch_Size : constant Int := Teapot_Data.Patchs'Length;
 
    type Vertices_Array is new
-     Singles.Vector3_Array (1 .. Patch_Size * Res_U * Res_V);
+     Singles.Vector3_Array (1 .. Patch_Size);
    type Colours_Array is new
-     Single_Array (1 .. 3 * Teapot_Data.Num_Patchs * Res_U * Res_V);
+     Single_Array (1 .. 3 * Teapot_Data.Num_Patchs);
    type CP_Colours_Array is array
      (Int range 1 .. 3 * Teapot_Data.Num_Control_Points) of GL.Types.Single;
 
-    procedure Build_CP_Colours (CP_Colours : out CP_Colours_Array);
-    procedure Build_CP_Elements (CP_Elements : out Patch_Element_Array);  --  For debugging
+   procedure Build_CP_Colours (CP_Colours : out CP_Colours_Array);
    function Build_Teapot (Patchs : Teapot_Data.Patch_Data; Num_Steps : Int)
                           return Singles.Vector3_Array;
 end Pascal_Teapot;
