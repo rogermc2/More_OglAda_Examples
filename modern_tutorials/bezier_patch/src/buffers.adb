e358a0f2bffa7c281289a31254c38409c5cbea0a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-12-25 17:36:27 +1100

    New example bezier patch

---
+++ modern_tutorials/bezier_patch/src/buffers.adb
@@ -0,0 +1,27 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Utilities;
+
+package body Buffers is
+   use GL.Types;
+
+   procedure Create_Vertex_Buffer (VBO      : in out GL.Objects.Buffers.Buffer;
+                                   Vertices : GL.Types.Singles.Vector3_Array) is
+      use GL.Objects.Buffers;
+   begin
+      VBO.Initialize_Id;
+      Array_Buffer.Bind (VBO);
+      Utilities.Load_Vertex_Buffer
+          (Array_Buffer, Vertices, Static_Draw);
+--        Utilities.Print_GL_Array3 ("Create_Vertex_Buffer, Vertices", Vertices);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Buffers.Create_Vertex_Buffers.");
+         raise;
+   end Create_Vertex_Buffer;
+
+   --  ------------------------------------------------------------------------
+
+end Buffers;
