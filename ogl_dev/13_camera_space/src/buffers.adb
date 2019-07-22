4af36f19e7dba4063f4f52e3b92fa644d764817f
Roger Mc Murtrie <rogermc@iinet.net.au.com>
2019-07-22 23:42:25 +1000

    Working

--- ogl_dev/13_camera_space/src/buffers.adb
+++ ogl_dev/13_camera_space/src/buffers.adb
@@ -7,15 +7,15 @@ package body Buffers is
     procedure Create_Vertex_Buffer (VBO : in out GL.Objects.Buffers.Buffer) is
     use GL.Objects.Buffers;
     use GL.Types;
-        Vertices : constant Maths.Vector5_Array :=
-         ((-1.0, -1.0, 0.5773,   0.0, 0.0),
-           (0.0, -1.0, -1.15475, 0.5, 0.0),
-           (1.0, -1.0, 0.5773,   1.0, 0.0),
-           (0.0,  1.0, 0.0,      0.5, 1.0));
+        Vertices : constant GL.Types.Singles.Vector3_Array :=
+         ((-1.0, -1.0, 0.5773),
+           (0.0, -1.0, -1.15475),
+           (1.0, -1.0, 0.5773),
+           (0.0,  1.0, 0.0));
     begin
         VBO.Initialize_Id;
         Array_Buffer.Bind (VBO);
-        Utilities.Load_Vector5_Buffer (Array_Buffer, Vertices, Static_Draw);
+        Utilities.Load_Vertex_Buffer (Array_Buffer, Vertices, Static_Draw);
     end Create_Vertex_Buffer;
 
     --  ------------------------------------------------------------------------
