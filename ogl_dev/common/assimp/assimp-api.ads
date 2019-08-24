79eb4367a0007874bb3d2bd803a23d6fc17a6465
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-08-24 16:37:40 +1000

    Cleanup

--- ogl_dev/common/assimp/assimp-api.ads
+++ ogl_dev/common/assimp/assimp-api.ads
@@ -5,33 +5,6 @@ with Scene;
 
 package Assimp.API is
 
-   --  GetTexture (aiTextureType type, unsigned int index, aiString *path,
-   --  aiTextureMapping *mapping=NULL, unsigned int *uvindex=NULL,
-   --  float *blend=NULL, aiTextureOp *op=NULL, aiTextureMapMode *mapmode=NULL) const
---     function Get_Material_Texture1 (aMaterial : Material.API_Material;
---                                    Tex_Type : Material.AI_Texture_Type;
---                                    Index : Interfaces.C.unsigned;
---                                    Path : access Assimp_Types.API_String)
---                                    return Assimp_Types.API_Return;
---     pragma Import (C, Get_Material_Texture1, "aiGetMaterialTexture");
-
---     function Get_Material_Texture (aMaterial : access Material.API_Material;
---                                    Tex_Type : Material.AI_Texture_Type;
---                                    Index : Interfaces.C.unsigned;
---                                    Path : access Assimp_Types.API_String := null;
---                                    Mapping : access Material.AI_Texture_Mapping := null;
---                                    UV_Index : access Interfaces.C.unsigned := null;
---                                    Blend : access Interfaces.C.C_float := null;
---                                    Op : access Material.AI_Texture_Op := null;
---                                    Map_Mode : access Material.AI_Texture_Map_Mode := null)
---                                    return Assimp_Types.API_Return;
---     pragma Import (C, Get_Material_Texture, "aiGetMaterialTexture");
-
---     function Get_Material_Texture_Count (aMaterial : Material.API_Material;
---                                          Tex_Type : Material.AI_Texture_Type)
---                                          return Interfaces.C.unsigned;
---     pragma Import (C, Get_Material_Texture_Count, "aiGetMaterialTextureCount");
-
    function Import_File (File_Name : Interfaces.C.char_array;
                          Flags : Interfaces.C.unsigned) return access Scene.API_Scene;
    pragma Import (C, Import_File, "aiImportFile");
@@ -39,9 +12,5 @@ package Assimp.API is
    function Read_File (File_Name : Interfaces.C.char_array;
                        Flags : Interfaces.C.unsigned) return access Scene.API_Scene;
    pragma Import (C, Read_File, "aiReadFile");
-
---     procedure Init_Mesh (theMesh : in out Assimp_Mesh.Mesh_Array_Pointer;
---                          Mesh_Index : Interfaces.C.unsigned);
---     pragma Import (C, Init_Mesh, "aiInitMesh");
 
 end Assimp.API;
