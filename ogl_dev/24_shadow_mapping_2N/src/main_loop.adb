d32225b4bab29e8332ea69854a403b96336b86e9
rogermc2 <rogermc@iinet.net.au>
2019-09-03 10:47:41 +1000

    Removed uneeded code

--- ogl_dev/24_shadow_mapping_2N/src/main_loop.adb
+++ ogl_dev/24_shadow_mapping_2N/src/main_loop.adb
@@ -179,9 +179,6 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
       Lighting_Technique_24N.Set_Light_WVP_Location (Lighting_Technique,
                                                      Ogldev_Pipeline.Get_WVP_Transform (Pipe));
 
---        Shadow_Map_Technique.Use_Program (Shadow_Technique);
---        Shadow_Map_Technique.Set_WVP (Shadow_Technique,
---                                      Ogldev_Pipeline.Get_WVP_Transform (Pipe));
       Ogldev_Texture.Bind (Ground_Texture, 0);
       Meshes_24N.Render (Quad_Mesh);
 
