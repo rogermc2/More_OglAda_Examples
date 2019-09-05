e49afc1d4e532bef1ee7a97a88a0c57134f500f0
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-09-05 23:38:09 +1000

    Parameter adjustment

--- ogl_dev/22_loading_models/src/main_loop.adb
+++ ogl_dev/22_loading_models/src/main_loop.adb
@@ -46,7 +46,7 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
 
       Window_Width        : Glfw.Size;
       Window_Height       : Glfw.Size;
-      Camera_Position     : constant Singles.Vector3 := (3.0, 7.0, 10.0); --  Normalized by Camera.Init
+      Camera_Position     : constant Singles.Vector3 := (0.0, 0.0, -10.0); --  Normalized by Camera.Init
       Target_Position     : constant Singles.Vector3 := (0.0, -0.2, -1.0);  --  Normalized by Camera.Init
       Up                  : constant Singles.Vector3 := (0.0, 1.0, 0.0);
    begin
