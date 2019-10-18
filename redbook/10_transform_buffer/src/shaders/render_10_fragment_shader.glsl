96a6b069ca568934b3647193012bbe32411556da
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-10-18 16:05:45 +1100

    Coded

--- redbook/10_transform_buffer/src/shaders/render_10_fragment_shader.glsl
+++ redbook/10_transform_buffer/src/shaders/render_10_fragment_shader.glsl
@@ -1,12 +1,12 @@
 #version 410 core
 
-layout (location = 0) out vec4 color;
+layout (location = 0) out vec4 colour;
 
 in vec3 vs_fs_normal;
 
-uniform vec4 pass_color;
+uniform vec4 pass_colour;
 
 void main(void)
     {
-    color = pass_color * (0.2 + pow(abs(vs_normal.z), 4.0)) + vec4(1.0, 1.0, 1.0, 0.0) * pow(abs(vs_normal.z), 37.0);
+    colour = pass_colour * (0.2 + pow(abs(vs_normal.z), 4.0)) + vec4(1.0, 1.0, 1.0, 0.0) * pow(abs(vs_normal.z), 37.0);
     }
