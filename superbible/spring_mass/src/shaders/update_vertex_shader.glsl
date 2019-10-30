f229c6ba46acbe16c43c15c09ea7de7b8730c226
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-10-30 14:52:00 +1100

    Cosmetic

--- superbible/spring_mass/src/shaders/update_vertex_shader.glsl
+++ superbible/spring_mass/src/shaders/update_vertex_shader.glsl
@@ -22,27 +22,33 @@ uniform float spring_constant = 7.1;
 // Gravity
 const vec3 gravity = vec3(0.0, -0.08, 0.0);
 
-// Global damping constant
-uniform float c = 2.8;
+// Global damping constant c
+uniform float damping_constant = 2.8;
 uniform float rest_length = 0.88;
 
 void main(void)
 {
-    vec3 p = position_mass.xyz;    // p can be our position
-    float mass = position_mass.w;     // m is the mass of our vertex
-    vec3 u = velocity;             // u is the initial velocity
-    vec3 force = gravity * mass - c * u;  // F is the force on the mass
+    vec3 vertex_pos = position_mass.xyz;
+    vec3 other_vertex_pos;
+    vec3 delta_pos;
+    vec3 displacement;
+    float x;
+    vec3 acc;
+    float mass = position_mass.w;     // mass of the vertex
+    vec3 initial_velocity = velocity;
+    vec3 final_vel;
+    // F is the force on the mass
+    vec3 force = gravity * mass - damping_constant * initial_velocity;
     bool fixed_node = true;        // Becomes false when force is applied
 
     for (int i = 0; i < 4; i++)
         {
         if (connection[i] != -1)
             {
-            // q is the position of the other vertex
-            vec3 q = texelFetch(tex_position, connection[i]).xyz;
-            vec3 d = q - p;
-            float x = length(d);
-            force = force -spring_constant * (rest_length - x) * normalize(d);
+            other_vertex_pos = texelFetch(tex_position, connection[i]).xyz;
+            delta_pos = other_vertex_pos - vertex_pos;
+            x = length(delta_pos);
+            force = force - spring_constant * (rest_length - x) * normalize(delta_pos);
             fixed_node = false;
             }
         }
@@ -52,20 +58,15 @@ void main(void)
         {
         force = vec3(0.0);
         }
-
     // Accelleration due to force
-    vec3 acc = force / mass;
-
-    // Displacement
-    vec3 s = u * time_step + 0.5 * acc * time_step * time_step;
-
-    // Final velocity
-    vec3 v = u + acc * time_step;
+    acc = force / mass;
+    displacement = initial_velocity * time_step + 0.5 * acc * time_step * time_step;
+    final_vel = initial_velocity + acc * time_step;
 
     // Constrain the absolute value of the displacement per step
-    s = clamp(s, vec3(-25.0), vec3(25.0));
+    displacement = clamp(displacement, vec3(-25.0), vec3(25.0));
 
     // Write the outputs
-    tf_position_mass = vec4(p + s, m);
-    tf_velocity = v;
+    tf_position_mass = vec4(vertex_pos + displacement, mass);
+    tf_velocity = final_vel;
 }
