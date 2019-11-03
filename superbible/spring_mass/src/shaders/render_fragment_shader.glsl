fbd5a135ba3b01d01d432a33f543529cf4862bca
rogermc2 <rogermc@iinet.net.au>
2019-11-03 11:43:53 +1100

    Added point colouring

--- superbible/spring_mass/src/shaders/render_fragment_shader.glsl
+++ superbible/spring_mass/src/shaders/render_fragment_shader.glsl
@@ -1,8 +1,10 @@
 #version 410 core
 
+layout (location = 0) in vec3 v_position;
+
 layout (location = 0) out vec4 colour;
 
 void main(void)
 {
-    colour = vec4(1.0);
+    colour = vec4(v_position[0], 0.8, v_position[1], 1.0);
 }
