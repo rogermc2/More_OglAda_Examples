c98a1f3ffe526b2d42fba3d7f99d46d1910b0265
rogermc2 <rogermc@iinet.net.au>
2019-11-03 16:00:48 +1100

    Points working - cleanup

--- superbible/spring_mass/src/shaders/update_vertex_shader.glsl
+++ superbible/spring_mass/src/shaders/update_vertex_shader.glsl
@@ -1,7 +1,7 @@
 #version 410 core
 
-// The position_mass vector contains the vertex position in xyz and the
-// mass of the vertex in w
+// The position_mass vector contains the vertex position in xyz
+// and the mass of the vertex in w
 layout (location = 0) in vec4 position_mass;
 layout (location = 1) in vec3 velocity;
 layout (location = 2) in ivec4 connection;
@@ -13,7 +13,7 @@ uniform samplerBuffer tex_position;
 out vec4 tf_position_mass;
 out vec3 tf_velocity;
 
-uniform float time_step;  //  0.07
+uniform float time_step;
 uniform float spring_constant = 7.1;
 
 const vec3 gravity = vec3(0.0, -0.08, 0.0);
@@ -26,29 +26,19 @@ void main(void)
     vec3 vertex_pos = position_mass.xyz;
     float mass = position_mass.w;
     vec3 initial_velocity = velocity;
-    vec3 other_vertex_pos;
     vec3 delta_pos;
     vec3 displacement;
-    float spring_length;
     vec3 acc;
     vec3 final_vel;
     vec3 force = gravity * mass - damping_constant * initial_velocity;
-    bool fixed_node = true;        // Becomes false when force is applied
 
     for (int i = 0; i < 4; i++)
         {
         if (connection[i] != -1)
             {
-            other_vertex_pos = texelFetch(tex_position, connection[i]).xyz;
-            delta_pos = other_vertex_pos - vertex_pos;
-            spring_length = length(delta_pos);
-            force = force - spring_constant * (rest_length - spring_length) * normalize(delta_pos);
-            fixed_node = false;
+            delta_pos = texelFetch(tex_position, connection[i]).xyz - vertex_pos;
+            force = force + spring_constant * (length(delta_pos) - rest_length) * normalize(delta_pos);
             }
-        }
-    if (fixed_node)
-        {
-        force = vec3(0.0);
         }
  
     acc = force / mass;
