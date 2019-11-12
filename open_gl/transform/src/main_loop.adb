066879f1d167f0f3796884b6ecb9ea7f517cc6c1
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-11-12 18:12:54 +1100

    Coding

--- open_gl/transform/src/main_loop.adb
+++ open_gl/transform/src/main_loop.adb
@@ -2,87 +2,139 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GL.Attributes;
+with GL.Low_Level.Enums;
 with GL.Objects.Buffers;
 with GL.Objects.Programs;
 with GL.Objects.Shaders;
+with GL.Objects.Textures;
+--  with GL.Objects.Textures.Targets;
 with GL.Objects.Vertex_Arrays;
 with GL.Types;
+with GL.Uniforms;
 
 with Glfw.Input;
 with Glfw.Input.Keys;
 with Glfw.Windows.Context;
 
+with Maths;
 with Program_Loader;
 with Utilities;
 with Vertex_Data;
 
+with Ogldev_Texture;
+
 procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
-    Rendering_Program     : GL.Objects.Programs.Program;
-    Vertex_Array          : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
-    Vertex_Buffer         : GL.Objects.Buffers.Buffer;
-    Position_Location     : GL.Attributes.Attribute;
+   Shader_Program      : GL.Objects.Programs.Program;
+   Vertex_Array        : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   Vertex_Buffer       : GL.Objects.Buffers.Buffer;
+   Element_Buffer      : GL.Objects.Buffers.Buffer;
+   Textures            : array (1 .. 2) of Ogldev_Texture.Ogl_Texture;
+   Projection_Location : GL.Uniforms.Uniform;
+   Transform_Location  : GL.Uniforms.Uniform;
+   View_Location       : GL.Uniforms.Uniform;
+   Projection_Matrix   : GL.Types.Singles.Matrix4;
+   View_Matrix         : GL.Types.Singles.Matrix4;
 
-    --  ------------------------------------------------------------------------
+   --  ------------------------------------------------------------------------
 
-    procedure Render is
-         use GL.Types;
-    begin
-        Rendering_Program.Use_Program;
-        GL.Objects.Vertex_Arrays.Draw_Arrays (GL.Types.Triangles, 0, 3);
-    exception
-        when others =>
-            Put_Line ("An exception occurred in Render.");
-            raise;
-    end Render;
+   procedure Render is
+      use GL.Types;
+   begin
+      Shader_Program.Use_Program;
+      GL.Objects.Vertex_Arrays.Draw_Arrays (GL.Types.Triangles, 0, 3);
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Render.");
+         raise;
+   end Render;
 
-    --  ------------------------------------------------------------------------
+   --  ------------------------------------------------------------------------
 
-    procedure Setup_Graphic is
-        use GL.Objects.Buffers;
-        use GL.Objects.Shaders;
-        use GL.Types;
-        use Program_Loader;
-    begin
-        Vertex_Array.Initialize_Id;
-        Vertex_Array.Bind;
+   procedure Setup is
+      use GL.Objects.Buffers;
+      use GL.Objects.Shaders;
+--        use GL.Objects.Textures.Targets;
+      use Program_Loader;
+   begin
+      Vertex_Array.Initialize_Id;
+      Vertex_Array.Bind;
 
-        Vertex_Buffer.Initialize_Id;
-        Array_Buffer.Bind (Vertex_Buffer);
-        Utilities.Load_Vertex_Buffer (Array_Buffer, Vertex_Data.Vertices, Static_Draw);
+      Vertex_Buffer.Initialize_Id;
+      Array_Buffer.Bind (Vertex_Buffer);
+      Utilities.Load_Vector8_Buffer
+        (Array_Buffer, Vertex_Data.Vertices, Static_Draw);
 
-        Rendering_Program := Program_From
-          ((Src ("src/shaders/vertex_shader.glsl", Vertex_Shader),
-           Src ("src/shaders/fragment_shader.glsl", Fragment_Shader)));
+      Element_Buffer.Initialize_Id;
+      Element_Array_Buffer.Bind (Element_Buffer);
+      Utilities.Load_Element_Buffer
+        (Element_Array_Buffer, Vertex_Data.Elements, Static_Draw);
 
-        Position_Location := GL.Objects.Programs.Attrib_Location
-          (Rendering_Program, "position");
-        GL.Attributes.Set_Vertex_Attrib_Pointer
-          (Position_Location, 2, GL.Types.Single_Type, True, 0, 0);
-        GL.Attributes.Enable_Vertex_Attrib_Array (Position_Location);
+      Shader_Program := Program_From
+        ((Src ("src/shaders/transform_vertex_shader.glsl", Vertex_Shader),
+         Src ("src/shaders/transform_fragment_shader.glsl", Fragment_Shader)));
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in Setup_Graphic.");
-            raise;
-    end Setup_Graphic;
+      GL.Objects.Programs.Use_Program (Shader_Program);
+      Transform_Location := GL.Objects.Programs.Uniform_Location
+        (Shader_Program, "trans");
+      View_Location := GL.Objects.Programs.Uniform_Location
+        (Shader_Program, "view");
+      Projection_Location := GL.Objects.Programs.Uniform_Location
+        (Shader_Program, "proj");
 
-    --  ------------------------------------------------------------------------
+      Maths.Init_Lookat_Transform (Position => (1.2, 1.2, 1.2),
+                                   Target   => (0.0, 0.0, 0.0),
+                                   Up       => (0.0, 0.0, 1.0),
+                                   Look_At  => View_Matrix);
+      Maths.Init_Perspective_Transform (View_Angle => Maths.Degrees (45.0),
+                                        Width      => 800.0,
+                                        Height     => 600.0,
+                                        Z_Near     => 1.0,
+                                        Z_Far      => 10.0,
+                                        Transform  => Projection_Matrix);
 
-    use Glfw.Input;
-    Running : Boolean := True;
+      GL.Uniforms.Set_Single (View_Location, View_Matrix);
+      GL.Uniforms.Set_Single (Projection_Location, Projection_Matrix);
+
+      GL.Objects.Textures.Set_Active_Unit (0);
+--        for index in 1 .. 2 loop
+--           Textures (index).Initialize_Id;
+--           Texture_2D.Bind (Textures (index));
+--        end loop;
+
+
+         if  Ogldev_Texture.Init_Texture (Textures (1), GL.Low_Level.Enums.Texture_2D,
+                                          "../content/sample.png") then
+            Ogldev_Texture.Load (Textures (1));
+         else
+            Put_Line ("Main_Loop.Init sample.png failed to load");
+         end if;
+      GL.Attributes.Set_Vertex_Attrib_Pointer
+        (Transform_Location, 2, GL.Types.Single_Type, True, 0, 0);
+      GL.Attributes.Enable_Vertex_Attrib_Array (Position_Location);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Setup.");
+         raise;
+   end Setup;
+
+   --  ------------------------------------------------------------------------
+
+   use Glfw.Input;
+   Running : Boolean := True;
 begin
-    Setup_Graphic;
-    while Running loop
-        Render;
-        Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
-        Glfw.Input.Poll_Events;
-        Running := Running and not
-          (Main_Window.Key_State (Glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
-        Running := Running and not Main_Window.Should_Close;
-    end loop;
+   Setup;
+   while Running loop
+      Render;
+      Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
+      Glfw.Input.Poll_Events;
+      Running := Running and not
+        (Main_Window.Key_State (Glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
+      Running := Running and not Main_Window.Should_Close;
+   end loop;
 exception
-    when others =>
-        Put_Line ("An exception occurred in Main_Loop.");
-        raise;
+   when others =>
+      Put_Line ("An exception occurred in Main_Loop.");
+      raise;
 end Main_Loop;
