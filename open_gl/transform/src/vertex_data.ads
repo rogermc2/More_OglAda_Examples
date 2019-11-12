b1808ae58f7acaf07e3925a7c36aac1d6b5078ad
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-11-12 18:12:54 +1100

    Coding

--- open_gl/transform/src/vertex_data.ads
+++ open_gl/transform/src/vertex_data.ads
@@ -1,10 +1,19 @@
 
 with GL.Types;
+with Maths;
 
 package Vertex_Data is
-    use GL.Types;
+   use  GL.Types;
 
-    Vertices  : constant Singles.Vector3_Array (1 .. 2) :=
-                       ((0.0, 0.5, 0.5),
-                       (-0.5, -0.5, -0.5));
+   Vertices  : constant Maths.Vector8_Array (1 .. 4) :=
+                 --  Position        Color             Texcoords
+                 ((-0.5, 0.5, 1.0,  0.0, 0.0, 0.0,  0.0, 0.0),   -- Top-left
+                  (0.5, 0.5, 0.0,   1.0, 0.0, 1.0,  0.0, 0.0),   --  Top-right
+                  (0.5, -0.5, 0.0,  0.0, 1.0, 1.0,  1.0, 1.0),   --  Bottom-right
+                  (-0.5, -0.5, 1.0, 1.0, 1.0, 0.0,  1.0, 1.0));  --  Bottom-left);
+
+   Elements : constant Int_Array (1 .. 6) :=
+                (0, 1, 2,
+                 2, 3, 0);
+
 end Vertex_Data;
