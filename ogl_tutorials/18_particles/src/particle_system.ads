878fb6c61d125c26390ffdd4252e8f7f09d2f578
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-11-12 12:54:09 +1100

    Deleted unused procedure

--- ogl_tutorials/18_particles/src/particle_system.ads
+++ ogl_tutorials/18_particles/src/particle_system.ads
@@ -5,7 +5,6 @@ package Particle_System  is
 
    procedure Init;
    procedure Render_Particles;
-   procedure Set_Texture_ID (Tex : Singles.Vector3);
    procedure Set_IDs (VP : Singles.Matrix4);
    procedure Update_Particles;
 
