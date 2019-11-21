0b6e3675c091add00dbc887dc4b6e98bedbb46a7
rogermc2 <rogermc@iinet.net.au>
2019-11-21 20:58:45 +1100

    Debugging

--- ogl_dev/28_feedback/src/random_texture.ads
+++ ogl_dev/28_feedback/src/random_texture.ads
@@ -5,7 +5,7 @@ with GL.Types;
 package Random_Texture is
 
    procedure Init_Random_Texture (aTexture : in out GL.Objects.Textures.Texture;
-                                  Size : GL.Types.UInt);
+                                  Texture_Length : GL.Types.UInt);
    procedure  Bind (aTexture : in out GL.Objects.Textures.Texture;
                     Texture_Unit : GL.Objects.Textures.Texture_Unit);
 
