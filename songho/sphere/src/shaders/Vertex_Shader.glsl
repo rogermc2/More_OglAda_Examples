ba876e24ba0ae733ca734c72e1756666b3665770
Roger Mc Murtrie <rogermc@grapevine.com.au>
2019-12-31 23:30:52 +1100

    songho/sphere project added

---
+++ songho/sphere/src/shaders/Vertex_Shader.glsl
@@ -0,0 +1,24 @@
+#version 410 core
+
+// blinn shading with texture
+
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexNormal;
+layout(location = 2) in vec2 vertexTexCoord;
+
+out vec3 esVertex;
+out vec3 esNormal;
+out vec2 texCoord0;
+
+uniform mat4 matrixModelView;
+uniform mat4 matrixNormal;
+uniform mat4 matrixModelViewProjection;
+
+void main()
+{
+    esVertex = vec3(matrixModelView * vec4(vertexPosition, 1.0));
+    esNormal = vec3(matrixNormal * vec4(vertexNormal, 1.0));
+    texCoord0 = vertexTexCoord;
+    gl_Position = matrixModelViewProjection * vec4(vertexPosition, 1.0);
+}
+
