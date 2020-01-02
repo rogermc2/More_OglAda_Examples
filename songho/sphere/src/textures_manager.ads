8b620fdc10fdf6e85ef9e2e0d348fc683845f163
rogermc2 <rogermc@iinet.net.au>
2020-01-02 17:49:25 +1100

    Coding

--- songho/sphere/src/textures_manager.ads
+++ songho/sphere/src/textures_manager.ads
@@ -4,7 +4,7 @@ with GL.Objects.Textures;
 package Textures_Manager is
 
    procedure Load_Texture (aTexture : in out GL.Objects.Textures.Texture;
-                           Image_File_Name : String);
+                           Image_File_Name : String; Wrap : Boolean);
 
    Image_Error : Exception;
 
