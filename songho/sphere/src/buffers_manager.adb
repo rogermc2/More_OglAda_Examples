dcb030cd414d65a2879a197bca09e89dd82837fd
rogermc2 <rogermc@iinet.net.au>
2020-01-04 17:07:15 +1100

    Refinement

--- songho/sphere/src/buffers_manager.adb
+++ songho/sphere/src/buffers_manager.adb
@@ -1,5 +1,4 @@
 
-with Maths;
 with Utilities;
 
 package body Buffers_Manager is
@@ -7,18 +6,15 @@ package body Buffers_Manager is
     procedure Create_Vertex_Buffers (VBO_1, VBO_2 : in out GL.Objects.Buffers.Buffer;
                                      Sphere_1, Sphere_2 : Sphere.Sphere) is
     use GL.Objects.Buffers;
-        Vertices_1 : Maths.Vector8_Array (1 .. Sphere.Get_Interleaved_Size (Sphere_1));
-        Vertices_2 : Maths.Vector8_Array (1 .. Sphere.Get_Interleaved_Size (Sphere_2));
-    begin
+     begin
         VBO_1.Initialize_Id;
         Array_Buffer.Bind (VBO_1);
-        Vertices_1 := Sphere.Get_Interleaved_Vertices (Sphere_1);
-        Utilities.Load_Vector8_Buffer (Array_Buffer, Vertices_1, Static_Draw);
-
+        Utilities.Load_Vector8_Buffer (Array_Buffer,
+                                       Sphere.Get_Interleaved_Vertices (Sphere_1), Static_Draw);
         VBO_2.Initialize_Id;
         Array_Buffer.Bind (VBO_2);
-        Vertices_2 := Sphere.Get_Interleaved_Vertices (Sphere_2);
-        Utilities.Load_Vector8_Buffer (Array_Buffer, Vertices_2, Static_Draw);
+        Utilities.Load_Vector8_Buffer (Array_Buffer,
+                                       Sphere.Get_Interleaved_Vertices (Sphere_2), Static_Draw);
     end Create_Vertex_Buffers;
 
     --  ------------------------------------------------------------------------
@@ -26,19 +22,15 @@ package body Buffers_Manager is
   procedure Create_Index_Buffers (IBO_1, IBO_2 : in out GL.Objects.Buffers.Buffer;
                                   Sphere_1, Sphere_2 : Sphere.Sphere) is
     use GL.Objects.Buffers;
-    use GL.Types;
-        Indices_1 : Int_Array (1 .. Sphere.Get_Indices_Size (Sphere_1));
-        Indices_2 : Int_Array (1 .. Sphere.Get_Indices_Size (Sphere_2));
     begin
         IBO_1.Initialize_Id;
         Element_Array_Buffer.Bind (IBO_1);
-        Indices_1 := Sphere.Get_Indices (Sphere_1);
-        Utilities.Load_Element_Buffer (Element_Array_Buffer, Indices_1, Static_Draw);
-
+        Utilities.Load_Element_Buffer (Element_Array_Buffer,
+                                       Sphere.Get_Indices (Sphere_1), Static_Draw);
         IBO_2.Initialize_Id;
         Element_Array_Buffer.Bind (IBO_2);
-        Indices_2 := Sphere.Get_Indices (Sphere_2);
-        Utilities.Load_Element_Buffer (Element_Array_Buffer, Indices_2, Static_Draw);
+        Utilities.Load_Element_Buffer (Element_Array_Buffer,
+                                       Sphere.Get_Indices (Sphere_2), Static_Draw);
 
     end Create_Index_Buffers;
 
