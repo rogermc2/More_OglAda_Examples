ff1345f6f460cac70ac49923c69604595ef20d45
rogermc2 <rogermc@iinet.net.au>
2020-01-07 10:58:00 +1100

    Changed Load_Buffers parameters to indexed

--- ogl_tutorials/16_shadow_mapping/src/buffers_manager.ads
+++ ogl_tutorials/16_shadow_mapping/src/buffers_manager.ads
@@ -5,9 +5,9 @@ with GL.Types;
 
 package Buffers_Manager is
 
-    procedure Load_Buffers (Normals_Buffer : in out GL.Objects.Buffers.Buffer;
-                            UVs_Buffer : in out GL.Objects.Buffers.Buffer;
-                            Vertex_Buffer : in out GL.Objects.Buffers.Buffer;
+    procedure Load_Buffers (Indexed_Vertices_Buffer : in out GL.Objects.Buffers.Buffer;
+                            Indexed_UVs_Buffer : in out GL.Objects.Buffers.Buffer;
+                            Indexed_Normals_Buffer : in out GL.Objects.Buffers.Buffer;
                             Element_Buffer : in out GL.Objects.Buffers.Buffer;
                             Indices_Size : out GL.Types.Int);
 
