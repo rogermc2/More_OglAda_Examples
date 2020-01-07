0921098973980900ba18cd4c7e4dd654e2fc47c2
rogermc2 <rogermc@iinet.net.au>
2020-01-07 13:47:15 +1100

    New package textures_manager

---
+++ ogl_tutorials/16_shadow_mapping/src/textures_manager.ads
@@ -0,0 +1,15 @@
+
+with GL.Objects.Textures;
+with GL.Objects.Framebuffers;
+
+package Textures_Manager is
+
+   procedure Init (Frame_Buffer : in out GL.Objects.Framebuffers.Framebuffer;
+                   Depth_Texture : in out GL.Objects.Textures.Texture);
+   procedure Load_Texture (Frame_Buffer : in out GL.Objects.Framebuffers.Framebuffer;
+                           Image_File_Name : String);
+
+   Image_Error : Exception;
+   Framebuffer_Error : Exception;
+
+end Textures_Manager;
