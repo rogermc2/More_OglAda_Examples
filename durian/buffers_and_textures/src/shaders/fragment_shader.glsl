cff827d3f3dcc4a56fe1aa183d95f916c7d18ef3
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-06-21 23:11:17 +1000

    Replace SOIL with opengl-images

--- durian/buffers_and_textures/src/shaders/fragment_shader.glsl
+++ durian/buffers_and_textures/src/shaders/fragment_shader.glsl
@@ -9,9 +9,8 @@ out vec4 Frag_Colour;
 
 void main()
 {
-    vec2 flipped_texcoord = vec2(texcoord.x, 1.0 - texcoord.y);
     Frag_Colour = mix(
-         texture(textures[0], flipped_texcoord),
-         texture(textures[1], flipped_texcoord),
+         texture(textures[0], texcoord),
+         texture(textures[1], texcoord),
          fade_factor);
 }
