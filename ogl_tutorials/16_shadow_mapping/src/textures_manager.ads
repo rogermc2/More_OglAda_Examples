ecb6b5868999d16d7f63f7817e0af9f40e17ba86
rogermc2 <rogermc@iinet.net.au>
2020-06-27 14:17:45 +1000

    Replaced SOIL with GL.Images

--- ogl_tutorials/16_shadow_mapping/src/textures_manager.ads
+++ ogl_tutorials/16_shadow_mapping/src/textures_manager.ads
@@ -7,7 +7,8 @@ package Textures_Manager is
    procedure Init (Frame_Buffer : in out GL.Objects.Framebuffers.Framebuffer;
                    Depth_Texture : in out GL.Objects.Textures.Texture);
    procedure Load_Texture (Frame_Buffer : in out GL.Objects.Framebuffers.Framebuffer;
-                           Image_File_Name : String);
+                           Image_File_Name : String;
+                           theTexture : in out GL.Objects.Textures.Texture);
 
    Image_Error : Exception;
    Framebuffer_Error : Exception;
