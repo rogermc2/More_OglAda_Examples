7badbabf4a845ef3efc9ccc4aa37a734d7700f9d
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-08-10 16:02:36 +1000

    New project text_render_test

---
+++ text_gl/text_render_test/src/texture_test.ads
@@ -0,0 +1,18 @@
+
+with GL.Objects.Programs;
+with GL.Types;
+with GL.Types.Colors;
+with GL.Uniforms;
+
+package Texture_Test is
+   procedure Initialize;
+   procedure Render_Text (Render_Program : GL.Objects.Programs.Program;
+                          Text   : String; X, Y, Scale : GL.Types.Single;
+                          Colour : GL.Types.Colors.Basic_Color;
+                          Texture_ID, Projection_Matrix_ID,
+                          Colour_ID : GL.Uniforms.Uniform;
+                          Projection_Matrix : GL.Types.Singles.Matrix4);
+
+   Texture_Test_Exception : Exception;
+
+end Texture_Test;
