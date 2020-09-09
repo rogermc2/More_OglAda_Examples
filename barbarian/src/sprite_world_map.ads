9952fc0c70ec4689217f91a1fb4345957a8f6368
rogermc2 <rogermc@iinet.net.au>
2020-09-09 21:25:21 +1000

    New packages sprite_shader_manager and sprite_world_map

---
+++ barbarian/src/sprite_world_map.ads
@@ -0,0 +1,21 @@
+
+--  The idea is to have a tile-by-tile 2D map of the world so that
+--  when rendering from the top-left tile to the bottom-right tile
+--  we can just look up the matching tiles here.
+--  Each tile here will have a list of sprites in bottom-to-top order
+--  so that they can be rendered in back-to-front order for transparency.
+--  This also means that when first placing or moving sprites to a new tile
+--  the sprites need to be removed and re-inserted in the correct place
+--  in the new tile's list
+
+package Sprite_World_Map is
+
+    Sprite_World_Map_Exception : Exception;
+
+    procedure Add_New_Sprite_To_World_Map (U, V : Integer; Y : Float;
+                                           Sprite_ID : Integer);
+    procedure Move_Sprite_To_World_Map (From_U, From_V, To_U, To_V : Integer;
+                                        Y : Float; Sprite_ID : Integer);
+    procedure Remove_Sprite_From_World_Map (U, V : Integer;
+                                           Sprite_ID : Integer);
+end Sprite_World_Map;
