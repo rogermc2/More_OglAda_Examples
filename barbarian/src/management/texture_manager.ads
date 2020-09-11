10e9cb1a6c4a116499a325dae056d40421bf58c3
rogermc2 <rogermc@iinet.net.au>
2020-09-11 17:02:54 +1000

    Bind_Texture and Bind_Cube_Texture coded

--- barbarian/src/management/texture_manager.ads
+++ barbarian/src/management/texture_manager.ads
@@ -1,12 +1,13 @@
 
 with GL.Objects.Textures; use GL.Objects.Textures;
-with GL.Types;
 
 package Texture_Manager is
 
     Texture_Exception : Exception;
 
-    procedure Bind_Texture (Slot : GL.Types.Int;
+    procedure Bind_Texture (Slot : Positive;
+                            Tex : GL.Objects.Textures.Texture);
+    procedure Bind_Cube_Texture (Slot : Positive;
                             Tex : GL.Objects.Textures.Texture);
     procedure Init_Texture_Manager;
     procedure Load_Image_To_Texture (File_Name : String;
