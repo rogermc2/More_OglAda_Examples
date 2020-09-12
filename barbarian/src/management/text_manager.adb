697555507b4139516018a5f4fc5f88026131825b
rogermc2 <rogermc@iinet.net.au>
2020-09-12 22:04:43 +1000

    Coded Preload_Comic_Texts

--- barbarian/src/management/text_manager.adb
+++ barbarian/src/management/text_manager.adb
@@ -1,56 +1,71 @@
 
+with Ada.Containers.Vectors;
 with Ada.Exceptions;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Streams.Stream_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 
-with Game_Utils;
-with Settings;
-with Text;
+with GL.Types.Colors;
 
 package body Text_Manager is
+    use GL.Types.Colors;
+
+    type Coloured_Text is record
+        Text   : Unbounded_String := To_Unbounded_String ("");
+        Colour : Color;
+    end record;
+
+    package Coloured_Text_Package is new
+      Ada.Containers.Vectors (Positive, Coloured_Text);
+    type Coloured_Text_List is new Coloured_Text_Package.Vector with null record;
+
+    type Popup_Data is record
+        Rx          : Integer;
+        Open_Paren  : Character := ' ';
+        Red         : Integer;
+        Green       : Integer;
+        Blue        : Integer;
+        A           : Integer;
+        Close_Paren : String (1 .. 2);
+        Popup_Text  : Unbounded_String := To_Unbounded_String ("");
+    end record;
+
+    Preloaded_Comic_Texts : Coloured_Text_List;
 
     -- -------------------------------------------------------------------------
 
-    procedure Preload_Comic_Texts (Path : String) is
-        use Ada.Streams;
-        Input_File       : Stream_IO.File_Type;
-        Input_Stream     : Stream_IO.Stream_Access;
-        aLine            : Unbounded_String;
-        Line_Length      : Integer;
-        Data             : Custom_Data;
-        Text_Height_Offset : constant Float :=
-                               220.0 / Float (Settings.Framebuffer_Height);
-        Mname_Y            : constant Float :=
-                               Top_Margin_Cl - 2.0 * Text_Height *
-                               Float (Num_Custom_Maps) - Text_Height_Offset;
+    procedure Preload_Comic_Texts
+      (Input_Stream : Ada.Streams.Stream_IO.Stream_Access) is
+        use GL.Types;
+        aLine       : Unbounded_String;
+        Pos         : Natural;
+        Popup_Count : Integer := 0;
+        Popup       : Popup_Data;
+        C_Text      : Coloured_Text;
     begin
-        Stream_IO.Open (Input_File, Stream_IO.In_File, Path);
-        Input_Stream := Stream_IO.Stream (Input_File);
-        while not Stream_IO.End_Of_File (Input_File) loop
-            Unbounded_String'Read (Input_Stream, aLine);
-            Line_Length := Length (aLine);
-            if Line_Length < 2 then
-                Game_Utils.Game_Log ("WARNING: skipping short line " &
-                                       To_String (aLine) & " in custom names list.");
-            else
-                for index in 1 .. Line_Length loop
-                    if Element (aLine, index) = '_'  then
-                        Replace_Element (aLine, index, ' ');
-                    end if;
-                end loop;
-                Data.Name := aLine;
-                Data.Text_ID := Text.Add_Text
-                  (To_String (aLine), Left_Margin_Cl, Mname_Y,
-                   25.0, 1.0, 1.0, 1.0, 1.0);
-                Text.Set_Text_Visible (Data.Text_ID, False);
-
-                Maps.Replace_Element (Num_Custom_Maps, Data);
-                Num_Custom_Maps := Num_Custom_Maps + 1;
+        Preloaded_Comic_Texts.Clear;
+        Unbounded_String'Read (Input_Stream, aLine);
+        Pos := Index (aLine, " ");
+        if Slice (aLine, 1, Pos - 1) /= "popups" then
+            raise Text_Manager_Exception with
+              "Text_Manager.Preload_Comic_Texts; Invalid Comic_Texts format";
+        end if;
+        Popup_Count :=
+          Integer'Value (Slice (aLine, Pos + 1, Length (aLine)));
+        for index in 1 .. Popup_Count loop
+            Popup.Open_Paren := ' ';
+            Popup.Popup_Text := To_Unbounded_String ("");
+            Popup_Data'Read (Input_Stream, Popup);
+            if Popup.Open_Paren = '(' and Length (Popup.Popup_Text) > 0 then
+                -- Process any \n?
+                C_Text.Text := Popup.Popup_Text;
+                C_Text.Colour := (Single (Popup.Red), Single (Popup.Green),
+                                  Single (Popup.Blue), Single (Popup.A));
+                Preloaded_Comic_Texts.Append (C_Text);
             end if;
+            --  register rx code
+            --  if (!gEventController.addReceiver (rx, RX_STORY,
+	    --  g_preloaded_comic_colours.size () - 1))
         end loop;
-
-        Stream_IO.Close (Input_File);
 
     exception
         when anError : others =>
