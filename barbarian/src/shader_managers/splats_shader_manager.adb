c25197edc42f075c2f3df0c87e0a676061b226ba
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-10-07 07:23:24 +1100

    splats_shader_manager coded

--- barbarian/src/shader_managers/splats_shader_manager.adb
+++ barbarian/src/shader_managers/splats_shader_manager.adb
@@ -17,22 +17,47 @@ package body Splats_Shader_Manager is
       use Program_Loader;
    begin
       Shader_Program := Program_From
-        ((Src ("src/shaders_3_2/health.vert", Vertex_Shader),
-         Src ("src/shaders_3_2/health.frag", Fragment_Shader)));
+        ((Src ("src/shaders_3_2/splats.vert", Vertex_Shader),
+         Src ("src/shaders_3_2/splats.frag", Fragment_Shader)));
 
       Bind_Attrib_Location (Shader_Program, Shader_Attributes.Attrib_VP, "vp");
+      Bind_Attrib_Location (Shader_Program, Shader_Attributes.Attrib_VT, "vt");
+      Bind_Attrib_Location (Shader_Program, Shader_Attributes.Attrib_VN, "vn");
 
-      Render_Uniforms.Base_Texture_ID :=
-          Uniform_Location (Shader_Program, "tex_base");
-      Render_Uniforms.Model_Matrix_ID :=
-        Uniform_Location (Shader_Program, "model_matrix");
-      Render_Uniforms.Red_Texture_ID :=
-          Uniform_Location (Shader_Program, "tex_red");
+      Render_Uniforms.Ambient_Colour_ID :=
+          Uniform_Location (Shader_Program, "L_a");
+      Render_Uniforms.Caster_Pos_ID :=
+          Uniform_Location (Shader_Program, "caster_pos_wor");
+      Render_Uniforms.Cube_Texture_ID :=
+          Uniform_Location (Shader_Program, "cube_texture");
+      Render_Uniforms.Dyn_Light_Pos_ID :=
+          Uniform_Location (Shader_Program, "dyn_light_pos_wor");
+      Render_Uniforms.Dyn_Light_Diff_ID :=
+          Uniform_Location (Shader_Program, "dyn_light_diff");
+      Render_Uniforms.Dyn_Light_Spec_ID :=
+          Uniform_Location (Shader_Program, "dyn_light_spec");
+      Render_Uniforms.Dyn_Light_Range_ID :=
+          Uniform_Location (Shader_Program, "dyn_light_range");
+      Render_Uniforms.Projection_Matrix_ID :=
+        Uniform_Location (Shader_Program, "P");
+      Render_Uniforms.Shadow_Enabled_ID :=
+        Uniform_Location (Shader_Program, "shadow_enabled");
+      Render_Uniforms.Texture_ID :=
+          Uniform_Location (Shader_Program, "tex");
+      Render_Uniforms.View_Matrix_ID :=
+        Uniform_Location (Shader_Program, "V");
 
       Use_Program (Shader_Program);
-      GL.Uniforms.Set_Int (Render_Uniforms.Base_Texture_ID, 0);
-      GL.Uniforms.Set_Single (Render_Uniforms.Model_Matrix_ID, Identity4);
-      GL.Uniforms.Set_Int (Render_Uniforms.Red_Texture_ID, 0);
+      GL.Uniforms.Set_Single (Render_Uniforms.Ambient_Colour_ID, Identity3);
+      GL.Uniforms.Set_Int (Render_Uniforms.Cube_Texture_ID, 0);
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Pos_ID, Identity3);
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Diff_ID, Identity3);
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Spec_ID, Identity3);
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Range_ID, 0.0);
+      GL.Uniforms.Set_Single (Render_Uniforms.Projection_Matrix_ID, Identity4);
+      GL.Uniforms.Set_Single (Render_Uniforms.Shadow_Enabled_ID, 0.0);
+      GL.Uniforms.Set_Int (Render_Uniforms.Texture_ID, 0);
+      GL.Uniforms.Set_Single (Render_Uniforms.View_Matrix_ID, Identity4);
 
    exception
       when others =>
@@ -42,31 +67,80 @@ package body Splats_Shader_Manager is
 
   --  -------------------------------------------------------------------------
 
-   procedure Set_Base_Texture (Texture : Int) is
+   procedure Set_Ambient_Colour (Ambient : Singles.Vector3) is
    begin
-      GL.Uniforms.Set_Int (Render_Uniforms.Base_Texture_ID, Texture);
-   end Set_Base_Texture;
+      GL.Uniforms.Set_Single (Render_Uniforms.Ambient_Colour_ID, Ambient);
+   end Set_Ambient_Colour;
 
    --  -------------------------------------------------------------------------
 
-   procedure Set_Health_Factor (Health_Factor : Single) is
+   procedure Set_Caster_Position (Pos : Singles.Vector3) is
    begin
-      GL.Uniforms.Set_Single (Render_Uniforms.Health_Factor_ID, Health_Factor);
-   end Set_Health_Factor;
+      GL.Uniforms.Set_Single (Render_Uniforms.Caster_Pos_ID, Pos);
+   end Set_Caster_Position;
 
    --  -------------------------------------------------------------------------
 
-   procedure Set_Model_Matrix (Model_Matrix : Singles.Matrix4) is
+   procedure Set_Cube_Texture (Texture : Int) is
    begin
-      GL.Uniforms.Set_Single (Render_Uniforms.Model_Matrix_ID, Model_Matrix);
-   end Set_Model_Matrix;
+      GL.Uniforms.Set_Int (Render_Uniforms.Cube_Texture_ID, Texture);
+   end Set_Cube_Texture;
 
    --  -------------------------------------------------------------------------
 
-   procedure Set_Red_Texture (Texture : Int) is
+   procedure Set_Dyn_Light_Pos (Pos : Singles.Vector3) is
    begin
-      GL.Uniforms.Set_Int (Render_Uniforms.Red_Texture_ID, Texture);
-   end Set_Red_Texture;
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Pos_ID, Pos);
+   end Set_Dyn_Light_Pos;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Dyn_Light_Diff (Diff : Singles.Vector3) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Diff_ID, Diff);
+   end Set_Dyn_Light_Diff;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Dyn_Light_Spec (Spec : Singles.Vector3) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Spec_ID, Spec);
+   end Set_Dyn_Light_Spec;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Dyn_Light_Range (Light_Range  : Single) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Dyn_Light_Range_ID, Light_Range);
+   end Set_Dyn_Light_Range;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Projection_Matrix (Projection_Matrix : Singles.Matrix4) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Projection_Matrix_ID, Projection_Matrix);
+   end Set_Projection_Matrix;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Shadow_Enabled (Enabled : Single) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Shadow_Enabled_ID, Enabled);
+   end Set_Shadow_Enabled;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_Texture (Texture : Int) is
+   begin
+      GL.Uniforms.Set_Int (Render_Uniforms.Texture_ID, Texture);
+   end Set_Texture;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Set_View_Matrix (View_Matrix : Singles.Matrix4) is
+   begin
+      GL.Uniforms.Set_Single (Render_Uniforms.Projection_Matrix_ID, View_Matrix);
+   end Set_View_Matrix;
 
    --  -------------------------------------------------------------------------
 
--- barbarian/src/shader_managers/splats_shader_manager.ads
+++ barbarian/src/shader_managers/splats_shader_manager.ads
@@ -7,24 +7,28 @@ package Splats_Shader_Manager is
 
    type Shader_Uniforms is record
       Ambient_Colour_ID    : GL.Uniforms.Uniform := 0;
+      Caster_Pos_ID        : GL.Uniforms.Uniform := 0;
+      Cube_Texture_ID      : GL.Uniforms.Uniform := 0;
       Dyn_Light_Pos_ID     : GL.Uniforms.Uniform := 0;
       Dyn_Light_Diff_ID    : GL.Uniforms.Uniform := 0;
       Dyn_Light_Spec_ID    : GL.Uniforms.Uniform := 0;
       Dyn_Light_Range_ID   : GL.Uniforms.Uniform := 0;
-      Model_Matrix_ID      : GL.Uniforms.Uniform := 0;
       Projection_Matrix_ID : GL.Uniforms.Uniform := 0;
+      Shadow_Enabled_ID    : GL.Uniforms.Uniform := 0;
       Texture_ID           : GL.Uniforms.Uniform := 0;
       View_Matrix_ID       : GL.Uniforms.Uniform := 0;
    end record;
 
    procedure Init (Shader_Program : in out GL.Objects.Programs.Program);
    procedure Set_Ambient_Colour (Ambient : Singles.Vector3);
+   procedure Set_Caster_Position (Pos : Singles.Vector3);
+   procedure Set_Cube_Texture (Texture : Int);
    procedure Set_Dyn_Light_Pos (Pos : Singles.Vector3);
    procedure Set_Dyn_Light_Diff (Diff : Singles.Vector3);
    procedure Set_Dyn_Light_Spec (Spec : Singles.Vector3);
    procedure Set_Dyn_Light_Range (Light_Range  : Single);
-   procedure Set_Model_Matrix (Model_Matrix : Singles.Matrix4);
    procedure Set_Projection_Matrix (Projection_Matrix : Singles.Matrix4);
+   procedure Set_Shadow_Enabled (Enabled : Single);
    procedure Set_Texture (Texture : Int);
    procedure Set_View_Matrix (View_Matrix : Singles.Matrix4);
 
