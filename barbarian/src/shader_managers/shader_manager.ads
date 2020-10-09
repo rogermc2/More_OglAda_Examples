008659123013e83e187e7144db8b082e36279cb4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-10-10 01:45:29 +1100

    Revised fallback shader routines

--- barbarian/src/shader_managers/shader_manager.ads
+++ barbarian/src/shader_managers/shader_manager.ads
@@ -1,6 +1,14 @@
 
+with GL.Types; use GL.Types;
+
 package Shader_Manager is
 
    procedure Init;
+   procedure Set_Model_Matrix (Program_Index : Positive;
+                               Model_Matrix  : Singles.Matrix4);
+   procedure Set_Projection_Matrix (Program_Index : Positive;
+                                    Projection_Matrix: Singles.Matrix4);
+   procedure Set_View_Matrix (Program_Index : Positive;
+                              View_Matrix  : Singles.Matrix4);
 
 end Shader_Manager;
