4dacc1f9bf27cb578d198326c92afea132c1a1dc
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-10-20 11:58:32 +1100

    4_1_textures working

--- learnopengl/4_1_textures/src/textures_41.adb
+++ learnopengl/4_1_textures/src/textures_41.adb
@@ -2,18 +2,32 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GL.Images;
+with GL.Objects.Textures.Targets;
 with GL.Pixels;
 
 package body Textures_41 is
 
    procedure Load_Texture (aTexture : in out GL.Objects.Textures.Texture;
                            Image_File_Name : String) is
+      use GL.Objects.Textures;
+      use GL.Objects.Textures.Targets;
    begin
+      aTexture.Initialize_Id;
+
+      GL.Objects.Textures.Set_Active_Unit (0);
+      Texture_2D.Set_X_Wrapping (Repeat);
+      Texture_2D.Set_Y_Wrapping (Repeat);
+      Texture_2D.Set_Minifying_Filter (Linear);
+      Texture_2D.Set_Magnifying_Filter (Linear);
+
       GL.Images.Load_File_To_Texture (
            Path           => Image_File_Name,
            Texture        => aTexture,
-           Texture_Format => GL.Pixels.RGBA,
+           Texture_Format => GL.Pixels.RGB,
            Try_TGA        => True);
+
+      Texture_2D.Bind (aTexture);
+      Texture_2D.Generate_Mipmap;
 
    exception
       when others =>
