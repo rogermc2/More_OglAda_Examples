f272663a1d2855938d23e00132ac123c6bd693a9
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-10-23 16:38:16 +1100

    Added frustrum enable cull and update

--- barbarian/src/frustum.ads
+++ barbarian/src/frustum.ads
@@ -7,6 +7,8 @@ package Frustum is
 
     --  Init debug variables for frustum wireframe
     procedure Draw_Frustum_Box;
+    procedure Enable_Frustum_Cull (State : Boolean);
+    procedure Enable_Update (State : Boolean);
     --  Set to false to stop frustum culling alogether for testing
     function Frustum_Cull_Enabled return Boolean;
     --  Set To False To Stop Frustum Plane Extraction
@@ -17,6 +19,6 @@ package Frustum is
     function Is_Aabb_In_Frustum (Mins, Maxs : Singles.Vector3) return Boolean;
     procedure Re_Extract_Frustum_Planes
       (Fovy_Deg : Maths.Degree; Aspect, Near, Far : Single;
-       Cam_Pos : Singles.Vector3; Mat : in out Singles.Matrix4);
+       Cam_Pos  : Singles.Vector3; Mat : in out Singles.Matrix4);
 
 end Frustum;
