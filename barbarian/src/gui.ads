c7f4765e8bd608be2ad5fcd238e21c3236231b7e
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-10-24 14:24:50 +1100

    Coding

--- barbarian/src/gui.ads
+++ barbarian/src/gui.ads
@@ -1,11 +1,13 @@
 
 package GUI is
 
-    procedure Draw_Controller_Button_Overlays (Elapsed : Float);
-    procedure Init_GUIs;
-    procedure Load_Gui_Shaders;
---      procedure Set_GUI_Gold (Amount : Integer);
---      procedure Set_GUI_Javalin_Ammo (Amount : Integer);
---      procedure Set_GUI_Kills (Amount : Integer);
+   procedure Draw_Controller_Button_Overlays (Elapsed : Float);
+   procedure Init_GUIs;
+   procedure Load_Gui_Shaders;
+   --      procedure Set_GUI_Gold (Amount : Integer);
+   --      procedure Set_GUI_Javalin_Ammo (Amount : Integer);
+   --      procedure Set_GUI_Kills (Amount : Integer);
+   procedure Show_Controller_Button_Overlay
+     (Pos_Index, Tex_Index : in out Integer);
 
 end GUI;
