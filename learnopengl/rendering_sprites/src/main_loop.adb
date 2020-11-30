463b8b91229deb6e4bbea45d6febe5bfa6bd459e
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-11-30 17:52:20 +1100

    Working

--- learnopengl/rendering_sprites/src/main_loop.adb
+++ learnopengl/rendering_sprites/src/main_loop.adb
@@ -1,17 +1,10 @@
 
-
 with Ada.Text_IO; use Ada.Text_IO;
 
-with GL.Attributes;
-with GL.Objects.Buffers;
 with GL.Objects.Programs;
-with GL.Objects.Shaders;
-with GL.Objects.Vertex_Arrays;
 with GL.Objects.Textures;
-with GL.Objects.Textures.Targets;
 with GL.Types;
 with GL.Types.Colors;
-with GL.Uniforms;
 with GL.Window;
 
 with Glfw;
@@ -21,96 +14,31 @@ with Glfw.Windows;
 with Glfw.Windows.Context;
 
 with Maths;
-with Program_Loader;
 with Utilities;
 
+with Sprite_Render;
 with Sprite_Textures;
-with Vertex_Data;
 
 procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
-
-   Quad_VAO          : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
-   Vertex_Buffer     : GL.Objects.Buffers.Buffer;
-   theTexture        : GL.Objects.Textures.Texture;
-   Sprite_SP         : GL.Objects.Programs.Program;
-   Model_ID          : GL.Uniforms.Uniform;
-   Projection_ID     : GL.Uniforms.Uniform;
-   Image_ID          : GL.Uniforms.Uniform;
-   Colour_ID         : GL.Uniforms.Uniform;
-   Projection_Matrix : GL.Types.Singles.Matrix4;
-
-   --  ------------------------------------------------------------------------
-
-   procedure Draw_Sprite (aTexture       : GL.Objects.Textures.Texture;
-                          Position, Size : GL.Types.Singles.Vector2;
-                          Rotate         : Maths.Radian;
-                          Colour         : GL.Types.Singles.Vector3) is
-      use GL.Objects.Buffers;
-      use GL.Objects.Programs;
-      use GL.Objects.Textures.Targets;
-      use GL.Types;
-      use Singles;
-      use Maths;
-      Model_Matrix : Singles.Matrix4;
-   begin
-
-      Model_Matrix :=
-        Scaling_Matrix ((Size (GL.X), Size (GL.Y), 1.0)) * Model_Matrix;
-      Model_Matrix := Translation_Matrix
-        ((-0.5 * Size (GL.X), -0.5 * Size (GL.Y), 0.0)) * Model_Matrix;
-      Model_Matrix := Rotation_Matrix (Rotate, (0.0, 0.0, 1.0)) * Model_Matrix;
-      Model_Matrix := Translation_Matrix
-        ((0.5 * Size (GL.X), 0.5 * Size (GL.Y), 0.0)) * Model_Matrix;
-      Model_Matrix := Translation_Matrix ((Position (GL.X), Position (GL.Y), 0.0));
-
-      Use_Program (Sprite_SP);
-      GL.Uniforms.Set_Single (Model_ID, Model_Matrix);
-      GL.Uniforms.Set_Single (Colour_ID, Colour);
-
-      GL.Objects.Textures.Set_Active_Unit (0);
-      Texture_2D.Bind (aTexture);
-      Quad_VAO.Bind;
-
-      GL.Objects.Vertex_Arrays.Draw_Arrays(Triangles, 0, 6);
-
-   end Draw_Sprite;
-
-   --  ------------------------------------------------------------------------
-
-   procedure Init_Render_Data (Window : in out Glfw.Windows.Window) is
-      use GL.Objects.Buffers;
-      use GL.Types;
-      Window_Width  : Glfw.Size;
-      Window_Height : Glfw.Size;
-      Stride        : constant Int := Maths.Vector4'Size / 8;
-   begin
-      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
-      Quad_VAO.Initialize_Id;
-      Quad_VAO.Bind;
-
-      Vertex_Buffer.Initialize_Id;
-      Array_Buffer.Bind (Vertex_Buffer);
-      Utilities.Load_Vertex_Buffer
-        (Array_Buffer, Vertex_Data.Vertices, Static_Draw);
-
-      GL.Attributes.Enable_Vertex_Attrib_Array (0);
-      GL.Attributes.Set_Vertex_Attrib_Pointer (0, 4, Single_Type, True, Stride, 0);
-   end Init_Render_Data;
+   use Sprite_Render;
+   theTexture    : GL.Objects.Textures.Texture;
+   Sprite_SP     : GL.Objects.Programs.Program;
 
    --  ------------------------------------------------------------------------
 
    procedure Render  (Window  : in out Glfw.Windows.Window) is
       use GL.Types;
-      Background    : constant GL.Types.Colors.Color := (0.2, 0.3, 0.3, 1.0);
+      Background    : constant Colors.Color := (0.0, 0.0, 0.0, 1.0);
       Window_Width  : Glfw.Size;
       Window_Height : Glfw.Size;
    begin
       Window.Get_Framebuffer_Size (Window_Width, Window_Height);
-      GL.Window.Set_Viewport (0, 0, GL.Types.Int (Window_Width),
-                              GL.Types.Int (Window_Height));
+      GL.Window.Set_Viewport (0, 0, Int (Window_Width),
+                              Int (Window_Height));
       Utilities.Clear_Background_Colour (Background);
 
-      GL.Objects.Buffers.Draw_Elements (Triangles, 6, UInt_Type, 0);
+      Draw_Sprite (Sprite_SP, theTexture, (200.0, 200.0), (300.0, 400.0),
+                   45.0, (0.0, 1.0, 0.0));
 
    exception
       when  others =>
@@ -120,48 +48,38 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Setup (Window : in out Glfw.Windows.Window) is
-      use GL.Objects.Buffers;
-      use GL.Objects.Shaders;
+   procedure Init (Window : in out Glfw.Windows.Window) is
       use GL.Types;
-      use Program_Loader;
-      Window_Width  : Glfw.Size;
-      Window_Height : Glfw.Size;
-      Stride        : constant Int := Maths.Vector4'Size / 8;
+      Window_Width      : Glfw.Size;
+      Window_Height     : Glfw.Size;
+      Projection_Matrix : Singles.Matrix4 := GL.Types.Singles.Identity4;
    begin
+      Load_Shaders (Sprite_SP);
       Init_Render_Data (Window);
 
-      Sprite_SP := Program_From
-        ((Src ("src/shaders/sprite.vs", Vertex_Shader),
-         Src ("src/shaders/sprite.fs", Fragment_Shader)));
-
-      GL.Objects.Programs.Use_Program (Sprite_SP);
-      Model_ID := GL.Objects.Programs.Uniform_Location (Sprite_SP, "model");
-      Projection_ID := GL.Objects.Programs.Uniform_Location (Sprite_SP, "projection");
-      Image_ID := GL.Objects.Programs.Uniform_Location (Sprite_SP, "image");
-      Colour_ID := GL.Objects.Programs.Uniform_Location (Sprite_SP, "spriteColour");
-
+      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
       Maths.Init_Orthographic_Transform
         (Single (Window_Height), 0.0, 0.0, Single (Window_Width), -1.0, 1.0,
          Projection_Matrix);
+      Utilities.Print_Matrix ("Projection_Matrix", Projection_Matrix);
+      GL.Objects.Programs.Use_Program (Sprite_SP);
+      Sprite_Render.Set_Image (0);
+      Sprite_Render.Set_Perspective (Projection_Matrix);
 
-      Sprite_Textures.Load_Texture (theTexture, "../../textures/awesomeface.png");
-
-      GL.Uniforms.Set_Int (Image_ID, 0);
-      Sprite_Textures.Load_Texture (theTexture,
-                                    "../resources/textures/container.jpg");
+      Sprite_Textures.Load_Texture
+        (theTexture, "../resources/textures/awesomeface.png");
    exception
       when others =>
          Put_Line ("An exception occurred in Setup.");
          raise;
-   end Setup;
+   end Init;
 
    --  ------------------------------------------------------------------------
 
    use Glfw.Input;
    Running : Boolean := True;
 begin
-   Setup (Main_Window);
+   Init (Main_Window);
    while Running loop
       Render (Main_Window);
       Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
--- learnopengl/rendering_sprites/src/shaders/sprite.vs
+++ learnopengl/rendering_sprites/src/shaders/sprite.vs
@@ -1,8 +1,6 @@
 #version 410 core
 
-layout (location = 0) in vec4 vertex;  --   <vec2 position, vec2 texCoords>
-
-out vec2 TexCoord;
+layout (location = 0) in vec4 vertex;  //w   <vec2 position, vec2 texCoords>
 
 out vec2 TexCoords;
 
