a6403adb15164d62c7f1c3ddcaf28d1332944259
rogermc2 <rogermc@iinet.net.au>
2021-01-09 13:33:06 +1100

    Changed PV and View_Matrix initialization to Singles.Identity4

--- barbarian/src/camera.ads
+++ barbarian/src/camera.ads
@@ -5,29 +5,7 @@ with Maths;
 
 package Camera is
 
-   type Camera_Data is record
-      PV                              : Singles.Matrix4 := Singles.Identity4;
-      View_Matrix                     : Singles.Matrix4 := (others => (others => 0.0));
-      Projection_Matrix               : Singles.Matrix4 := (others => (others => 0.0));
-      Clip_Plane                      : Singles.Matrix4 := (others => (others => 0.0));
-      GUI_Proj_Matrix                 : Singles.Matrix4 := (others => (others => 0.0));
-      World_Position                  : Singles.Vector3 := (0.0, 0.0, 0.0);
-      Shake_Mod_Position              : Singles.Vector3 := (0.0, 0.0, 0.0);
-      Original_Screen_Shake_Time      : Float := 1.0;
-      Screen_Shake_Countdown_Secs     : Float := 0.0;
-      Wind_In_Countdown               : Float := 0.0;
-      Original_Screen_Shake_Amplitude : Float := 0.0;
-      Screen_Shake_Amplitude          : Float := 0.0;
-      Screen_Shake_Frequency          : Float := 0.0;
-      Wind_In_Angle                   : Maths.Degree := 0.0;
-      FOY_Y                           : Maths.Degree := 67.0;
-      Aspect                          : Single := 0.0;
-      Near                            : Single := 0.0;
-      Far                             : Single := 0.0;
-      Height                          : Single := 13.0;
-      Is_Dirty                        : Boolean := True;
-      Manual_Override                 : Boolean := False;
-   end record;
+   type Camera_Data is private;
 
    procedure Camera_Wind_In;
    function Default_Camera return Camera_Data;
@@ -50,5 +28,32 @@ package Camera is
    function View_Matrix return Singles.Matrix4;
    procedure Update_Camera_Effects (Delta_Time : Float);
    function World_Position return Singles.Vector3;
+
+private
+
+   type Camera_Data is record
+      PV                              : Singles.Matrix4 := Singles.Identity4;
+      View_Matrix                     : Singles.Matrix4 := Singles.Identity4;
+      Projection_Matrix               : Singles.Matrix4 := (others => (others => 0.0));
+      Clip_Plane                      : Singles.Matrix4 := (others => (others => 0.0));
+      GUI_Proj_Matrix                 : Singles.Matrix4 := Singles.Identity4;
+      World_Position                  : Singles.Vector3 := (0.0, 0.0, 0.0);
+      Shake_Mod_Position              : Singles.Vector3 := (0.0, 0.0, 0.0);
+      Original_Screen_Shake_Time      : Float := 1.0;
+      Screen_Shake_Countdown_Secs     : Float := 0.0;
+      Wind_In_Countdown               : Float := 0.0;
+      Original_Screen_Shake_Amplitude : Float := 0.0;
+      Screen_Shake_Amplitude          : Float := 0.0;
+      Screen_Shake_Frequency          : Float := 0.0;
+      Wind_In_Angle                   : Maths.Degree := 0.0;
+      FOY_Y                           : Maths.Degree := 67.0;
+      Aspect                          : Single := 0.0;
+      Near                            : Single := 0.0;
+      Far                             : Single := 0.0;
+      Height                          : Single := 13.0;
+      Is_Dirty                        : Boolean := True;
+      Manual_Override                 : Boolean := False;
+   end record;
+
 
 end Camera;
