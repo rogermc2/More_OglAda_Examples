fe249fc471338643c93482dfea33baea053fd804
rogermc2 <rogermc@iinet.net.au>
2021-03-05 12:30:37 +1100

    Fixed Add_Transparency_Item

--- barbarian/src/transparency.adb
+++ barbarian/src/transparency.adb
@@ -35,6 +35,75 @@ package body Transparency is
 
    --  ----------------------------------------------------------------------------
 
+   procedure Add_Transparency_Item (Item_Type : Transparency_Type;
+                                    Render_ID : Positive;
+                                    Position  : GL.Types.Singles.Vector3;
+                                    Brad      : GL.Types.Single) is
+      use GL.Types;
+      use Singles;
+      use Transparent_Package;
+      Dist         : constant Vector3 := TR_Camera_Position - Position;
+      Sq_Dist      : constant Single := Maths.Length_Sq (Dist) - Brad ** 2;
+      Inspect_Node : Transparency_Node;
+      Inspect      : Natural;
+      New_Node     : Transparency_Node;
+      P_Closer     : Natural;
+      Found        : Boolean := False;
+
+      procedure Update_Farthest (Node : in out Transparency_Node) is
+      begin
+          Node.Farther := TR_Nodes.Last_Index;
+      end Update_Farthest;
+
+   begin
+      New_Node.Tr_Type := Item_Type;
+      New_Node.Render_Id := Integer (Render_ID);
+      New_Node.Sq_Dist := Sq_Dist;
+
+      if Is_Empty (TR_Nodes) then
+         TR_Nodes.Append (New_Node);
+         TR_Closest_Node := 0;
+         TR_Farthest_Node := 0;
+         Found := True;
+      else
+         Inspect := TR_Closest_Node;
+         while Inspect /= 0 and not Found loop
+            Inspect_Node := TR_Nodes.Element (Inspect);
+            Found := Sq_Dist < Inspect_Node.Sq_Dist;
+            if Found then
+               P_Closer :=  Inspect_Node.Closer;
+               Inspect_Node.Closer := TR_Nodes.Last_Index;
+               New_Node.Closer := P_Closer;
+               New_Node.Farther := Inspect;
+               if Inspect = TR_Closest_Node then
+                  TR_Closest_Node := TR_Nodes.Last_Index;
+               else
+               TR_Nodes.Update_Element (P_Closer,
+                                        Update_Farthest'Access);
+               end if;
+               TR_Nodes.Replace_Element (Inspect, Inspect_Node);
+               TR_Nodes.Append (New_Node);
+            end if;
+            Inspect := Inspect_Node.Farther;
+         end loop;
+
+         if not Found then
+            --  The new node is the farthest node
+            New_Node.Closer := TR_Farthest_Node;
+            New_Node.Farther := 0;
+            TR_Nodes.Append (New_Node);
+            if TR_Farthest_Node > 0 then
+                TR_Nodes.Update_Element (TR_Farthest_Node,
+                                         Update_Farthest'Access);
+            end if;
+            TR_Farthest_Node := TR_Nodes.Last_Index;
+         end if;
+      end if;
+
+   end Add_Transparency_Item;
+
+   --  ----------------------------------------------------------------------------
+
    procedure Draw_Transparency_List is
       use GL.Blending;
       use GL.Toggles;
@@ -49,7 +118,7 @@ package body Transparency is
       GL.Buffers.Depth_Mask (False);
       Enable (Depth_Test);
 
-      while Inspect /= 0 loop
+      while Inspect > 0 loop
            Put_Line ("Transparency.Draw_Transparency_List, Inspect: " &
                     Integer'Image (Inspect));
          Node := TR_Nodes.Element (Inspect);
@@ -83,73 +152,6 @@ package body Transparency is
       TR_Farthest_Node := 0;
       TR_Camera_Position := Camera_Position;
    end Reset_Transparency_List;
-
-   --  ----------------------------------------------------------------------------
-
-   procedure Add_Transparency_Item (Item_Type : Transparency_Type;
-                                    Render_ID : Positive;
-                                    Position  : GL.Types.Singles.Vector3;
-                                    Brad      : GL.Types.Single) is
-      use GL.Types;
-      use Singles;
-      use Transparent_Package;
-      Dist         : constant Vector3 := TR_Camera_Position - Position;
-      Sq_Dist      : constant Single := Maths.Length_Sq (Dist) - Brad ** 2;
-      Inspect_Node : Transparency_Node;
-      Inspect      : Natural;
-      New_Node     : Transparency_Node;
-      P_Closer     : Natural;
-      Found        : Boolean := False;
-
-      procedure Update_Farthest (Node : in out Transparency_Node) is
-      begin
-          Node.Farther := TR_Nodes.Last_Index;
-      end Update_Farthest;
-
-   begin
-      New_Node.Tr_Type := Item_Type;
-      New_Node.Render_Id := Integer (Render_ID);
-      New_Node.Sq_Dist := Sq_Dist;
-
-      if Is_Empty (TR_Nodes) then
-         TR_Nodes.Append (New_Node);
-         TR_Closest_Node := TR_Nodes.First_Index;
-         TR_Farthest_Node := TR_Closest_Node;
-         Found := True;
-      else
-         Inspect := TR_Closest_Node;
-         while Inspect /= 0 and not Found loop
-            Inspect_Node := TR_Nodes.Element (Inspect);
-            Found := Sq_Dist < Inspect_Node.Sq_Dist;
-            if Found then
-               P_Closer :=  Inspect_Node.Closer;
-               Inspect_Node.Closer := TR_Nodes.Last_Index;
-               New_Node.Closer := P_Closer;
-               New_Node.Farther := Inspect;
-               if Inspect = TR_Closest_Node then
-                  TR_Closest_Node := TR_Nodes.Last_Index;
-               else
-               TR_Nodes.Update_Element (P_Closer,
-                                        Update_Farthest'Access);
-               end if;
-               TR_Nodes.Replace_Element (Inspect, Inspect_Node);
-               TR_Nodes.Append (New_Node);
-            end if;
-            Inspect := Inspect_Node.Farther;
-         end loop;
-
-         if not Found then
-            --  The new node is the farthest node
-            New_Node.Closer := TR_Farthest_Node;
-            New_Node.Farther := 0;
-            TR_Nodes.Append (New_Node);
-            TR_Nodes.Update_Element (TR_Farthest_Node,
-                                     Update_Farthest'Access);
-            TR_Farthest_Node := TR_Nodes.Last_Index;
-         end if;
-      end if;
-
-   end Add_Transparency_Item;
 
    --  ----------------------------------------------------------------------------
 
