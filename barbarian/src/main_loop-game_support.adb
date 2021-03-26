3476943b1c5e1f7e311fcafbb971ecea855695bc
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-03-26 23:58:13 +1100

    Uncomment Draw_Manifold_Around

--- barbarian/src/main_loop-game_support.adb
+++ barbarian/src/main_loop-game_support.adb
@@ -100,7 +100,6 @@ package body Main_Loop.Game_Support is
     --  -------------------------------------------------------------------------
 
     procedure Player_1_View (Window     : in out Input_Callback.Barbarian_Window;
-
                              Tile_Tex, Tile_Spec_Tex, Ramp_Diff_Tex,
                              Ramp_Spec_Tex : GL.Objects.Textures.Texture;
                              Delta_Time : Float; Dump_Video : Boolean;
@@ -128,13 +127,13 @@ package body Main_Loop.Game_Support is
         FB_Effects.Bind_Main_Scene_FB;
         Utilities.Clear_Colour_Buffer_And_Depth;
         Transparency.Reset_Transparency_List (Camera_Position);
---          Manifold.Draw_Manifold_Around (Camera_Position,
---                                         Single (Settings.Render_Distance),
---                                         Tile_Tex, Tile_Spec_Tex, Ramp_Diff_Tex,
---                                         Ramp_Spec_Tex);
---          Blood_Splats.Render_Splats;
---          Prop_Renderer.Render_Props_Around_Split (Centre_X, Centre_Z,
---                                                   Int (Settings.Render_Distance));
+        Manifold.Draw_Manifold_Around (Camera_Position,
+                                       Single (Settings.Render_Distance),
+                                       Tile_Tex, Tile_Spec_Tex, Ramp_Diff_Tex,
+                                       Ramp_Spec_Tex);
+        Blood_Splats.Render_Splats;
+        Prop_Renderer.Render_Props_Around_Split (Centre_X, Centre_Z,
+                                                 Int (Settings.Render_Distance));
         Sprite_World_Map.Cache_Sprites_Around
           (UV (GL.X), UV (GL.Y), Int (Settings.Render_Distance));
         Transparency.Draw_Transparency_List;
