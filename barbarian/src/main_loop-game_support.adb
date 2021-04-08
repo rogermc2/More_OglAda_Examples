7fe8a26bcaef74abcc540b37a09f36d58acd75f4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-04-08 13:14:51 +1000

    Debugging

--- barbarian/src/main_loop-game_support.adb
+++ barbarian/src/main_loop-game_support.adb
@@ -112,15 +112,15 @@ package body Main_Loop.Game_Support is
         Centre_Z        : constant Int := Int ((1.0 + Camera_Position (GL.Z)) / 2.0);
         UV              : constant Ints.Vector2 :=
                             (Abs (Centre_X), Abs (Centre_Z));
-        Back_Colour     : constant GL.Types.Colors.Color := (0.6, 0.6, 0.6, 1.0);
+--          Back_Colour     : constant GL.Types.Colors.Color := (0.6, 0.6, 0.6, 1.0);
     begin
-        Utilities.Clear_Background_Colour_And_Depth (Back_Colour);
+--          Utilities.Clear_Background_Colour_And_Depth (Back_Colour);
         if Settings.Shadows_Enabled and Camera.Is_Dirty then
             for index in Shadow_Direction'Range loop
                 Bind_Shadow_FB (index);
---                  Manifold.Draw_Manifold_Around_Depth_Only;
---                  Prop_Renderer.Render_Props_Around_Depth_Only
---                    (UV (GL.X), UV (GL.Y), Shadow_Caster_Max_Tiles_Away);
+                Manifold.Draw_Manifold_Around_Depth_Only;
+                Prop_Renderer.Render_Props_Around_Depth_Only
+                  (UV (GL.X), UV (GL.Y), Shadow_Caster_Max_Tiles_Away);
             end loop;
         end if;   --  end of shadow mapping pass
 
