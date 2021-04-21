91855f0ee6869af62b89faaf3424203017020be4
rogermc2 <rogermc@iinet.net.au>
2021-04-21 18:18:50 +1000

    Changed folder name

---
+++ redbook/10_fur_layering/src/shaders/fur.fs
@@ -0,0 +1,21 @@
+#version 410 core
+
+layout (location = 0) out vec4 colour;
+
+uniform sampler2D fur_texture;
+uniform vec4 fur_colour;
+
+in GS_FS_INTERFACE
+{
+    vec3 normal;
+    vec2 tex_coord;
+    flat float fur_strength;
+} fragment_in;
+
+void main(void)
+{
+    vec4 rgba = texture(fur_texture, fragment_in.tex_coord);
+    float t = rgba.a;
+    t = t * fragment_in.fur_strength;
+    color = fur_color * vec4(1.0, 1.0, 1.0, t);
+}
---
+++ redbook/10_fur_layering/src/shaders/fur.vs
@@ -0,0 +1,18 @@
+#version 410 core
+
+layout (location = 0) in vec4 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 tex_coord;
+
+out VS_FS_INTERFACE
+{
+    vec3 normal;
+    vec2 tex_coord;
+} vertex_vs;
+
+void main(void)
+{
+    vertex_vs.normal = normal;
+    vertex_vs.tex_coord = tex_coord;
+    gl_Position = position;
+}
