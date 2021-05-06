0d5518da67b29796eb79e836dcefa307cd9c3191
rogermc2 <rogermc@iinet.net.au>
2021-05-06 21:28:44 +1000

    Coding lines example

--- games/fun/src/main_loop.adb
+++ games/fun/src/main_loop.adb
@@ -25,9 +25,26 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
     Background         : constant GL.Types.Colors.Color := ((0.6, 0.6, 0.6, 1.0));
     Rendering_Program  : GL.Objects.Programs.Program;
-    Vertex_Array       : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+    Points_VAO         : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+    Lines_VAO          : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
     Vertex_Buffer      : GL.Objects.Buffers.Buffer;
-    Num_Dimension      : constant GL.Types.Int := 2;
+    Num_Point_Bytes    : GL.Types.Int;
+    --      Num_Dimension      : constant GL.Types.Int := 2;
+
+    --  ----------------------------------------------------------------------------
+
+    procedure Draw_Lines is
+        use GL.Types;
+        Num_Points    : constant Int := 3;
+    begin
+        GL.Objects.Vertex_Arrays.Draw_Arrays (Lines, Num_Point_Bytes, Num_Points);
+
+    exception
+        when anError : others =>
+            Put_Line ("An exceptiom occurred in Draw_Lines.");
+            Put_Line (Exception_Information (anError));
+            raise;
+    end Draw_Lines;
 
     --  ----------------------------------------------------------------------------
 
@@ -35,7 +52,10 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
         use GL.Types;
         Num_Points    : constant Int := 3;
     begin
+        -- Point size is set in the vertex shader
+        GL.Toggles.Enable (GL.Toggles.Vertex_Program_Point_Size);
         GL.Objects.Vertex_Arrays.Draw_Arrays (Points, 0, Num_Points);
+        GL.Toggles.Disable (GL.Toggles.Vertex_Program_Point_Size);
 
     exception
         when anError : others =>
@@ -47,13 +67,10 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
     --  ----------------------------------------------------------------------------
 
     procedure Update is
-        use GL.Types;
     begin
         Utilities.Clear_Colour;
-        GL.Attributes.Set_Vertex_Attrib_Pointer  (0, Num_Dimension, Single_Type, False, 0, 0);
-        GL.Attributes.Enable_Vertex_Attrib_Array (0);
         Draw_Points;
-        GL.Attributes.Disable_Vertex_Attrib_Array (0);
+        Draw_Lines;
 
     exception
         when anError : others =>
@@ -65,28 +82,53 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
     --  ----------------------------------------------------------------------------
 
     procedure Setup_Graphic is
+        use GL.Attributes;
         use GL.Types;
         use Program_Loader;
         use GL.Objects.Buffers;
         use GL.Objects.Shaders;
-        Vertex_Data : constant GL.Types.Singles.Vector2_Array (1 .. 3) :=
+        Points_Data : constant GL.Types.Singles.Vector2_Array (1 .. 3) :=
                         ((0.1, -0.6),
                          (0.7, -0.6),
                          (0.4, -0.1));
+        Lines_Data  : constant GL.Types.Singles.Vector2_Array (1 .. 4) :=
+                        ((0.1, -0.6),
+                         (0.7, -0.6),
+                         (0.7, -0.6),
+                         (0.4, -0.1));
+        Point_Bytes : constant Long := Points_Data'Length / 8;
+        Line_Bytes  : constant Long := Lines_Data'Length / 8;
     begin
+        Num_Point_Bytes := Int (Point_Bytes);
         GL.Buffers.Set_Color_Clear_Value (Background);
-        Rendering_Program := Program_From (
-                                           (Src ("src/shaders/vertex_shader.glsl", Vertex_Shader),
-                                           Src ("src/shaders/fragment_shader.glsl", Fragment_Shader))
-                                          );
+        Rendering_Program := Program_From
+          ((Src ("src/shaders/vertex_shader.glsl", Vertex_Shader),
+           Src ("src/shaders/fragment_shader.glsl", Fragment_Shader)));
         GL.Objects.Programs.Use_Program (Rendering_Program);
-        Vertex_Array.Initialize_Id;
-        Vertex_Array.Bind;
+
         Vertex_Buffer.Initialize_Id;
         Array_Buffer.Bind (Vertex_Buffer);
-        Utilities.Load_Vertex_Buffer (Array_Buffer, Vertex_Data, Static_Draw);
-        -- Point size is set in the vertex shader
-        GL.Toggles.Enable (GL.Toggles.Vertex_Program_Point_Size);
+        Array_Buffer.Allocate (Point_Bytes + Line_Bytes, Static_Draw);
+        Utilities.Load_Vertex_Sub_Buffer (Array_Buffer, 0, Points_Data);
+        Utilities.Load_Vertex_Sub_Buffer (Array_Buffer, Int (Point_Bytes),
+                                          Lines_Data);
+
+        Points_VAO.Initialize_Id;
+        Lines_VAO.Initialize_Id;
+
+        Points_VAO.Bind;
+        Enable_Vertex_Attrib_Array (0);
+        Enable_Vertex_Attrib_Array (1);
+        Array_Buffer.Bind (Vertex_Buffer);
+        Set_Vertex_Attrib_Pointer (0, 3, Single_Type, False, 0, 0);
+        Set_Vertex_Attrib_Pointer (1, 4, Single_Type, False, 0, 0);
+
+        Lines_VAO.Bind;
+        Enable_Vertex_Attrib_Array (0);
+        Enable_Vertex_Attrib_Array (1);
+        Array_Buffer.Bind (Vertex_Buffer);
+        Set_Vertex_Attrib_Pointer (0, 3, Single_Type, False, 0, Int (Point_Bytes));
+        Set_Vertex_Attrib_Pointer (1, 4, Single_Type, False, 0, Int (Point_Bytes));
     exception
         when anError : others =>
             Put_Line ("An exceptiom occurred in Setup_Graphic.");
@@ -97,7 +139,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
     --  ----------------------------------------------------------------------------
 
     use Glfw.Input;
-    Running : Boolean := True;
+    Running  : Boolean := True;
 begin
     Setup_Graphic;
     while Running loop
@@ -112,4 +154,8 @@ exception
     when Program_Loader.Shader_Loading_Error =>
         -- message was already written to stdout
         null;
+    when anError : others =>
+        Put_Line ("An exceptiom occurred in Setup_Graphic.");
+        Put_Line (Exception_Information (anError));
+        raise;
 end Main_Loop;
--- games/fun/src/shaders/vertex_shader.glsl
+++ games/fun/src/shaders/vertex_shader.glsl
@@ -1,9 +1,10 @@
 #version 410
 
-layout (location = 0) in vec2 coord2d;
+layout (location = 0) in vec2 points2d;
+layout (location = 1) in vec2 lines2d;
 
 void main()
 {
   gl_PointSize = 10.0;
-  gl_Position = vec4(coord2d, 0.0, 1.0);
+  gl_Position = vec4(points2d, 0.0, 1.0);
 }
