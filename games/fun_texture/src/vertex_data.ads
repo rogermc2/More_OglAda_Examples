9108dd1491de5a80dadddba33dcc768029af69ab
rogermc2 <rogermc@iinet.net.au>
2021-05-08 10:46:52 +1000

    Update

--- games/fun_texture/src/vertex_data.ads
+++ games/fun_texture/src/vertex_data.ads
@@ -5,12 +5,12 @@ package Vertex_Data is
    use  GL.Types;
 
    Quad_Vertices : constant GL.Types.Singles.Vector2_Array (1 .. 6) :=
-                     ((0.1, -0.1),   --  top left
-                      (0.1, -0.6),   --  bottom left
-                      (0.6, -0.6),   --  bottom right
-                      (0.6, -0.6),   --  bottom right
-                      (0.6, -0.1),   --  top right
-                      (0.1, -0.1));  --  top left
+                     ((0.0, 0.5),   --  top left
+                      (0.0, 0.0),   --  bottom left
+                      (0.5, 0.0),   --  bottom right
+                      (0.5, 0.0),   --  bottom right
+                      (0.5, 0.5),   --  top right
+                      (0.0, 0.5));  --  top left
 
    Texture_Coords : constant Singles.Vector2_Array (1 .. 6) :=
                       ((0.0, 0.0),
