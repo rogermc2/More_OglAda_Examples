285d0e23053e0a53f1c990951d26babd7d977fd2
rogermc2 <rogermc@iinet.net.au>
2021-05-11 21:39:41 +1000

    New package input_manager

---
+++ games/robo_racer_2d_chap_4/src/input_manager.adb
@@ -0,0 +1,41 @@
+
+with Glfw.Input.Keys;
+
+package body Input_Manager is
+
+    Current_Command : Command;
+
+    --  ------------------------------------------------------------------------
+
+    function Get_Command return Command is
+    begin
+        return Current_Command;
+    end Get_Command;
+
+    --  ------------------------------------------------------------------------
+
+    procedure Update (Window : in out Input_Callback.Call_Back_Window;
+                      Delta_Time : Float) is
+        use Glfw.Input.Keys;
+        use Input_Callback;
+    begin
+         if Was_Key_Pressed (Window, Left) or Was_Key_Pressed (Window, A) then
+            Current_Command := Command_Left;
+         elsif Was_Key_Pressed (Window, Right) or
+          Was_Key_Pressed (Window, D) then
+            Current_Command := Command_Right;
+         elsif Was_Key_Pressed (Window, Up) then
+            Current_Command := Command_Up;
+         elsif Was_Key_Pressed (Window, Down) then
+            Current_Command := Command_Down;
+         elsif Was_Key_Pressed (Window, Escape) or
+          Was_Key_Pressed (Window, Q) then
+            Current_Command := Command_Up;
+         else
+            Current_Command := Command_Stop;
+         end if;
+    end Update;
+
+    --  ------------------------------------------------------------------------
+
+end Input_Manager;
---
+++ games/robo_racer_2d_chap_4/src/input_manager.ads
@@ -0,0 +1,13 @@
+
+with Input_Callback;
+
+package Input_Manager is
+
+    type Command is (Command_Left, Command_Right, Command_Stop, Command_Up,
+                    Command_Down, Command_Quit);
+
+    function Get_Command return Command;
+    procedure Update (Window : in out Input_Callback.Call_Back_Window;
+                      Delta_Time : Float);
+
+end Input_Manager;
