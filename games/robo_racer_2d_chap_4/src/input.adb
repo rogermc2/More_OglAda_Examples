be31b232ba415f135bd650d704307d25335b8d8b
rogermc2 <rogermc@iinet.net.au>
2021-05-11 18:58:59 +1000

    Coding

--- games/robo_racer_2d_chap_4/src/input.adb
+++ games/robo_racer_2d_chap_4/src/input.adb
@@ -1,9 +1,29 @@
 
+with Glfw.Input.Keys;
+
 package body Input is
 
-    procedure Update (Delta_Time : Float) is
+    aCommand : Command;
+
+    --  ------------------------------------------------------------------------
+
+    function Get_Command return Command is
     begin
-        null;
+        return aCommand;
+    end Get_Command;
+
+    --  ------------------------------------------------------------------------
+
+    procedure Update (Window : in out Input_Callback.Call_Back_Window;
+                      Delta_Time : Float) is
+        use Glfw.Input.Keys;
+        use Input_Callback;
+    begin
+         if Was_Key_Pressed (Window, Escape) then
+            null;
+         end if;
     end Update;
+
+    --  ------------------------------------------------------------------------
 
 end Input;
--- games/robo_racer_2d_chap_4/src/input.ads
+++ games/robo_racer_2d_chap_4/src/input.ads
@@ -1,6 +1,13 @@
 
+with Input_Callback;
+
 package Input is
 
-    procedure Update (Delta_Time : Float);
+    type Command is (Command_Left, Command_Right, Command_Stop, Command_Up,
+                    Command_Down, Command_Quit);
+
+    function Get_Command return Command;
+    procedure Update (Window : in out Input_Callback.Call_Back_Window;
+                      Delta_Time : Float);
 
 end Input;
