cf0bbaa5b5ad3226df871f0335c05950161320fe
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-16 21:13:15 +1000

    Renamed robo_racer_2d4_gui to robo_racer_2d4

--- games/robo_racer_2d_chap_4/src/robo_racer.adb
+++ games/robo_racer_2d_chap_4/src/robo_racer.adb
@@ -1,4 +1,4 @@
--- Program Robo_Racer Chapter 4 GUI
+-- Program Robo_Racer Chapter 4
 -- Author Roger Mc Murtrie
 -- Created 13 May 2021
 
@@ -11,9 +11,9 @@ with Initialize_With_Callbacks;
 with Input_Callback;
 with Main_Loop;
 
-procedure Robo_Racer_GUI is
+procedure Robo_Racer is
     Main_Window : Input_Callback.Callback_Window;
-    Window_Title : constant String := "Robo Racer 2D Chapter 4 GUI";
+    Window_Title : constant String := "Robo Racer 2D Chapter 4";
 begin
     Glfw.Init;
     Initialize_With_Callbacks (Main_Window, Window_Title);
@@ -22,10 +22,10 @@ begin
 
 exception
     when anError : Constraint_Error =>
-        Put ("Robo_Racer_GUI returned a constraint error: ");
+        Put ("Robo_Racer returned a constraint error: ");
         Put_Line (Exception_Information (anError));
 
     when anError :  others =>
-        Put_Line ("An exception occurred in Robo_Racer_GUI.");
+        Put_Line ("An exception occurred in Robo_Racer.");
         Put_Line (Exception_Information (anError));
-end Robo_Racer_GUI;
+end Robo_Racer;
