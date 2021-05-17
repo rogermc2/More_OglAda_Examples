701c1c7fe14cedab0b3cc96eed57bf961cd4705c
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-17 23:43:00 +1000

    Coding Draw_Text

--- games/robo_racer_2d_chap_6/src/font_manger.ads
+++ games/robo_racer_2d_chap_6/src/font_manger.ads
@@ -1,7 +1,11 @@
 
+with Glfw.Windows;
+
+with GL.Types;
+
 package Font_Manger is
 
-procedure Draw_Text (Text : String; X, Y, R, G, B : Float);
-
+   procedure Draw_Text (Text : String; X, Y, R, G, B : GL.Types.Single);
+   procedure Initialize  (Window  : in out Glfw.Windows.Window);
 
 end Font_Manger;
