76c05a812861674495f7e7a39201d2e4277983c9
rogermc2 <rogermc@iinet.net.au>
2021-05-17 21:21:14 +1000

    Font Rendering files added

---
+++ games/robo_racer_2d_chap_6/src/font_manager.adb
@@ -0,0 +1,191 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GL.Attributes;
+with GL.Blending;
+with GL.Objects.Buffers;
+with GL.Objects.Vertex_Arrays;
+with GL.Objects.Textures.Targets;
+with GL.Pixels;
+with GL.Text;
+with GL.Toggles;
+
+with FT;
+with FT.Faces;
+
+package body Font_Manager is
+
+   Renderer_Ref         : GL.Text.Renderer_Reference;
+   Vertex_Array         : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   Vertex_Buffer        : GL.Objects.Buffers.Buffer;
+   String_Texture       : GL.Objects.Textures.Texture;
+   Triangles_Per_Quad   : constant GL.Types.Int := 2;
+   Font_File_1          : constant String := "./NotoSerif-Regular.ttf";
+
+   function Initialize_Font_Data (Font_File : String)
+                                  return GL.Text.Renderer_Reference;
+   procedure Load_Vertex_Buffer is new
+     GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector4_Pointers);
+   procedure Setup_Buffer (Text : String; X, Y, Scale : GL.Types.Single);
+   procedure Setup_Font (theLibrary : FT.Library_Reference;
+                         Face_Ptr   : out FT.Faces.Face_Reference;
+                         Font_File  : String);
+
+   --  ------------------------------------------------------------------------
+
+    procedure Build_Font is
+    begin
+        null;
+    end Build_Font;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Initialize is
+   begin
+      Vertex_Array.Initialize_Id;
+      Vertex_Array.Bind;
+      Vertex_Buffer.Initialize_Id;
+      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
+      String_Texture.Initialize_Id;
+      GL.Objects.Textures.Targets.Texture_2D.Bind (String_Texture);
+
+      Renderer_Ref := Initialize_Font_Data (Font_File_1);
+   end Initialize;
+
+   --  ------------------------------------------------------------------------
+
+   function Initialize_Font_Data (Font_File : String)
+                                  return GL.Text.Renderer_Reference is
+      theLibrary   : FT.Library_Reference;
+      Face_Ptr     : FT.Faces.Face_Reference;
+      Shader_Ref   : GL.Text.Shader_Program_Reference;
+      Renderer_Ref : GL.Text.Renderer_Reference;
+   begin
+      theLibrary.Init;
+      GL.Text.Create (Shader_Ref);
+      if GL.Text.Created (Shader_Ref) then
+        Setup_Font (theLibrary, Face_Ptr, Font_File);
+        GL.Text.Create (Renderer_Ref, Shader_Ref, Face_Ptr);
+      else
+        raise Texture_Test_Exception with
+        "Texture_Test.Initialize_Font_Data, Shader_Ref creation failed.";
+      end if;
+      return Renderer_Ref;
+   end Initialize_Font_Data;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render (String_Texture : GL.Objects.Textures.Texture;
+                     Texture_ID : GL.Uniforms.Uniform) is
+      use GL.Objects.Textures.Targets;
+      use GL.Types;
+
+      Blend_State               : constant GL.Toggles.Toggle_State :=
+                                    GL.Toggles.State (GL.Toggles.Blend);
+      Src_Alpha_Blend           : constant  GL.Blending.Blend_Factor :=
+                                    GL.Blending.Blend_Func_Src_Alpha;
+      One_Minus_Src_Alpha_Blend : constant  GL.Blending.Blend_Factor :=
+                                    GL.Blending.One_Minus_Src_Alpha;
+      Num_Components            : constant GL.Types.Int := 4;  -- Coords vector size;
+      Num_Vertices              : constant GL.Types.Int := 3 * Triangles_Per_Quad;
+   begin
+      GL.Toggles.Set (GL.Toggles.Blend, Blend_State);
+      GL.Blending.Set_Blend_Func (Src_Alpha_Blend, One_Minus_Src_Alpha_Blend);
+      GL.Objects.Textures.Set_Active_Unit (0);
+      Texture_2D.Bind (String_Texture);
+      GL.Uniforms.Set_Int (Texture_ID, 0);
+
+      GL.Attributes.Enable_Vertex_Attrib_Array (0);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (Index      => 0,
+                                               Count      => Num_Components,
+                                               Kind       => GL.Types.Single_Type,
+                                               Normalized => True,
+                                               Stride     => 0, Offset => 0);
+      GL.Objects.Vertex_Arrays.Draw_Arrays (Triangle_Strip, 0, Num_Vertices);
+      GL.Attributes.Disable_Vertex_Attrib_Array (0);
+
+   end Render;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render_Text (Render_Program                   : GL.Objects.Programs.Program;
+                          Text                             : String; X, Y, Scale     : GL.Types.Single;
+                          Colour                           : GL.Types.Colors.Basic_Color;
+                          Texture_ID, Projection_Matrix_ID : GL.Uniforms.Uniform;
+                          Colour_ID                        : GL.Uniforms.Uniform;
+                          Projection_Matrix                : GL.Types.Singles.Matrix4) is
+      use GL.Text;
+      use GL.Types.Colors;
+   begin
+      if not Renderer_Ref.Created then
+            Put_Line ("Texture_Management.Render_Text Renderer_Ref is not created.");
+      elsif String_Texture.Initialized then
+            String_Texture := To_Texture (Renderer_Ref, UTF_8_String (Text),
+                                          (Colour (R), Colour (G), Colour (B), 1.0));
+      else
+            Put_Line ("Texture_Management.Render_Text String_Texture is not initialized.");
+      end if;
+
+      GL.Objects.Programs.Use_Program (Render_Program);
+      GL.Uniforms.Set_Single (Colour_ID, Colour (R), Colour (G), Colour (B));
+      GL.Uniforms.Set_Single (Projection_Matrix_ID, Projection_Matrix);
+
+      Setup_Buffer (Text, X, Y, Scale);
+      Render (String_Texture, Texture_ID);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Texture_Management.Render_Text.");
+         raise;
+   end Render_Text;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Buffer (Text : String; X, Y, Scale : GL.Types.Single) is
+      use GL.Objects.Buffers;
+      use GL.Text;
+      use GL.Types;
+      Num_Vertices        : constant GL.Types.Int := 3 * Triangles_Per_Quad;
+      X_Start             : constant Single := X;
+      X_End               : Single;
+      Width               : Pixel_Difference;
+      Base                : Pixel_Difference;
+      S_Base              : Single;
+      Top                 : Pixel_Difference;
+      S_Top               : Single;
+      Vertex_Data         : Singles.Vector4_Array (1 .. Num_Vertices);
+
+   begin
+      Calculate_Dimensions (Renderer_Ref, Text, Width, Base, Top);
+      S_Base :=  Y + Single (Base);
+      S_Top := Y + Single (Top) * Scale;
+      X_End := X_Start + Single (Width) * Scale;
+
+      Vertex_Data :=
+           ((X_Start, S_Base, 0.0, 0.0),  --  Lower left X, Y, U, V
+            (X_End,   S_Base, 1.0, 0.0),  --  Lower right
+            (X_Start, S_Top,  0.0, 1.0),  --  Upper left
+
+            (X_Start, S_Top,  0.0, 1.0),  --  Upper left
+            (X_End,   S_Top,  1.0, 1.0),  --  Upper Right
+            (X_End,   S_Base, 1.0, 0.0)); --  Lower right
+
+      Vertex_Array.Bind;
+      Array_Buffer.Bind (Vertex_Buffer);
+      Load_Vertex_Buffer (Array_Buffer, Vertex_Data, Dynamic_Draw);
+   end Setup_Buffer;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Font (theLibrary : FT.Library_Reference;
+                         Face_Ptr   : out FT.Faces.Face_Reference;
+                         Font_File  : String) is
+   begin
+      FT.Faces.New_Face (theLibrary, Font_File, 0, Face_Ptr);
+      FT.Faces.Set_Pixel_Sizes (Face_Ptr, 0, 48);
+      GL.Pixels.Set_Unpack_Alignment (GL.Pixels.Bytes);
+   end Setup_Font;
+
+   --  ------------------------------------------------------------------------
+
+end Font_Manager;
---
+++ text_gl/text_rendering/src/texture_management_with_to_render.adb
@@ -0,0 +1,369 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Interfaces.C;
+
+with GL.Attributes;
+--  with GL.Blending;
+with GL.Objects.Buffers;
+with GL.Objects.Vertex_Arrays;
+with GL.Objects.Textures.Targets;
+with GL.Pixels;
+--  with GL.Toggles;
+
+with FT;
+with FT.Faces;
+with FT.Glyphs;
+
+with Maths;
+
+package body Texture_Management is
+   use Interfaces.C;
+
+   --  Character_Record is based on the learnopengl.com/In-Practice/Text-Rendering tutorial
+   type Character_Record is record     --  glyph bitmap data
+      Texture   : GL.Objects.Textures.Texture;
+      Width     : GL.Types.Int := 0;  -- character width in pixels
+      Rows      : GL.Types.Int := 0;  -- number of rows of pixels in bitmap
+      Left      : GL.Types.Int := 0;  -- position of left side of character in pixels
+      --  relative to left side of bitmap
+      Top       : GL.Types.Int := 0;  -- position of top of character in pixels
+      --  relative to bitmap reference line
+      Advance_X : GL.Types.Int := 0;   -- Total width of bitmap in 1/64th pixel
+   end record;
+
+   type Character_Data_Vector is array (Natural range <>) of Character_Record;
+
+   Renderer_Ref         : GL.Text.Renderer_Reference;
+   Vertex_Array         : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   Vertex_Buffer        : GL.Objects.Buffers.Buffer;
+   String_Texture       : GL.Objects.Textures.Texture;
+   Extended_Ascii_Data  : Character_Data_Vector (0 .. 255);
+   Triangles_Per_Quad   : constant GL.Types.Int := 2;
+   Font_File_1          : constant String := "../fonts/NotoSerif-Regular.ttf";
+
+   procedure Load_Vertex_Buffer is new
+     GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector4_Pointers);
+   procedure Setup_Buffer (Text : String; X, Y, Scale : GL.Types.Single);
+   procedure Setup_Character_Textures (Face_Ptr : FT.Faces.Face_Reference);
+   procedure Setup_Font (theLibrary : FT.Library_Reference;
+                         Face_Ptr   : out FT.Faces.Face_Reference;
+                         Font_File  : String);
+
+   --  ------------------------------------------------------------------------
+
+   function Advance_X (Data : Character_Record) return GL.Types.Single is
+      use GL.Types;
+   begin
+      return Single (Data.Advance_X) / 64.0;
+   end Advance_X;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Initialize is
+   begin
+      Vertex_Array.Initialize_Id;
+      Vertex_Array.Bind;
+      Vertex_Buffer.Initialize_Id;
+      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
+      String_Texture.Initialize_Id;
+      GL.Objects.Textures.Targets.Texture_2D.Bind (String_Texture);
+
+      Renderer_Ref := Texture_Management.Initialize_Font_Data (Font_File_1);
+   end Initialize;
+
+   --  ------------------------------------------------------------------------
+
+   function Initialize_Font_Data (Font_File : String)
+                                  return GL.Text.Renderer_Reference is
+      theLibrary   : FT.Library_Reference;
+      Face_Ptr     : FT.Faces.Face_Reference;
+      Shader_Ref   : GL.Text.Shader_Program_Reference;
+      Renderer_Ref : GL.Text.Renderer_Reference;
+   begin
+      theLibrary.Init;
+      GL.Text.Create (Shader_Ref);
+      --  Shader_Ref holds a reference to the compiled shader program used to render
+      --  text as bitmaps. The shader program can only be initialized once the target
+      --  OpenGL context has been created.
+      if GL.Text.Created (Shader_Ref) then
+        Setup_Font (theLibrary, Face_Ptr, Font_File);
+        GL.Text.Create (Renderer_Ref, Shader_Ref, Face_Ptr);
+        --  A renderer needs to be created for each font family, size and variant.
+        --  A renderer renders text to a monochrome texture which can then be used
+        --  as an alpha-channel to render the text to the screen with
+        --  arbitrary color / texture.
+
+        Setup_Character_Textures (Face_Ptr);
+      else
+        raise Texture_Management_Exception with
+        "Texture_Management.Initialize_Font_Data, Shader_Ref creation failed.";
+      end if;
+      return Renderer_Ref;
+   end Initialize_Font_Data;
+
+   --  ------------------------------------------------------------------------
+--
+--     procedure Load_Texture (Face_Ptr           : FT.Faces.Face_Reference;
+--                             Char_Data          : in out Character_Record;
+--                             Width, Height      : GL.Types.Size;
+--                             X_Offset, Y_Offset : GL.Types.Int) is
+--        use GL.Objects.Textures.Targets;
+--        use GL.Pixels;
+--        use GL.Types;
+--        aTexture          : GL.Objects.Textures.Texture;
+--        Bitmap_Image_Ptr  : GL.Objects.Textures.Image_Source;
+--        Bitmap            : constant FT.Bitmap_Record :=
+--                              FT.Glyphs.Bitmap (Face_Ptr.Glyph_Slot);
+--        Num_Levels        : constant GL.Types.Size := 1;
+--        Mip_Level_0       : constant GL.Objects.Textures.Mipmap_Level := 0;
+--     begin
+--        aTexture.Initialize_Id;
+--        Texture_2D.Bind (aTexture);
+--        Texture_2D.Set_Minifying_Filter (GL.Objects.Textures.Linear);
+--        Texture_2D.Set_Magnifying_Filter (GL.Objects.Textures.Linear);
+--        Texture_2D.Set_X_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_S
+--        Texture_2D.Set_Y_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_T
+--
+--        if Width > 0 and then Height > 0 then
+--           Texture_2D.Storage (Num_Levels, RGBA8, Width, Height);
+--        else
+--           Texture_2D.Storage (Num_Levels, RGBA8, 1, 1);
+--        end if;
+--
+--        Bitmap_Image_Ptr := GL.Objects.Textures.Image_Source (Bitmap.Buffer);
+--        if Width > 0 and Height > 0 then
+--           Texture_2D.Load_Sub_Image_From_Data
+--             (Mip_Level_0, X_Offset, Y_Offset, Width, Height, Red, Unsigned_Byte,
+--              Bitmap_Image_Ptr);
+--        end if;
+--        Char_Data.Texture := aTexture;
+--     end Load_Texture;
+
+   -- --------------------------------------------------------------------------
+
+--     procedure Render (Text_Length : GL.Types.Int;
+   procedure Render (String_Texture : GL.Objects.Textures.Texture;
+                     Texture_ID : GL.Uniforms.Uniform) is
+--        use GL.Objects.Buffers;
+      use GL.Objects.Textures.Targets;
+      use GL.Types;
+
+--        Blend_State               : constant GL.Toggles.Toggle_State :=
+--                                      GL.Toggles.State (GL.Toggles.Blend);
+--        Src_Alpha_Blend           : constant  GL.Blending.Blend_Factor :=
+--                                      GL.Blending.Blend_Func_Src_Alpha;
+--        One_Minus_Src_Alpha_Blend : constant  GL.Blending.Blend_Factor :=
+--                                      GL.Blending.One_Minus_Src_Alpha;
+      Num_Components            : constant GL.Types.Int := 4;  -- Coords vector size;
+--        Num_Vertices              : constant GL.Types.Int :=
+--                                      Text_Length * 3 * Triangles_Per_Quad;
+      Num_Vertices              : constant GL.Types.Int := 3 * Triangles_Per_Quad;
+   begin
+      GL.Objects.Textures.Set_Active_Unit (1);
+      Texture_2D.Bind (String_Texture);
+      GL.Uniforms.Set_Int (Texture_ID, 1);
+
+      GL.Attributes.Enable_Vertex_Attrib_Array (0);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (Index      => 0,
+                                               Count      => Num_Components,
+                                               Kind       => GL.Types.Single_Type,
+                                               Normalized => True,
+                                               Stride     => 0, Offset => 0);
+      GL.Objects.Vertex_Arrays.Draw_Arrays (Triangle_Strip, 0, Num_Vertices);
+      GL.Attributes.Disable_Vertex_Attrib_Array (0);
+
+--        GL.Toggles.Set (GL.Toggles.Blend, Blend_State);
+--        GL.Blending.Set_Blend_Func (Src_Alpha_Blend, One_Minus_Src_Alpha_Blend);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Texture_Management.Render.");
+         raise;
+   end Render;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render_Text (Render_Program                   : GL.Objects.Programs.Program;
+                          Text                             : String; X, Y, Scale     : GL.Types.Single;
+                          Colour                           : GL.Types.Colors.Basic_Color;
+                          Texture_ID, Projection_Matrix_ID : GL.Uniforms.Uniform;
+                          Colour_ID                        : GL.Uniforms.Uniform;
+                          Projection_Matrix                : GL.Types.Singles.Matrix4) is
+      use GL.Text;
+      use GL.Types.Colors;
+   begin
+      if not Renderer_Ref.Created then
+            Put_Line ("Texture_Management.Render Renderer_Ref is not created.");
+      elsif String_Texture.Initialized then
+            String_Texture := To_Texture (Renderer_Ref, UTF_8_String (Text),
+                                          (Colour (R), Colour (G), Colour (B), 1.0));
+      else
+            Put_Line ("Texture_Management.Render String_Texture is not initialized.");
+      end if;
+
+      --  Blending allows a fragment colour's alpha value to control the resulting
+      --  colour which will be transparent for all the glyph's background colours and
+      --  non-transparent for the actual character pixels.
+--        GL.Toggles.Enable (GL.Toggles.Blend);
+--        GL.Blending.Set_Blend_Func (GL.Blending.Src_Alpha,
+--                                    GL.Blending.One_Minus_Src_Alpha);
+
+      GL.Objects.Programs.Use_Program (Render_Program);
+      GL.Uniforms.Set_Single (Colour_ID, Colour (R), Colour (G), Colour (B));
+      GL.Uniforms.Set_Single (Projection_Matrix_ID, Projection_Matrix);
+
+      Setup_Buffer (Text, X, Y, Scale);
+
+      --           Char_Texture :=  Char_Data.Texture;
+      --           if not GL.Objects.Textures.Is_Texture  (Char_Texture.Raw_Id) then
+      if not GL.Objects.Textures.Is_Texture  (String_Texture.Raw_Id) then
+         raise Texture_Management_Exception with
+              "Texture_Management.Render_Text, String_Texture is invalid.";
+      end if;
+      Render (String_Texture, Texture_ID);
+--        Render (Text'Length, String_Texture, Texture_ID);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Texture_Management.Render.");
+         raise;
+   end Render_Text;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Set_Char_Data (Char_Data : in out Character_Record;
+                            Width     : GL.Types.Int; Height : GL.Types.Int;
+                            Left      : GL.Types.Int; Top    : GL.Types.Int;
+                            Advance_X : GL.Types.Int) is
+   begin
+      Char_Data.Width := Width;
+      Char_Data.Rows := Height;
+      Char_Data.Left := Left;
+      Char_Data.Top := Top;
+      Char_Data.Advance_X := Advance_X;
+   end Set_Char_Data;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Setup_Buffer (Text : String; X, Y, Scale : GL.Types.Single) is
+      use GL.Objects.Buffers;
+      use GL.Types;
+      Num_Vertices        : constant GL.Types.Int := 3 * Triangles_Per_Quad; -- Two triangles per quad
+
+      X_Start             : constant Single := X;
+      Y_Orig              : constant Single := Y;
+      X_End               : Single := X_Start;
+      Base                : Single;
+      Base_Min            : Single := Y_Orig;
+      Top                 : Single;
+      Top_Max             : Single := Y_Orig;
+--        Quad_Width          : Single;
+--        Quad_Height         : Single;
+      Char                : Character;
+      Char_Data           : Character_Record;
+      --        Char_Texture   : GL.Objects.Textures.Texture;
+      --        Width          : Pixel_Size;
+      --        Y_Min          : Pixel_Difference;
+      --        Y_Max          : Pixel_Size;
+      Num_Chars           : constant GL.Types.Int := Text'Length;
+      --  2D quad rendered as two triangles requires 2 * 3 vertices
+      Vertex_Data         : Singles.Vector4_Array (1 .. Num_Vertices);
+--        Vertex_Data         : Singles.Vector4_Array (1 .. Num_Chars * Num_Vertices);
+
+   begin
+      for index in 1 .. Num_Chars loop
+         Char := Text (Integer (index));
+         Char_Data := Extended_Ascii_Data (Character'Pos (Char));
+--           X_Pos := X_Orig + Single (Char_Data.Left) * Scale;
+         X_End := X_End + Advance_X (Char_Data) * Scale;
+         Base := Y_Orig - Single (Char_Data.Rows - Char_Data.Top) * Scale;
+         Base_Min := Maths.Min (Base_Min, Base);
+--           Quad_Width := Single (Char_Data.Width) * Scale;
+--           Quad_Height := Single (Char_Data.Rows) * Scale;
+         Top := Base + Single (Char_Data.Rows) * Scale;
+         Top_Max := Maths.Max (Top_Max, Top);
+--           X_Orig := X_Orig + Single (Advance_X (Char_Data)) / 64.0 * Scale;
+      end loop;
+
+--        Vertex_Data := Vertex_Data (6 * (index - 1) + 1 .. 6 * (index - 1) + 6)
+--             ((X_Pos, Base,                            0.0, 0.0),  --  Lower left X, Y, U, V
+--              (X_Pos + Quad_Width, Base,               1.0, 0.0),  --  Lower right
+--              (X_Pos, Base + Quad_Height,              0.0, 1.0),  --  Upper left
+--
+--              (X_Pos, Base + Quad_Height,              0.0, 1.0),  --  Upper left
+--              (X_Pos + Quad_Width, Base + Quad_Height, 1.0, 1.0),  --  Upper Right --  Lower right --  Lower right
+--              (X_Pos + Quad_Width, Base,               1.0, 0.0)); --  Lower right
+
+      Vertex_Data :=
+           ((X_Start, Base_Min, 0.0, 0.0),  --  Lower left X, Y, U, V
+            (X_End,   Base_Min, 1.0, 0.0),  --  Lower right
+            (X_Start, Top_Max,  0.0, 1.0),  --  Upper left
+
+            (X_Start, Top_Max,  0.0, 1.0),  --  Upper left
+            (X_End,   Top_Max,  1.0, 1.0),  --  Upper Right --  Lower right --  Lower right
+            (X_End,   Base_Min, 1.0, 0.0)); --  Lower right
+
+      Vertex_Array.Bind;
+      Array_Buffer.Bind (Vertex_Buffer);
+      Load_Vertex_Buffer (Array_Buffer, Vertex_Data, Dynamic_Draw);
+   end Setup_Buffer;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Character_Textures (Face_Ptr : FT.Faces.Face_Reference) is
+--        use GL.Objects.Buffers;
+      Glyph_Slot   : constant FT.Glyph_Slot_Reference := Face_Ptr.Glyph_Slot;
+      Width        : GL.Types.Size;
+      Height       : GL.Types.Size;
+--        X_Offset     : constant GL.Types.Int := 0;
+--        Y_Offset     : constant GL.Types.Int := 0;
+      Char_Data    : Character_Record;
+   begin
+      --  Blending allows a fragment colour's alpha value to control the resulting
+      --  colour which will be transparent for all the glyph's background colours and
+      --  non-transparent for the actual character pixels.
+--        GL.Toggles.Enable (GL.Toggles.Blend);
+--        GL.Blending.Set_Blend_Func (GL.Blending.Src_Alpha,
+--                                    GL.Blending.One_Minus_Src_Alpha);
+--
+--        Vertex_Array.Bind;
+--        Array_Buffer.Bind (Vertex_Buffer);
+
+      for index in Extended_Ascii_Data'Range loop
+         --  Load_Render asks FreeType to create an 8-bit grayscale bitmap image
+         --  that can be accessed via face->glyph->bitmap.
+         FT.Faces.Load_Character (Face_Ptr, unsigned_long (index),
+                                  FT.Faces.Load_Render);
+         --  Ensure that the glyph image is an anti-aliased bitmap
+         FT.Glyphs.Render_Glyph (Glyph_Slot, FT.Faces.Render_Mode_Mono);
+
+         Width := GL.Types.Int (FT.Glyphs.Bitmap (Glyph_Slot).Width);
+         Height := GL.Types.Int (FT.Glyphs.Bitmap (Glyph_Slot).Rows);
+
+         Set_Char_Data (Char_Data, Width, Height,
+                        GL.Types.Int (FT.Glyphs.Bitmap_Left (Glyph_Slot)),
+                        GL.Types.Int (FT.Glyphs.Bitmap_Top (Glyph_Slot)),
+                        GL.Types.Int (FT.Glyphs.Advance (Glyph_Slot).X));
+
+--           Load_Texture (Face_Ptr, Char_Data, Width, Height, X_Offset, Y_Offset);
+         Extended_Ascii_Data (index) := Char_Data;
+      end loop;
+   end Setup_Character_Textures;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Font (theLibrary : FT.Library_Reference;
+                         Face_Ptr   : out FT.Faces.Face_Reference;
+                         Font_File  : String) is
+   begin
+      FT.Faces.New_Face (theLibrary, Font_File, 0, Face_Ptr);
+      --  Set pixel size to 48 x 48
+      FT.Faces.Set_Pixel_Sizes (Face_Ptr, 0, 48);
+      --  Disable byte-alignment restriction
+      GL.Pixels.Set_Unpack_Alignment (GL.Pixels.Bytes);
+   end Setup_Font;
+
+   --  ------------------------------------------------------------------------
+
+end Texture_Management;
