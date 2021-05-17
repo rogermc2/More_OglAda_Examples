2203a635a0bd66f46911af3b6f893227c4c6810f
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-17 23:20:56 +1000

    Added new package font_manger

---
+++ games/robo_racer_2d_chap_6/src/font_manger.adb
@@ -0,0 +1,11 @@
+
+package body Font_Manger is
+
+   procedure Draw_Text (Text : String; X, Y, R, G, B : Float) is
+   begin
+      null;
+   end Draw_Text;
+
+   --  -------------------------------------------------------------------------
+
+end Font_Manger;
---
+++ games/robo_racer_2d_chap_6/src/font_manger.ads
@@ -0,0 +1,7 @@
+
+package Font_Manger is
+
+procedure Draw_Text (Text : String; X, Y, R, G, B : Float);
+
+
+end Font_Manger;
