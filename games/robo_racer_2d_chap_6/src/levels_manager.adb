153fa6e99e4bc2a5bb2d682acc0647d2778de471
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-18 14:52:35 +1000

    levels_manager.Game_Stats coded

--- games/robo_racer_2d_chap_6/src/levels_manager.adb
+++ games/robo_racer_2d_chap_6/src/levels_manager.adb
@@ -1,14 +1,42 @@
 
+with Glfw;
+
+with GL.Types;
+
+with Player_Manager;
+with Text_Manager;
+
 package body Levels_Manager is
 
     Pickups_Threshold       : constant Integer := 5;
     Pickup_Spawn_Adjustment : constant Float := 0.25;
-    Enemies_Hit             : Integer := 0;
-    Pickups_Received        : Integer := 0;
-    Level_Max_Time          : Float := 30.0;
+
+   --  -------------------------------------------------------------------------------------------------
+
+   procedure Game_Stats (Window   : in out Input_Callback.Callback_Window;
+                         Pickups_Received, Enemies_Hit : Integer) is
+      use GL.Types;
+      use Player_Manager;
+      Pickups_Stat  : constant String := "Pickups: " & Integer'Image (Pickups_Received);
+      Enemies_Stat  : constant String := "Enemies Hit:" & Integer'Image (Enemies_Hit);
+      Score         : constant String :=
+                        "Score: " & Integer'Image (Get_Value (Get_Current_Player));
+      Screen_Width  : Glfw.Size;
+      Screen_Height : Glfw.Size;
+      Height        : Single;
+    begin
+      Window.Get_Framebuffer_Size (Screen_Width, Screen_Height);
+      Height := Single (Screen_Height);
+      Text_Manager.Draw_Text (Window, Enemies_Stat, 350.0, Height - 270.0, 0.0, 0.0, 1.0);
+      Text_Manager.Draw_Text (Window, Pickups_Stat, 350.0, Height - 320.0, 0.0, 0.0, 1.0);
+      Text_Manager.Draw_Text (Window, Score, 350.0, Height - 370.0, 0.0, 0.0, 1.0);
+   end Game_Stats;
+
+   --  -------------------------------------------------------------------------------------------------
 
     procedure Next_Level (Game_State : in out Game_Status;
-                          Level_Timer, Pickup_Spawn_Threshold : in out Float) is
+                          Level_Timer, Pickup_Spawn_Threshold : in out Float;
+                          Pickups_Received : Integer) is
     begin
         if Pickups_Received < Pickups_Threshold then
             Game_State := Game_Over;
@@ -18,6 +46,8 @@ package body Levels_Manager is
             Level_Timer := 0.0;
             Game_State := Game_Next_Level;
         end if;
-    end Next_Level;
+   end Next_Level;
+
+   --  -------------------------------------------------------------------------------------------------
 
 end Levels_Manager;
--- games/robo_racer_2d_chap_6/src/levels_manager.ads
+++ games/robo_racer_2d_chap_6/src/levels_manager.ads
@@ -1,11 +1,16 @@
 
+with Input_Callback;
+
 package Levels_Manager is
 
     type Game_Status is (Game_Splash, Game_Loading, Game_Menu, Game_Credits,
                          Game_Running, Game_Next_Level, Game_Paused, Game_Over);
     Game_State  : Game_Status := Game_Splash;
 
+    procedure Game_Stats (Window   : in out Input_Callback.Callback_Window;
+                          Pickups_Received, Enemies_Hit : Integer);
     procedure Next_Level (Game_State : in out Game_Status;
-                          Level_Timer, Pickup_Spawn_Threshold : in out Float);
+                          Level_Timer, Pickup_Spawn_Threshold : in out Float;
+                          Pickups_Received : Integer);
 
 end Levels_Manager;
