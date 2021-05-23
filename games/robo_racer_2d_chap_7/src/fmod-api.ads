b296ae5ec8354c3228775d6c3166e278d14d1275
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-23 16:01:44 +1000

    Coding audio

--- games/robo_racer_2d_chap_7/src/fmod-api.ads
+++ games/robo_racer_2d_chap_7/src/fmod-api.ads
@@ -3,19 +3,24 @@ with System;
 
 package Fmod.API is
 
-   type F_System is new System.Address;
-   type F_System_Handle is access F_System;
+--     type F_System is new System.Address;
+   type Fmod_System is record
+      null;
+   end record;
+   pragma Convention (C_Pass_By_Copy, Fmod_System);
+
+   type F_System_Handle is access Fmod_System;
    pragma Convention (C, F_System_Handle);
 
    function System_Close (aSystem : in out F_System_Handle) return Fmod_Result;
-   pragma Import (C, System_Close, "FMOD_System_Close");
+   pragma Import (StdCall, System_Close, "FMOD_System_Close");
 
    function System_Create (aSystem : in out F_System_Handle) return Fmod_Result;
-   pragma Import (C, System_Create, "FMOD_System_Create");
+   pragma Import (StdCall, System_Create, "FMOD_System_Create");
 
-   function System_Init (aSystem : in out F_System_Handle;
-                         maxchannels : Int; flags : Fmod_Init_Flags;
+   function System_Init (aSystem         : in out F_System_Handle;
+                         maxchannels     : Int; flags : Fmod_Init_Flags;
                          extradriverdata : System.Address) return Fmod_Result;
-   pragma Import (C, System_Init, "FMOD_System_Init");
+   pragma Import (StdCall, System_Init, "FMOD_System_Init");
 
 end Fmod.API;
--- games/robo_racer_2d_chap_7/src/fmod.adb
+++ games/robo_racer_2d_chap_7/src/fmod.adb
@@ -3,23 +3,36 @@ with Fmod.API;
 
 package body Fmod is
 
-   Audio    : Fmod.API.F_System_Handle;
-   pragma Convention (C, Audio);
+   Audio     : aliased Fmod.API.Fmod_System;
+   Audio_Ptr : Fmod.API.F_System_Handle;
+   pragma Convention (C, Audio_Ptr);
+
+   --  -------------------------------------------------------------------------
 
    function Close_System return Fmod_Result is
    begin
-      return Fmod.API.System_Close (Audio);
+      Audio_Ptr.all := Audio;
+      return Fmod.API.System_Close (Audio_Ptr);
    end Close_System;
+
+   --  -------------------------------------------------------------------------
 
    function Create_System return Fmod_Result is
    begin
-      return Fmod.API.System_Create (Audio);
+      Audio_Ptr.all := Audio;
+      return Fmod.API.System_Create (Audio_Ptr);
    end Create_System;
+
+   --  -------------------------------------------------------------------------
 
    function Init_System (maxchannels     : Int; flags : Fmod_Init_Flags;
                          extradriverdata : System.Address) return Fmod_Result is
    begin
-      return Fmod.API.System_Init (Audio, maxchannels, flags, extradriverdata);
+      Audio_Ptr.all := Audio;
+      return Fmod.API.System_Init (Audio_Ptr, maxchannels, flags,
+                                   extradriverdata);
    end Init_System;
+
+   --  -------------------------------------------------------------------------
 
 end Fmod;
