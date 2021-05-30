a1071bd5d69f0d41bf0e23cd14ab943dda9fcdee
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-05-31 00:39:58 +1000

    Update

--- games/space_racer_3d_chaps_9_10/src/shaders/vertex_model_shader.glsl
+++ games/space_racer_3d_chaps_9_10/src/shaders/vertex_model_shader.glsl
@@ -1,7 +1,7 @@
 #version 410 core
 
 layout (location = 0) in vec3 points3d;
-layout (location = 1) in vec3 normals;
+// layout (location = 1) in vec3 normals;
 // layout (location = 1) in vec2 uvs_in;
 
 uniform mat4 model_matrix;
