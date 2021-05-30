3b39062c5f931cc3f8025a163f6371c6d738ca99
rogermc2 <rogermc@iinet.net.au>
2021-05-30 10:27:55 +1000

    Model.Update coded

--- games/space_racer_3d_chaps_9_10/src/model.adb
+++ games/space_racer_3d_chaps_9_10/src/model.adb
@@ -1,24 +1,18 @@
 
 with GL.Attributes;
-with GL.Objects.Buffers;
-with GL.Objects.Vertex_Arrays;
 with GL.Types;
 
 with Load_Object_File;
+with Maths;
 with Utilities;
 
 package body Model is
 
-   Model_VAO            : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
-   Model_Vertex_Buffer  : GL.Objects.Buffers.Buffer;
-   Model_Normals_Buffer : GL.Objects.Buffers.Buffer;
-   Model_UVs_Buffer     : GL.Objects.Buffers.Buffer;
-
    --  ------------------------------------------------------------------------
 
-   procedure Bind_Model_VAO is
+   procedure Bind_Model_VAO (aModel : in out Model_Data) is
    begin
-      Model_VAO.Bind;
+      aModel.Model_VAO.Bind;
    end Bind_Model_VAO;
 
    --  --------------------------------------------------------------------------
@@ -30,8 +24,8 @@ package body Model is
       Vertex_Count : Int;
    begin
       aModel.Model_Colour := Colour;
-      Model_VAO.Initialize_Id;
-      Model_VAO.Bind;
+      aModel.Model_VAO.Initialize_Id;
+      aModel.Model_VAO.Bind;
 
       Vertex_Count := Load_Object_File.Mesh_Size (File_Path);
       declare
@@ -41,22 +35,22 @@ package body Model is
       begin
          Load_Object_File.Load_Object (File_Path, Vertices, UVs, Normals);
 
-         Model_Vertex_Buffer.Initialize_Id;
-         Array_Buffer.Bind (Model_Vertex_Buffer);
+         aModel.Model_Vertex_Buffer.Initialize_Id;
+         Array_Buffer.Bind (aModel.Model_Vertex_Buffer);
          Utilities.Load_Vertex_Buffer (Array_Buffer, Vertices, Static_Draw);
          GL.Attributes.Set_Vertex_Attrib_Pointer (0, 3, Single_Type, False,
                                                   0, 0);
          GL.Attributes.Enable_Vertex_Attrib_Array (0);
 
-         Model_Normals_Buffer.Initialize_Id;
-         Array_Buffer.Bind (Model_Normals_Buffer);
+         aModel.Model_Normals_Buffer.Initialize_Id;
+         Array_Buffer.Bind (aModel.Model_Normals_Buffer);
          Utilities.Load_Vertex_Buffer (Array_Buffer, Normals, Static_Draw);
          GL.Attributes.Set_Vertex_Attrib_Pointer (1, 3, Single_Type, False,
                                                   0, 0);
          GL.Attributes.Enable_Vertex_Attrib_Array (1);
 
-         Model_UVs_Buffer.Initialize_Id;
-         Array_Buffer.Bind (Model_UVs_Buffer);
+         aModel.Model_UVs_Buffer.Initialize_Id;
+         Array_Buffer.Bind (aModel.Model_UVs_Buffer);
          Utilities.Load_Vertex_Buffer (Array_Buffer, UVs, Static_Draw);
          GL.Attributes.Set_Vertex_Attrib_Pointer (2, 2, Single_Type, False,
                                                   0, 0);
@@ -76,13 +70,23 @@ package body Model is
    --  ------------------------------------------------------------------------
 
    procedure Update (aModel : in out Model_Data; Delta_Time : Float) is
+      use Maths;
+      use Maths.Single_Math_Functions;
+      use GL.Types;
       use GL.Types.Singles;
+      Distance         : constant Single :=
+                             aModel.Velocity * Single (Delta_Time);
       Target_Position  : Vector3 := aModel.Position;
-      Current_Position : Vector3 := aModel.Position;
-      Target_Rotation  : Vector3 := aModel.Heading_Rotation;
-      Distance         : Float := aModel.Velocity * Delta_Time;
+      Delta_Position   : Vector3;
+--        Target_Rotation  : Vector3 := aModel.Heading_Rotation;
    begin
-      null;
+      Delta_Position (GL.X) := Cos (Target_Position (GL.Z)) * Distance;
+      Delta_Position (GL.Y) := -Sin (Target_Position (GL.Z)) * Distance;
+      Delta_Position (GL.Z) := Sin (Target_Position (GL.X)) * Distance;
+
+      Target_Position := Target_Position + Delta_Position;
+      aModel.Position := Target_Position;
+
    end Update;
 
    --  ------------------------------------------------------------------------
--- games/space_racer_3d_chaps_9_10/src/model.ads
+++ games/space_racer_3d_chaps_9_10/src/model.ads
@@ -1,11 +1,13 @@
 
+with GL.Objects.Buffers;
+with GL.Objects.Vertex_Arrays;
 with GL.Types.Colors;
 
 package Model is
 
    type Model_Data is private;
 
-   procedure Bind_Model_VAO;
+   procedure Bind_Model_VAO (aModel : in out Model_Data);
    procedure Initialize (aModel : in out Model_Data; File_Path : String;
                          Colour : GL.Types.Colors.Basic_Color);
    procedure Render;
@@ -13,13 +15,17 @@ package Model is
 
 private
    type Model_Data is record
+   Model_VAO            : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   Model_Vertex_Buffer  : GL.Objects.Buffers.Buffer;
+   Model_Normals_Buffer : GL.Objects.Buffers.Buffer;
+   Model_UVs_Buffer     : GL.Objects.Buffers.Buffer;
    Position             : GL.Types.Singles.Vector3 := (0.0, 0.0, 0.0);
    Heading              : GL.Types.Singles.Vector3 := (0.0, 0.0, 0.0);
    Base_Rotation        : GL.Types.Singles.Vector3 := (0.0, 0.0, 0.0);
    Heading_Rotation     : GL.Types.Singles.Vector3 := (0.0, 0.0, 0.0);
    Model_Colour         : GL.Types.Colors.Basic_Color := (0.0, 0.0, 0.0);
-   Velocity             : Float := 0.0;
-   Radius               : Float := 1.0;
+   Velocity             : GL.Types.Single := 0.0;
+   Radius               : GL.Types.Single := 1.0;
    Is_Ship              : Boolean := False;
    Is_Visible           : Boolean := True;
    Is_Collidable        : Boolean := True;
