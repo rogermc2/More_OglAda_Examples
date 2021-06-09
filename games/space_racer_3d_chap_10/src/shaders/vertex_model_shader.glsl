b589a2dd5b0e4a8d9dbc390dde3876e1bf41cdd4
rogermc2 <rogermc@iinet.net.au>
2021-06-09 15:19:06 +1000

    Cleanup

--- games/space_racer_3d_chap_10/src/shaders/vertex_model_shader.glsl
+++ games/space_racer_3d_chap_10/src/shaders/vertex_model_shader.glsl
@@ -1,8 +1,6 @@
 #version 410 core
 
 layout (location = 0) in vec3 points3d;
-// layout (location = 1) in vec3 normals;
-// layout (location = 1) in vec2 uvs_in;
 
 uniform mat4 model_matrix;
 uniform mat4 view_matrix;
@@ -10,13 +8,10 @@ uniform mat4 projection_matrix;
 
 uniform vec3 colour_in;
 
-//  out vec2 uvs;
 out vec3 frag_colour;
 
 void main()
     {
- //   gl_PointSize = 40.0;
     gl_Position = projection_matrix * view_matrix * model_matrix * vec4(points3d, 1.0);
     frag_colour = colour_in;
-//    uvs = uvs_in;
     }
