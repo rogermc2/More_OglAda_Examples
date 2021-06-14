dc20a87c06a2556304d6e8856c88284c0ff8fc20
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-06-14 15:25:24 +1000

    Unecessary code deleted

--- games/fun_with_3d_chap_12/src/main_loop.adb
+++ games/fun_with_3d_chap_12/src/main_loop.adb
@@ -4,7 +4,6 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with Glfw.Input;
 with Glfw.Input.Keys;
-with Glfw.Input.Mouse;
 with Glfw.Windows;
 with Glfw.Windows.Context;
 
@@ -19,7 +18,6 @@ with Maths;
 with Utilities;
 
 with Shader_Manager_Texture;
-with Shader_Manager_UI;
 with Textures_Manager;
 
 --  ------------------------------------------------------------------------
@@ -27,14 +25,11 @@ with Textures_Manager;
 procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
 
    Back             : constant GL.Types.Colors.Color := (0.6, 0.6, 0.6, 0.0);
---     Vec4_Zero        : constant GL.Types.Singles.Vector4 :=
---                             (0.0, 0.0, 0.0, 0.0);
    Rotation_Vector  : GL.Types.Singles.Vector3 := (1.0, 1.0, 1.0);
    Texture_Marble   : GL.Objects.Textures.Texture;
 
-   procedure Initialize_2D;
    procedure Resize_GL_Scene (Screen : in out Input_Callback.Callback_Window);
-   procedure Start_Game  (Screen : in out Input_Callback.Callback_Window);
+   procedure Start_Game;
 
    --  -------------------------------------------------------------------------
 
@@ -43,16 +38,16 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
       use GL.Types;
       use GL.Types.Singles;
       use Shader_Manager_Texture;
-       View_Matrix       : constant Matrix4 := Identity4;
-       Rot_Matrix        : Matrix4 := Identity4;
-       Model_Matrix      : Matrix4 := Identity4;
-       Projection_Matrix : Matrix4 := Identity4;
+      View_Matrix       : constant Matrix4 := Identity4;
+      Rot_Matrix        : Matrix4 := Identity4;
+      Model_Matrix      : Matrix4 := Identity4;
+      Projection_Matrix : Matrix4 := Identity4;
    begin
       Init_Rotation_Transform (Rotation_Vector, Rot_Matrix);
       Model_Matrix := Translation_Matrix ((0.0, 0.0, 5.0)) * Rot_Matrix *
-          Model_Matrix;
+        Model_Matrix;
       Init_Orthographic_Transform
-          (5.0, -5.0, -5.0, 5.0, 0.0, 10.0, Projection_Matrix);
+        (5.0, -5.0, -5.0, 5.0, 0.0, 10.0, Projection_Matrix);
       Use_Texture_Program;
       Set_Model_Matrix (Model_Matrix);
       Set_View_Matrix (View_Matrix);
@@ -64,51 +59,6 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
       Rotation_Vector := Rotation_Vector + (0.1, 0.2, 0.3);
 
    end Draw_Textured_Cube;
-
-   --  ------------------------------------------------------------------------
-
-   procedure Enable_2D (Screen : in out Input_Callback.Callback_Window) is
-      use GL.Types;
-      use Shader_Manager_UI;
-      Screen_Width      : Glfw.Size;
-      Screen_Height     : Glfw.Size;
-      Projection_Matrix : Singles.Matrix4;
-   begin
-      Screen.Get_Framebuffer_Size (Screen_Width, Screen_Height);
-      Use_2D_Program;
-      Maths.Init_Orthographic_Transform
-        (Top => Single (Screen_Height), Bottom => 0.0,
-         Left => 0.0, Right => Single (Screen_Width),
-         Z_Near => 0.0, Z_Far => 1.0, Transform => Projection_Matrix);
-      Shader_Manager_UI.Set_Projection_Matrix (Projection_Matrix);
-   end Enable_2D;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Enable_Mouse_Callbacks
-     (Window : in out Input_Callback.Callback_Window;
-      Enable : Boolean) is
-      use Glfw.Windows.Callbacks;
-   begin
-      if Enable then
-         Window.Enable_Callback (Mouse_Position);
-         Window.Enable_Callback (Mouse_Enter);
-         Window.Enable_Callback (Mouse_Button);
-         Window.Enable_Callback (Mouse_Scroll);
-      else
-         Window.Disable_Callback (Mouse_Position);
-         Window.Disable_Callback (Mouse_Enter);
-         Window.Disable_Callback (Mouse_Button);
-         Window.Disable_Callback (Mouse_Scroll);
-      end if;
-   end Enable_Mouse_Callbacks;
-
-   ----------------------------------------------------------------------------
-
-   procedure Initialize_2D is
-   begin
-      Shader_Manager_UI.Init_Shaders;
-   end Initialize_2D;
 
    --  ------------------------------------------------------------------------
 
@@ -125,13 +75,6 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Render_2D (Screen : in out Input_Callback.Callback_Window) is
-   begin
-      Enable_2D (Screen);
-   end Render_2D;
-
-   --  ------------------------------------------------------------------------
-
    procedure Render_3D is
    begin
       Draw_Textured_Cube;
@@ -144,7 +87,6 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
       Utilities.Clear_Colour_Buffer_And_Depth;
       Resize_GL_Scene (Screen);
       Render_3D;
-      Render_2D (Screen);
    end Render;
 
    --  ------------------------------------------------------------------------
@@ -164,26 +106,13 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Start_Game  (Screen : in out Input_Callback.Callback_Window) is
+   procedure Start_Game is
       use GL.Types;
-      use Glfw.Input;
-      Window_Width  : Glfw.Size;
-      Window_Height : Glfw.Size;
    begin
-      Screen.Set_Cursor_Mode (Mouse.Normal);
-      Screen'Access.Get_Size (Window_Width, Window_Height);
-      Screen'Access.Set_Cursor_Pos
-        (Mouse.Coordinate (0.5 * Single (Window_Width)),
-         Mouse.Coordinate (0.5 * Single (Window_Height)));
-
       Utilities.Clear_Background_Colour_And_Depth (Back);
-        GL.Toggles.Enable (GL.Toggles.Depth_Test);
-        GL.Buffers.Set_Depth_Function (GL.Types.LEqual);
-      Input_Callback.Clear_All_Keys;
-      --          GL.Toggles.Enable (GL.Toggles.Vertex_Program_Point_Size);
-      Initialize_2D;
+      GL.Toggles.Enable (GL.Toggles.Depth_Test);
+      GL.Buffers.Set_Depth_Function (GL.Types.LEqual);
       Initialize_3D;
-      Enable_Mouse_Callbacks (Screen, True);
 
    exception
       when anError : others =>
@@ -194,22 +123,12 @@ procedure Main_Loop (Main_Window : in out Input_Callback.Callback_Window) is
 
    --  -------------------------------------------------------------------------
 
-   procedure Update is
-   begin
-      null;
-   end Update;
-
-   --  ------------------------------------------------------------------------
-
    use Glfw.Input;
    Running  : Boolean := True;
 begin
-   Start_Game (Main_Window);
+   Start_Game;
    while Running loop
-      Update;
-      if Running then
-         Render (Main_Window);
-      end if;
+      Render (Main_Window);
       Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
       Glfw.Input.Poll_Events;
       Running := Running and not
